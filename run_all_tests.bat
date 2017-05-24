ceead3fb014a8c84b57a3f7c59e12ff94e106b46
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-05-24 16:32:12 +0100

    Add instron to run_tests and run_all_tests scripts

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -1,5 +1,7 @@
 @echo off
-REM Run all known tests using the IOC Testing Framework
+REM Run all known tests using hte IOC Testing Framework
+
+SET CurrentDir=%~dp0
 
 call "%~dp0..\..\..\config_env.bat"
 
@@ -10,14 +12,15 @@ echo ---------------------------------------
 echo;
 
 echo ---------------------------------------
-echo TESTING JULABO Rec Sim
-call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d julabo -p %EPICS_KIT_ROOT%\ioc\master\JULABO\iocBoot\iocJULABO-IOC-01
+echo TESTING AMINT2L Dev Sim
+call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX%  -d amint2l -p %EPICS_KIT_ROOT%\ioc\master\AMINT2L\iocBoot\iocAMINT2L-IOC-01 -e %PYTHONDIR%\Scripts -ea %EPICS_KIT_ROOT%\support\DeviceEmulator\master -ek lewis_emulators
 echo ---------------------------------------
 echo;
 
+
 echo ---------------------------------------
-echo TESTING AMINT2L Dev Sim
-call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX%  -d amint2l -p %EPICS_KIT_ROOT%\ioc\master\AMINT2L\iocBoot\iocAMINT2L-IOC-01 -e %PYTHONDIR%\Scripts -ea %EPICS_KIT_ROOT%\support\DeviceEmulator\master -ek lewis_emulators
+echo TESTING JULABO Rec Sim
+call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d julabo -p %EPICS_KIT_ROOT%\ioc\master\JULABO\iocBoot\iocJULABO-IOC-01
 echo ---------------------------------------
 echo;
 
@@ -26,6 +29,7 @@ echo TESTING AMINT2L Rec Sim
 call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d amint2l -p %EPICS_KIT_ROOT%\ioc\master\AMINT2L\iocBoot\iocAMINT2L-IOC-01
 echo ---------------------------------------
 echo;
+
 
 echo ---------------------------------------
 echo TESTING INSTRON Dev Sim
