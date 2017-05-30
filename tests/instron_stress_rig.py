6ace7a4ddff7dbeb99ca9f65b9d0689bf1e9f9c0
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-05-30 16:38:23 +0100

    Rename tests

--- tests/instron_stress_rig.py
+++ tests/instron_stress_rig.py
@@ -17,23 +17,23 @@ class Instron_stress_rigTests(unittest.TestCase):
         self.ca = ChannelAccess(15)
         self.ca.wait_for("INSTRON_01:CHANNEL", timeout=30)
 
-    def test_that_when_the_rig_is_initialized_the_status_is_ok(self):
+    def test_WHEN_the_rig_is_initialized_THEN_the_status_is_ok(self):
         self.ca.assert_that_pv_is("INSTRON_01:STAT:DISP", "System OK")
 
-    def test_that_when_the_rig_is_initialized_it_is_not_going(self):
+    def test_WHEN_the_rig_is_initialized_THEN_it_is_not_going(self):
         self.ca.assert_that_pv_is("INSTRON_01:GOING", "NO")
 
-    def test_that_when_the_rig_is_initialized_it_is_not_panic_stopping(self):
+    def test_WHEN_the_rig_is_initialized_THEN_it_is_not_panic_stopping(self):
         self.ca.assert_that_pv_is("INSTRON_01:PANIC:SP", "READY")
 
-    def test_that_when_the_rig_is_initialized_it_is_not_stopping(self):
+    def test_WHEN_the_rig_is_initialized_THEN_it_is_not_stopping(self):
         self.ca.assert_that_pv_is("INSTRON_01:STOP:SP", "READY")
 
     def test_that_the_rig_is_not_normally_in_control_mode(self):
         self.ca.assert_that_pv_is("INSTRON_01:STOP:SP", "READY")
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_that_going_and_then_stopping_reflects_the_expected_state(self):
+    def test_WHEN_going_and_then_stopping_THEN_going_pv_reflects_the_expected_state(self):
         self.ca.assert_that_pv_is("INSTRON_01:GOING", "NO")
         self.ca.set_pv_value("INSTRON_01:MOVE:GO:SP", 1)
         self.ca.assert_that_pv_is("INSTRON_01:GOING", "YES")
@@ -41,7 +41,7 @@ class Instron_stress_rigTests(unittest.TestCase):
         self.ca.assert_that_pv_is("INSTRON_01:GOING", "NO")
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_that_going_and_then_panic_stopping_reflects_the_expected_state(self):
+    def test_WHEN_going_and_then_panic_stopping_THEN_going_pv_reflects_the_expected_state(self):
         self.ca.assert_that_pv_is("INSTRON_01:GOING", "NO")
         self.ca.set_pv_value("INSTRON_01:MOVE:GO:SP", 1)
         self.ca.assert_that_pv_is("INSTRON_01:GOING", "YES")
@@ -49,19 +49,19 @@ class Instron_stress_rigTests(unittest.TestCase):
         self.ca.assert_that_pv_is("INSTRON_01:GOING", "NO")
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_that_an_arbitrary_command_Q22_can_be_sent(self):
+    def test_WHEN_arbitrary_command_Q22_is_sent_THEN_the_response_is_a_status_code(self):
         self.ca.set_pv_value("INSTRON_01:ARBITRARY:SP", "Q22")
         # Assert that the response to Q22 is a status code
         self.ca.assert_that_pv_is_an_integer_between("INSTRON_01:ARBITRARY", min=0, max=65535)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_that_an_arbitrary_command_Q300_can_be_sent(self):
+    def test_WHEN_arbitrary_command_Q300_is_sent_THEN_the_response_is_a_number_between_1_and_3(self):
         self.ca.set_pv_value("INSTRON_01:ARBITRARY:SP", "Q300")
         # Assert that the response to Q300 is between 1 and 3
         self.ca.assert_that_pv_is_an_integer_between("INSTRON_01:ARBITRARY", min=1, max=3)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_that_an_arbitrary_command_C1_and_Q1_act_as_expected(self):
+    def test_WHEN_arbitrary_command_C1_is_sent_THEN_Q1_gives_back_the_value_that_was_just_set(self):
 
         def _set_and_check(value):
             self.ca.set_pv_value("INSTRON_01:ARBITRARY:SP", "C1," + value)
@@ -73,7 +73,7 @@ class Instron_stress_rigTests(unittest.TestCase):
             _set_and_check(v)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_that_if_the_movement_type_on_rig_is_hold_then_it_gets_stopped(self):
+    def test_WHEN_the_movement_type_on_rig_is_hold_THEN_it_gets_stopped(self):
 
         self.ca.set_pv_value("INSTRON_01:MOVE:SP", 1)
         self.ca.assert_that_pv_is_one_of("INSTRON_01:MOVE", ["RAMP_RUNNING", "RAND_RUNNING"])
@@ -81,14 +81,14 @@ class Instron_stress_rigTests(unittest.TestCase):
         self.ca.set_pv_value("INSTRON_01:MOVE:SP", 2)
         self.ca.assert_that_pv_is("INSTRON_01:MOVE", "STOPPED")
 
-    def test_that_control_channel_is_one_of_the_allowed_values(self):
+    def test_WHEN_control_channel_is_requested_THEN_an_allowed_value_is_returned(self):
         self.ca.assert_that_pv_is_one_of("INSTRON_01:CHANNEL", ["Stress", "Strain", "Position"])
 
-    def test_that_control_channel_setpoint_is_one_of_the_allowed_values(self):
+    def test_WHEN_control_channel_setpoint_is_requested_THEN_it_is_one_of_the_allowed_values(self):
         self.ca.assert_that_pv_is_one_of("INSTRON_01:CHANNEL:SP", ["Stress", "Strain", "Position"])
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_that_setting_the_control_channel_works_as_expected(self):
+    def test_WHEN_the_control_channel_is_set_THEN_the_readback_contains_the_value_that_was_just_set(self):
 
         def _set_and_check(set_value, return_value):
             self.ca.set_pv_value("INSTRON_01:CHANNEL:SP", set_value)
