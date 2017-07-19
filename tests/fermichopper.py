620eac319dd9290f864db661f055f44b08ff5651
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-07-19 15:11:03 +0100

    Allow passthrough of strings through asub

--- tests/fermichopper.py
+++ tests/fermichopper.py
@@ -13,6 +13,8 @@ class FermichopperTests(unittest.TestCase):
     Tests for the Fermi Chopper IOC.
     """
 
+    valid_commands = ["0001", "0002", "0003", "0006", "0007"]
+
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("fermichopper")
 
@@ -24,12 +26,12 @@ class FermichopperTests(unittest.TestCase):
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_WHEN_last_command_is_set_via_backdoor_THEN_pv_updates(self):
-        for value in ["0003", "0004", "0006"]:
+        for value in self.valid_commands:
             self._lewis.backdoor_command(["device", "last_command", "'" + value + "'"])
             self.ca.assert_that_pv_is("FERMCHOP_01:THING", value)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_WHEN_last_command_is_set_THEN_readback_updates(self):
-        for value in ["0003", "0004", "0006"]:
+        for value in self.valid_commands:
             self.ca.set_pv_value("FERMCHOP_01:SENDTHING", value)
             self.ca.assert_that_pv_is("FERMCHOP_01:THING", value, timeout=20)
