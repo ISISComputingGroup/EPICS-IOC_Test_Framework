878964f70131cffe763b1c0d75ad82d271ea4791
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-07-21 16:52:58 +0100

    Also check alarm state

--- tests/fermichopper.py
+++ tests/fermichopper.py
@@ -39,29 +39,40 @@ class FermichopperTests(unittest.TestCase):
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_get_speed_sp_rbv_returns_18000(self):
         self.ca.assert_that_pv_is("FERMCHOP_01:SPEED:SP:RBV", 18000)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:SPEED:SP:RBV", self.ca.ALARM_NONE)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_get_speed_returns_17999(self):
         self.ca.assert_that_pv_is("FERMCHOP_01:SPEED", 17999)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:SPEED", self.ca.ALARM_NONE)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_get_delay_sp_rbv_returns_416_and_two_thirds(self):
         self.ca.assert_that_pv_is_number("FERMCHOP_01:DELAY:SP:RBV", 416.67, tolerance=0.02)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:DELAY:SP:RBV", self.ca.ALARM_NONE)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_get_delay_returns_416_and_two_thirds(self):
         self.ca.assert_that_pv_is_number("FERMCHOP_01:DELAY", 416.67, tolerance=0.02)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:DELAY", self.ca.ALARM_NONE)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_get_gatewidth_returns_833_nsec(self):
         self.ca.assert_that_pv_is_number("FERMCHOP_01:GATEWIDTH", 0.833, tolerance=0.001)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:GATEWIDTH", self.ca.ALARM_NONE)
 
     @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
     def test_get_drive_current(self):
         self.ca.assert_that_pv_is_number("FERMCHOP_01:DRIVECURRENT", 0.98985, tolerance=0.00005)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:DRIVECURRENT", self.ca.ALARM_NONE)
 
     def test_get_autozero_voltages(self):
         self.ca.assert_that_pv_is_number("FERMCHOP_01:AUTOZERO:1:UPPER", -0.6, tolerance=0.05)
         self.ca.assert_that_pv_is_number("FERMCHOP_01:AUTOZERO:2:UPPER", -0.2, tolerance=0.05)
         self.ca.assert_that_pv_is_number("FERMCHOP_01:AUTOZERO:1:LOWER", -0.1, tolerance=0.05)
         self.ca.assert_that_pv_is_number("FERMCHOP_01:AUTOZERO:2:LOWER", 0.6, tolerance=0.05)
+
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:AUTOZERO:1:UPPER", self.ca.ALARM_NONE)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:AUTOZERO:2:UPPER", self.ca.ALARM_NONE)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:AUTOZERO:1:LOWER", self.ca.ALARM_NONE)
+        self.ca.assert_pv_alarm_is("FERMCHOP_01:AUTOZERO:1:LOWER", self.ca.ALARM_NONE)
