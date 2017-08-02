d4fa280f587473441b16d206b8717452f1a9ae71
esouthren <eilidhsouthren@gmail.com>
2017-08-02 15:46:23 +0100

    Update tdk_lambda_genesys.py
    Formatting changes

--- tests/tdk_lambda_genesys.py
+++ tests/tdk_lambda_genesys.py
@@ -1,8 +1,3 @@
-'''
-Tests for the TDK Genesys PSU
-@author Eilidh Southren
-'''
-
 import unittest
 from utils.channel_access import ChannelAccess
 from utils.testing import get_running_lewis_and_ioc
