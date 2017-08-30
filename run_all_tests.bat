1cd87e742103056363c3c573abb2a441d2c9280a
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2017-08-30 11:39:09 +0100

    Add eurotherm test command

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -114,3 +114,9 @@ echo TESTING RKNPS Rec Sim
 call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d rknps -p %EPICS_KIT_ROOT%\ioc\master\RKNPS\iocBoot\iocRKNPS-IOC-01
 echo ---------------------------------------
 echo;
+
+echo ---------------------------------------
+echo TESTING EUROTHRM Dev Sim
+call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX%  -d eurotherm -p %EPICS_KIT_ROOT%\ioc\master\EUROTHRM\iocBoot\iocEUROTHRM-IOC-01 -e %PYTHONDIR%\Scripts -ea %EPICS_KIT_ROOT%\support\DeviceEmulator\master -ek lewis_emulators
+echo ---------------------------------------
+echo;
