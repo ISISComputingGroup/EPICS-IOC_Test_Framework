0c8fd13d58123e29ef879bd65ae62ba08aa965aa
Attah <mohammed.attah@stfc.ac.uk>
2017-09-20 14:32:48 +0100

    first half of tests

--- tests/lakeshore460.py
+++ tests/lakeshore460.py
@@ -13,8 +13,7 @@ class UnitStrings(object):
     DC = "DC"
     PEAK = "PEAK"
     RMS = "RMS"
-    AUTORANGEON = "Auto Range " + ON
-    AUTORANGEOFF = "Auto Range " + OFF
+
 
 class UnitFlags(object):
     GAUSS = 0
@@ -45,7 +44,9 @@ class Lakeshore460Tests(unittest.TestCase):
         self._ioc.set_simulated_value("LKSH460_01:SIM:MAXREADING", expected_max_reading)
 
     def _set_unit(self, expected_unit):
-        self.ca.set_pv_value("LKSH460_01:UNIT:SP", expected_unit)
+        expected_channels = {"X": expected_unit, "Y": expected_unit, "Z": expected_unit, "V": expected_unit}
+        for channel, unit in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:UNIT:SP".format(channel), unit)
 
     def _set_total_fields(self, expected_total_fields):
         self._lewis.backdoor_set_on_device("total_fields", expected_total_fields)
@@ -65,138 +66,89 @@ class Lakeshore460Tests(unittest.TestCase):
         self.ca.set_pv_value("LKSH460_01:IDN.PROC", 1)
         self.ca.assert_that_pv_is("LKSH460_01:IDN", expected_idn)
 
-    def test_GIVEN_unit_set_WHEN_read_THEN_unit_is_as_expected(self):
+    def test_GIVEN_unit_set_for_a_channel_WHEN_read_THEN_unit_is_as_expected_for_channel(self):
         expected_unit_str = UnitStrings.TESLA
         expected_unit_flag = UnitFlags.TESLA
         self._set_unit(expected_unit_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:UNIT", expected_unit_str)
+        expected_channels = {"X": expected_unit_str, "Y": expected_unit_str, "Z": expected_unit_str,
+                             "V": expected_unit_str}
+        for channel, unit in expected_channels.iteritems():
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:UNIT".format(channel), unit)
 
-    def test_GIVEN_output_status_set_WHEN_read_THEN_output_STATUS_is_as_expected(self):
+    def test_GIVEN_output_status_set_for_a_channel_WHEN_read_THEN_output_STATUS_is_as_expected_for_channel(self):
         expected_output_status_flag = UnitFlags.ON
-        self.ca.set_pv_value("LKSH460_01:OUTPUTSTATUS:SP", expected_output_status_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:OUTPUTSTATUS", UnitStrings.ON)
+        expected_channels = {"X": expected_output_status_flag, "Y": expected_output_status_flag,
+                             "Z": expected_output_status_flag, "V": expected_output_status_flag}
+        for channel, output_status in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:OUTPUTSTATUS:SP".format(channel), output_status)
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:OUTPUTSTATUS".format(channel), UnitStrings.ON)
 
-    def test_GIVEN_output_mode_set_WHEN_read_THEN_output_mode_is_as_expected(self):
-        expected_output_status_flag = UnitFlags.DC
-        self.ca.set_pv_value("LKSH460_01:OUTPUTMODE:SP", expected_output_status_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:OUTPUTMODE", UnitStrings.DC)
+    def test_GIVEN_output_mode_set_for_a_channel_WHEN_read_THEN_output_mode_is_as_expected_for_channel(self):
+        expected_output_mode_flag = UnitFlags.DC
+        expected_channels = {"X": expected_output_mode_flag, "Y": expected_output_mode_flag,
+                             "Z": expected_output_mode_flag, "V": expected_output_mode_flag}
+        for channel, output_mode in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:OUTPUTMODE:SP".format(channel), output_mode)
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:OUTPUTMODE".format(channel), UnitStrings.DC)
 
-    def test_GIVEN_prms_set_WHEN_read_THEN_prms_is_as_expected(self):
+    def test_GIVEN_prms_set_for_a_channel_WHEN_read_THEN_prms_is_as_expected_for_set_channel(self):
         expected_prms_status_flag = UnitFlags.PEAK
-        self.ca.set_pv_value("LKSH460_01:PRMS:SP", expected_prms_status_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:PRMS", UnitStrings.PEAK)
+        expected_channels = {"X": expected_prms_status_flag, "Y": expected_prms_status_flag,
+                             "Z": expected_prms_status_flag, "V": expected_prms_status_flag}
+        for channel, prms_status in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:PRMS:SP".format(channel), prms_status)
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:PRMS".format(channel), UnitStrings.PEAK)
 
-    def test_GIVEN_display_filter_set_WHEN_read_THEN_display_filter_is_as_expected(self):
+    def test_GIVEN_display_filter_set_for_channel_WHEN_read_THEN_display_filter_is_as_expected_for_channel(self):
         expected_display_filter_flag = UnitFlags.ON
-        self.ca.set_pv_value("LKSH460_01:FILTER:SP", expected_display_filter_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:FILTER", UnitStrings.ON)
+        expected_channels = {"X": expected_display_filter_flag, "Y": expected_display_filter_flag,
+                             "Z": expected_display_filter_flag, "V": expected_display_filter_flag}
+        for channel, display_filter in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:FILTER:SP".format(channel), display_filter)
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:FILTER".format(channel), UnitStrings.ON)
 
-    def test_GIVEN_relative_mode_set_WHEN_read_THEN_relative_mode_is_as_expected(self):
+    def test_GIVEN_relative_mode_set_for_channel_WHEN_read_THEN_relative_mode_is_as_expected_for_set_channel(self):
         expected_relative_mode_flag = UnitFlags.ON
-        self.ca.set_pv_value("LKSH460_01:RELMODE:SP", expected_relative_mode_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:RELMODE", UnitStrings.ON)
+        expected_channels = {"X": expected_relative_mode_flag, "Y": expected_relative_mode_flag,
+                             "Z": expected_relative_mode_flag, "V": expected_relative_mode_flag}
+        for channel, display_filter in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:RELMODE:SP".format(channel), display_filter)
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:RELMODE".format(channel), UnitStrings.ON)
 
     def test_GIVEN_max_hold_set_WHEN_read_THEN_max_hold_is_as_expected(self):
         expected_max_hold_flag = UnitFlags.ON
-        self.ca.set_pv_value("LKSH460_01:MAXHOLD:SP", expected_max_hold_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:MAXHOLD", UnitStrings.ON)
+        expected_channels = {"X": expected_max_hold_flag, "Y": expected_max_hold_flag,
+                             "Z": expected_max_hold_flag, "V": expected_max_hold_flag}
+        for channel, max_hold in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:MAXHOLD:SP".format(channel), max_hold)
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:MAXHOLD".format(channel), UnitStrings.ON)
 
     def test_GIVEN_auto_range_set_WHEN_read_THEN_auto_range_is_as_expected(self):
         expected_auto_range_flag = UnitFlags.ON
-        self.ca.set_pv_value("LKSH460_01:AUTO:SP", expected_auto_range_flag)
-        self.ca.assert_that_pv_is("LKSH460_01:AUTO", UnitStrings.AUTORANGEON)
+        expected_channels = {"X": expected_auto_range_flag, "Y": expected_auto_range_flag,
+                             "Z": expected_auto_range_flag, "V": expected_auto_range_flag}
+        for channel, range in expected_channels.iteritems():
+            self.ca.set_pv_value("LKSH460_01:{0}:AUTO:SP".format(channel), range)
+            self.ca.assert_that_pv_is("LKSH460_01:{0}:AUTO".format(channel), UnitStrings.ON)
 
-    def test_GIVEN_all_fields_set_WHEN_read_THEN_all_fields_is_as_expected(self):
-        expected_total_fields = 8.3
-        self._set_total_fields(expected_total_fields)
-        self.ca.assert_that_pv_is("LKSH460_01:ALLFIELDS", expected_total_fields)
+    # def test_GIVEN_source_set_WHEN_read_THEN_source_is_as_expected(self):
+    #     expected_source = 3
+    #     self.ca.set_pv_value("LKSH460_01:SOURCE:SP", expected_source)
+    #     self.ca.assert_that_pv_is("LKSH460_01:SOURCE", "XZ")
 
-    def test_GIVEN_source_set_WHEN_read_THEN_source_is_as_expected(self):
-        expected_source = 3
-        self.ca.set_pv_value("LKSH460_01:SOURCE:SP", expected_source)
-        self.ca.assert_that_pv_is("LKSH460_01:SOURCE", "XZ")
+    def test_GIVEN_filter_window_set_for_a_channel_WHEN_read_THEN_filter_window_is_as_expected_AND_within_range(self):
+        # expected_window_percentage = [1, 11, 10, 22]
+        # alarm_state = [ChannelAccess.ALARM_NONE, ChannelAccess.ALARM_MAJOR, ChannelAccess.ALARM_NONE,
+        #                ChannelAccess.ALARM_MAJOR]
+        channels = ["X", "Y", "Z", "V"]
+        sample_data = {1: ChannelAccess.ALARM_NONE, 11: ChannelAccess.ALARM_MAJOR, 10: ChannelAccess.ALARM_NONE,
+                       22: ChannelAccess.ALARM_MAJOR}
 
-    def test_GIVEN_channel_set_WHEN_read_THEN_channel_is_as_expected(self):
-        sample_data = [1, 2, 3,4]
-        expected_range = ["Channel X", "Channel Y", "Channel Z", "Vector Magnitude Channel"]
-        for input, output in zip(sample_data, expected_range):
-            self.ca.set_pv_value("LKSH460_01:CHANNEL:SP", input)
-            self.ca.assert_that_pv_is("LKSH460_01:CHANNEL", output)
+        self.ca.set_pv_value("LKSH460_01:X:FWIN:SP", 5)
+        self.ca.assert_that_pv_is("LKSH460_01:X:FWIN", 5)
+               # self.ca.assert_pv_alarm_is("LKSH460_01:X:FWIN", A)
 
-    def test_GIVEN_filter_window_set_WHEN_read_THEN_filter_window_is_as_expected_AND_within_range(self):
-        expected_window_percentage = [1, 11, 10, 22]
-        alarm_state = [ChannelAccess.ALARM_NONE, ChannelAccess.ALARM_MAJOR, ChannelAccess.ALARM_NONE, ChannelAccess.ALARM_MAJOR]
-        for input, output in zip(expected_window_percentage, alarm_state):
-            self.ca.set_pv_value("LKSH460_01:FWIN:SP", input)
-            self.ca.assert_that_pv_is("LKSH460_01:FWIN", input)
-            self.ca.assert_pv_alarm_is("LKSH460_01:FWIN", output)
 
-    def test_GIVEN_filter_points_set_WHEN_read_THEN_filter_points_is_as_expected_AND_within_range(self):
-        expected_filter_points = [70, 64, 65, 2]
-        alarm_state = [ChannelAccess.ALARM_MAJOR, ChannelAccess.ALARM_NONE, ChannelAccess.ALARM_MAJOR, ChannelAccess.ALARM_NONE]
-        for input, output in zip(expected_filter_points, alarm_state):
-            self.ca.set_pv_value("LKSH460_01:FNUM:SP", input)
-            self.ca.assert_that_pv_is("LKSH460_01:FNUM", input)
-            self.ca.assert_pv_alarm_is("LKSH460_01:FNUM", output)
-
-    def test_GIVEN_range_set_manually_WHEN_read_THEN_range_is_as_expected(self):
-        sample_data = [0, 1, 2, 3]
-        expected_range = ["First Range", "Second Range", "Third Range", "Fourth Range"]
-        for input, output in zip(sample_data, expected_range):
-            self.ca.set_pv_value("LKSH460_01:RANGE:SP", input)
-            self.ca.assert_that_pv_is("LKSH460_01:RANGE", output)
-
-    def test_GIVEN_max_reading_set_WHEN_read_THEN_max_reading_is_as_expected(self):
-        expected_max_reading = 500
-        self._set_max_reading(expected_max_reading)
-        self.ca.assert_that_pv_is("LKSH460_01:MAXREADING", expected_max_reading)
-
-    @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_GIVEN_max_reading_unit_set_WHEN_read_THEN_max_reading_unit_is_as_expected(self):
-        sample_data = [0, 1, 2, 3]
-        expected_unit = ["uG", "mG", "G", "kG"]
-        for input, output in zip(sample_data, expected_unit):
-            self.ca.set_pv_value("LKSH460_01:MAX:MULTIPLIER", input)
-            self.ca.assert_that_pv_is("LKSH460_01:MAXREADING.EGU", output)
-
-    def test_GIVEN_relative_mode_set_point_set_WHEN_read_THEN_relative_mode_set_point_is_as_expected(self):
-        expected_rel_set_point = 4132
-        self.ca.set_pv_value("LKSH460_01:RELSMODE:SP", expected_rel_set_point)
-        self.ca.assert_that_pv_is("LKSH460_01:RELSMODE", expected_rel_set_point)
-
-    @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_GIVEN_relative_mode_set_point_unit_set_WHEN_read_THEN_relative_mode_set_point_unit_is_as_expected(self):
-        sample_data = [0, 1, 2, 3]
-        expected_unit = ["uG", "mG", "G", "kG"]
-        for input, output in zip(sample_data, expected_unit):
-            self.ca.set_pv_value("LKSH460_01:RELS:MULTIPLIER", input)
-            self.ca.assert_that_pv_is("LKSH460_01:RELSMODE.EGU", output)
-
-    def test_GIVEN_relative_mode_reading_set_WHEN_read_THEN_relative_mode_reading_is_as_expected(self):
-        expected_rel_mode_reading = 500
-        self._set_rel_mode_reading(expected_rel_mode_reading)
-        self.ca.assert_that_pv_is("LKSH460_01:RELMODEREADING", expected_rel_mode_reading)
-
-    @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_GIVEN_relative_mode_reading_unit_set_WHEN_read_THEN_relative_mode_reading_unit_is_as_expected(self):
-        sample_data = [0, 1, 2, 3]
-        expected_unit = ["uG", "mG", "G", "kG"]
-        for input, output in zip(sample_data, expected_unit):
-            self.ca.set_pv_value("LKSH460_01:RELRM:MULTIPLIER", input)
-            self.ca.assert_that_pv_is("LKSH460_01:RELMODEREADING.EGU", output)
-
-    def test_GIVEN_magnetic_field_reading_set_WHEN_read_THEN_magnetic_field_reading_is_as_expected(self):
-        expected_field_reading = 400
-        self._set_magnetic_field_reading(expected_field_reading)
-        self.ca.assert_that_pv_is("LKSH460_01:FIELDREADING", expected_field_reading)
-
-    @skipIf(IOCRegister.uses_rec_sim, "In rec sim this test fails")
-    def test_GIVEN_magnetic_field_reading_unit_set_WHEN_read_THEN_magnetic_field_reading_unit_is_as_expected(self):
-        sample_data = [0, 1, 2, 3]
-        expected_unit = ["uG", "mG", "G", "kG"]
-        for input, output in zip(sample_data, expected_unit):
-            self.ca.set_pv_value("LKSH460_01:FIELD:MULTIPLIER", input)
-            self.ca.assert_that_pv_is("LKSH460_01:FIELDREADING.EGU", output)
 
 
 
