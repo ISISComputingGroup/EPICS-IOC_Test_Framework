732b4eedf72603ef77e93c8f066c0c7662b881b0
AdrianPotter <adrian.potter@tessella.com>
2017-09-25 11:30:45 +0100

    Quotes not needed

--- utils/lewis_launcher.py
+++ utils/lewis_launcher.py
@@ -138,7 +138,7 @@ class LewisLauncher(object):
         lewis_command_line = [self._python_path, os.path.join(self._lewis_path, "lewis.exe"),
                               "-r", "127.0.0.1:{control_port}".format(control_port=self._control_port)]
         if self._lewis_protocol is not None:
-            lewis_command_line.extend(["-p", "\"{protocol}: {bind_address: 127.0.0.1, port: {port}}\""
+            lewis_command_line.extend(["-p", "{protocol}: {{bind_address: 127.0.0.1, port: {port}}}"
                                       .format(protocol=self._lewis_protocol, port=self.port)])
         if self._lewis_additional_path is not None:
             lewis_command_line.extend(["-a", self._lewis_additional_path])
