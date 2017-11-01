980f2292a3dc9f289b065ea53fa0fd86c4b0c005
Adrian Potter <Adrian.Potter@tessella.com>
2017-11-01 11:10:17 +0000

    Don't wait for values we know shouldn't be achieved

--- tests/oscillating_collimator.py
+++ tests/oscillating_collimator.py
@@ -65,27 +65,27 @@ class Oscillating_collimatorTests(unittest.TestCase):
             self.ca.assert_that_pv_is_number("VEL:SP", outputs[key][1], tolerance)
 
     def test_WHEN_angle_set_negative_THEN_angle_is_zero(self):
-        self.ca.set_pv_value(ANGLE, -1.0)
+        self.ca.set_pv_value(ANGLE, -1.0, wait=False)
         self.ca.assert_that_pv_is_number(ANGLE, 0.0)
 
     def test_WHEN_angle_set_greater_than_two_THEN_angle_is_two(self):
-        self.ca.set_pv_value(ANGLE, 5.0)
+        self.ca.set_pv_value(ANGLE, 5.0, wait=False)
         self.ca.assert_that_pv_is_number(ANGLE, 2.0)
 
     def test_WHEN_frequency_set_negative_THEN_angle_is_zero(self):
-        self.ca.set_pv_value(FREQUENCY, -1.0)
+        self.ca.set_pv_value(FREQUENCY, -1.0, wait=False)
         self.ca.assert_that_pv_is_number(FREQUENCY, 0.0)
 
     def test_WHEN_angle_set_greater_than_half_THEN_angle_is_half(self):
-        self.ca.set_pv_value(FREQUENCY, 1.0)
+        self.ca.set_pv_value(FREQUENCY, 1.0, wait=False)
         self.ca.assert_that_pv_is_number(FREQUENCY, 0.5)
 
     def test_WHEN_frq_set_greater_than_two_THEN_angle_is_two(self):
-        self.ca.set_pv_value(ANGLE, 5.0)
+        self.ca.set_pv_value(ANGLE, 5.0, wait=False)
         self.ca.assert_that_pv_is_number(ANGLE, 2.0)
 
     def test_WHEN_mounting_radius_set_negative_THEN_mounting_radius_is_zero(self):
-        self.ca.set_pv_value(RADIUS, -1.0)
+        self.ca.set_pv_value(RADIUS, -1.0, wait=False)
         self.ca.assert_that_pv_is_number(RADIUS, 0.0)
 
     def test_WHEN_input_values_cause_discriminant_to_be_negative_THEN_discriminant_pv_is_one(self):
