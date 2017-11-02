e05be28250a414160d8338b8fb99323280e8b763
Adrian Potter <Adrian.Potter@tessella.com>
2017-11-02 17:04:13 +0000

    Use auto pv prefix

--- tests/kepco.py
+++ tests/kepco.py
@@ -27,8 +27,8 @@ class KepcoTests(unittest.TestCase):
 
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("kepco")
-        self.ca = ChannelAccess(default_timeout=30)
-        self.ca.wait_for("KEPCO_01:VOLTAGE", timeout=30)
+        self.ca = ChannelAccess(default_timeout=30, device_prefix="KEPCO_01")
+        self.ca.wait_for("VOLTAGE", timeout=30)
 
     def _write_voltage(self, expected_voltage):
         self._lewis.backdoor_set_on_device("voltage", expected_voltage)
@@ -52,45 +52,45 @@ class KepcoTests(unittest.TestCase):
     def test_GIVEN_voltage_set_WHEN_read_THEN_voltage_is_as_expected(self):
         expected_voltage = 1.2
         self._write_voltage(expected_voltage)
-        self.ca.assert_that_pv_is("KEPCO_01:VOLTAGE", expected_voltage)
+        self.ca.assert_that_pv_is("VOLTAGE", expected_voltage)
 
     def test_GIVEN_current_set_WHEN_read_THEN_current_is_as_expected(self):
         expected_current = 1.5
         self._write_current(expected_current)
-        self.ca.assert_that_pv_is("KEPCO_01:CURRENT", expected_current)
+        self.ca.assert_that_pv_is("CURRENT", expected_current)
 
     def test_GIVEN_setpoint_voltage_set_WHEN_read_THEN_setpoint_voltage_is_as_expected(self):
         # Get current Voltage
-        current_voltage = self.ca.get_pv_value("KEPCO_01:VOLTAGE")
+        current_voltage = self.ca.get_pv_value("VOLTAGE")
         # Set new Voltage via SP
-        self.ca.set_pv_value("KEPCO_01:VOLTAGE:SP", current_voltage + 5.0)
+        self.ca.set_pv_value("VOLTAGE:SP", current_voltage + 5.0)
         # Check SP RBV matches new current
-        self.ca.assert_that_pv_is("KEPCO_01:VOLTAGE:SP:RBV", current_voltage + 5.0)
+        self.ca.assert_that_pv_is("VOLTAGE:SP:RBV", current_voltage + 5.0)
 
     def test_GIVEN_setpoint_current_set_WHEN_read_THEN_setpoint_current_is_as_expected(self):
         # Get current current
-        current_current = self.ca.get_pv_value("KEPCO_01:CURRENT")
+        current_current = self.ca.get_pv_value("CURRENT")
         # Set new Current via SP
-        self.ca.set_pv_value("KEPCO_01:CURRENT:SP", current_current + 5.0)
+        self.ca.set_pv_value("CURRENT:SP", current_current + 5.0)
         # Check SP RBV matches new current
-        self.ca.assert_that_pv_is("KEPCO_01:CURRENT:SP:RBV", current_current + 5.0)
+        self.ca.assert_that_pv_is("CURRENT:SP:RBV", current_current + 5.0)
 
     def test_GIVEN_output_mode_set_WHEN_read_THEN_output_mode_is_as_expected(self):
         expected_output_mode_flag = UnitFlags.CURRENT
         expected_output_mode_str =  OutputMode.CURRENT
         self._set_output_mode(expected_output_mode_flag)
         # Check OUTPUT MODE matches new OUTPUT MODE
-        self.ca.assert_that_pv_is("KEPCO_01:OUTPUTMODE", expected_output_mode_str)
+        self.ca.assert_that_pv_is("OUTPUTMODE", expected_output_mode_str)
 
     def test_GIVEN_output_status_set_WHEN_read_THEN_output_STATUS_is_as_expected(self):
         expected_output_status_flag = UnitFlags.ON
         expected_output_status_str = Status.ON
-        self.ca.set_pv_value("KEPCO_01:OUTPUTSTATUS:SP", expected_output_status_flag, wait=False)
-        self.ca.assert_that_pv_is("KEPCO_01:OUTPUTSTATUS:SP:RBV", expected_output_status_str)
+        self.ca.set_pv_value("OUTPUTSTATUS:SP", expected_output_status_flag, wait=False)
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", expected_output_status_str)
 
     def test_GIVEN_idn_set_WHEN_read_THEN_idn_is_as_expected(self):
         expected_idn = "000000000000000000111111111111111111111"
         self._set_IDN(expected_idn)
         # Made Proc field force scan as IDN scan is passive
-        self.ca.set_pv_value("KEPCO_01:IDN.PROC", 1, wait=False)
-        self.ca.assert_that_pv_is("KEPCO_01:IDN", expected_idn)
+        self.ca.set_pv_value("IDN.PROC", 1, wait=False)
+        self.ca.assert_that_pv_is("IDN", expected_idn)
