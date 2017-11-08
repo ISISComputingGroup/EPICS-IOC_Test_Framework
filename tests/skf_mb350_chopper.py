4640aff6e27100d772e87312141f6dd1f1b9df4e
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-11-08 11:16:53 +0000

    Test setting rotator angle

--- tests/skf_mb350_chopper.py
+++ tests/skf_mb350_chopper.py
@@ -103,3 +103,8 @@ class Skf_mb350_chopperTests(unittest.TestCase):
         for angle in TEST_ANGLES:
             self._lewis.backdoor_set_on_device("rotator_angle", angle)
             self.ca.assert_that_pv_is_number("ANGLE:ROTATOR", angle, 0.01)
+
+    def test_WHEN_rotator_angle_is_set_via_pv_THEN_rotator_angle_pv_updates_with_the_angle_just_set(self):
+        for angle in TEST_ANGLES:
+            self.ca.set_pv_value("ANGLE:ROTATOR:SP", angle)
+            self.ca.assert_that_pv_is_number("ANGLE:ROTATOR", angle, 0.01)
