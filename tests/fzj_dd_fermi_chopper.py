14119cd35f817bd5e116f804ccb4305decb15bf1
David Keymer <david.keymer@stfc.ac.uk>
2017-11-15 14:17:12 +0000

    added tests

--- tests/fzj_dd_fermi_chopper.py
+++ tests/fzj_dd_fermi_chopper.py
@@ -24,7 +24,7 @@ class Fzj_dd_fermi_chopperTests(unittest.TestCase):
         # self._lewis.backdoor_set_on_device("address", ADDRESS)
 
     def _set_state(self, expected_state):
-        self._lewis.backdoor_set_on_device("magnetic_bearing_state", expected_state)
+        self._lewis.backdoor_set_on_device("magnetic_bearing_status", expected_state)
         self._ioc.set_simulated_value("SIM:MB:STATUS", expected_state)
 
     def test_GIVEN_magnetic_bearings_state_WHEN_read_THEN_state_is_as_expected(self):
@@ -33,3 +33,23 @@ class Fzj_dd_fermi_chopperTests(unittest.TestCase):
 
         self.ca.assert_that_pv_is("MB:STATUS", expected_state)
         self.ca.assert_pv_alarm_is("MB:STATUS", ChannelAccess.ALARM_NONE)
+
+    def test_GIVEN_reference_frequency_WHEN_read_all_status_THEN_value_is_as_expected(self):
+        expected_value = 50
+        self._set_all_status(reference_frequency=expected_value)
+
+        self.ca.assert_that_pv_is("FREQ:REF", expected_value)
+        self.ca.assert_pv_alarm_is("FREQ:REF", ChannelAccess.ALARM_NONE)
+
+    def test_GIVEN_frequency_setpoint_WHEN_read_all_status_THEN_value_is_as_expected(self):
+        expected_value = 60
+        self._set_all_status(frequency_setpoint=expected_value)
+
+        self.ca.assert_that_pv_is("FREQ:SP:RBV", expected_value)
+        self.ca.assert_pv_alarm_is("FREQ:SP:RBV", ChannelAccess.ALARM_NONE)
+
+    def _set_all_status(self, reference_frequency=10,frequency_setpoint=10):
+        self._lewis.backdoor_set_on_device("reference_frequency", reference_frequency)
+        self._lewis.backdoor_set_on_device("frequency_setpoint", frequency_setpoint)
+        self._ioc.set_simulated_value("SIM:FREQ:REF", reference_frequency)
+        self._ioc.set_simulated_value("SIM:FREQ:SP:RBV", frequency_setpoint)
