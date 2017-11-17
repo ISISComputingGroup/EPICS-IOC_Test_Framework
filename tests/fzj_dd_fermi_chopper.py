2822de2a4a50aac14eab0e83edfa392b03ae4a76
David Keymer <david.keymer@stfc.ac.uk>
2017-11-17 17:16:13 +0000

    added tests

--- tests/fzj_dd_fermi_chopper.py
+++ tests/fzj_dd_fermi_chopper.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import skipIf
 
+import datetime
+
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import IOCRegister
 from utils.testing import get_running_lewis_and_ioc
@@ -27,32 +29,32 @@ class Fzj_dd_fermi_chopperTests(unittest.TestCase):
         self._lewis.backdoor_set_on_device("magnetic_bearing_status", expected_value)
         self._ioc.set_simulated_value("SIM:MB:STATUS", expected_value)
 
-    def _set_all_status(self, reference_frequency=10, frequency_setpoint=10, frequency=10, phase_setpoint=10,
-                        phase=10, phase_status="NOK"):
-        self._lewis.backdoor_set_on_device("reference_frequency", reference_frequency)
+
+    def _set_all_status(self, frequency_reference=10, frequency_setpoint=10, frequency=10, phase_setpoint=10,
+                        phase=10, phase_status="NOK", magnetic_bearing="OFF", magnetic_bearing_status="NOK"):
+        start = datetime.datetime.now()
+        self._lewis.backdoor_set_on_device("frequency_reference", frequency_reference)
         self._lewis.backdoor_set_on_device("frequency_setpoint", frequency_setpoint)
         self._lewis.backdoor_set_on_device("frequency", frequency)
         self._lewis.backdoor_set_on_device("phase_setpoint", phase_setpoint)
         self._lewis.backdoor_set_on_device("phase", phase)
         self._lewis.backdoor_set_on_device("phase_status", phase_status)
+        self._lewis.backdoor_set_on_device("magnetic_bearing", magnetic_bearing)
+        self._lewis.backdoor_set_on_device("magnetic_bearing_status", magnetic_bearing_status)
 
-        self._ioc.set_simulated_value("SIM:FREQ:REF", reference_frequency)
+        self._ioc.set_simulated_value("SIM:FREQ:REF", frequency_reference)
         self._ioc.set_simulated_value("SIM:FREQ:SP:RBV", frequency_setpoint)
         self._ioc.set_simulated_value("SIM:FREQ", frequency)
         self._ioc.set_simulated_value("SIM:PHAS:SP:RBV", phase_setpoint)
         self._ioc.set_simulated_value("SIM:PHAS", phase)
         self._ioc.set_simulated_value("SIM:PHAS:STATUS", phase_status)
+        self._ioc.set_simulated_value("SIM:MB", magnetic_bearing)
+        self._ioc.set_simulated_value("SIM:MB:STATUS", magnetic_bearing_status)
+        print "elapsed {}".format(datetime.datetime.now() - start)
 
-    def test_GIVEN_magnetic_bearings_state_WHEN_read_THEN_state_is_as_expected(self):
-        expected_value = "ON"
-        self._set_value(expected_value)
-
-        self.ca.assert_that_pv_is("MB:STATUS", expected_value)
-        self.ca.assert_pv_alarm_is("MB:STATUS", ChannelAccess.ALARM_NONE)
-
-    def test_GIVEN_reference_frequency_WHEN_read_all_status_THEN_value_is_as_expected(self):
+    def test_GIVEN_frequency_reference_WHEN_read_all_status_THEN_value_is_as_expected(self):
         expected_value = 50
-        self._set_all_status(reference_frequency=expected_value)
+        self._set_all_status(frequency_reference=expected_value)
 
         self.ca.assert_that_pv_is("FREQ:REF", expected_value)
         self.ca.assert_pv_alarm_is("FREQ:REF", ChannelAccess.ALARM_NONE)
@@ -85,9 +87,23 @@ class Fzj_dd_fermi_chopperTests(unittest.TestCase):
         self.ca.assert_that_pv_is("PHAS", expected_value)
         self.ca.assert_pv_alarm_is("PHAS", ChannelAccess.ALARM_NONE)
 
-    def test_GIVEN_phase_status_WHEN_read_all_status_THEN_value_is_as_expected(self):
+    def test_GIVEN_phase_status_WHEN_read_all_status_THEN_status_is_as_expected(self):
         expected_value = "OK"
         self._set_all_status(phase_status=expected_value)
 
         self.ca.assert_that_pv_is("PHAS:STATUS", expected_value)
         self.ca.assert_pv_alarm_is("PHAS:STATUS", ChannelAccess.ALARM_NONE)
+
+    def test_GIVEN_magnetic_bearing_WHEN_read_all_status_THEN_status_is_as_expected(self):
+        expected_value = "ON"
+        self._set_all_status(magnetic_bearing=expected_value)
+
+        self.ca.assert_that_pv_is("MB", expected_value)
+        self.ca.assert_pv_alarm_is("MB", ChannelAccess.ALARM_NONE)
+
+    def test_GIVEN_magnetic_bearing_status_WHEN_read_all_status_THEN_status_is_as_expected(self):
+        expected_value = "OK"
+        self._set_all_status(magnetic_bearing_status=expected_value)
+
+        self.ca.assert_that_pv_is("MB:STATUS", expected_value)
+        self.ca.assert_pv_alarm_is("MB:STATUS", ChannelAccess.ALARM_NONE)
