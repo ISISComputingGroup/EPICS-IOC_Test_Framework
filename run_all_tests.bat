8e00b16d4c738a79c568b5ad67b25894ed32ccc4
John-Holt-Tessella <John.Holt@tessella.com>
2017-11-22 16:27:58 +0000

    Add tests for mb350

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -168,3 +168,16 @@ echo TESTING EUROTHRM Dev Sim
 call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX%  -d eurotherm -p %EPICS_KIT_ROOT%\ioc\master\EUROTHRM\iocBoot\iocEUROTHRM-IOC-01 -e %PYTHONDIR%\Scripts -ea %EPICS_KIT_ROOT%\support\DeviceEmulator\master -ek lewis_emulators
 echo ---------------------------------------
 echo;
+
+echo ---------------------------------------
+echo TESTING SKF MB350 Dev Sim
+call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX%  -d skf_mb350_chopper -p %EPICS_KIT_ROOT%\ioc\master\SKFMB350\iocBoot\iocSKFMB350-IOC-01 -e %PYTHONDIR%\Scripts -ea %EPICS_KIT_ROOT%\support\DeviceEmulator\master -ek lewis_emulators
+echo ---------------------------------------
+echo;
+
+echo ---------------------------------------
+echo TESTING SKF MB350 Rec Sim
+call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d skf_mb350_chopper -p %EPICS_KIT_ROOT%\ioc\master\SKFMB350\iocBoot\iocSKFMB350-IOC-01
+echo ---------------------------------------
+echo;
+ 
