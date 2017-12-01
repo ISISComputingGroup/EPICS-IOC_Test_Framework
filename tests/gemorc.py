b859fe0baf57ea5d579384e8450d66efa44853a4
Adrian Potter <Adrian.Potter@tessella.com>
2017-12-01 17:06:04 +0000

    Add lots of tests

--- tests/gemorc.py
+++ tests/gemorc.py
@@ -11,8 +11,7 @@ class GemorcTests(unittest.TestCase):
     Tests for the Gemorc IOC.
     """
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Gemorc")
+        self._lewis, self._ioc = get_running_lewis_and_ioc("gemorc")
         self.ca = ChannelAccess(device_prefix="GEMORC_01")
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+    def test_WHEN_status_requested_THEN_not_blank
