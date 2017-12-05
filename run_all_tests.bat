c77a1dea9871aad1fbafa4968763db2f0870c413
Adrian Potter <Adrian.Potter@tessella.com>
2017-12-05 12:48:43 +0000

    Add tests for optional init state

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -10,9 +10,9 @@ echo TESTING GEMORC Dev Sim
 call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX% -d gemorc -p %EPICS_KIT_ROOT%\ioc\master\GEMORC\iocBoot\iocGEMORC-IOC-01 -e %PYTHONDIR%\Scripts -ea %EPICS_KIT_ROOT%\support\DeviceEmulator\master -ek lewis_emulators
 echo ---------------------------------------
 echo;
-
-echo ---------------------------------------
-echo TESTING GEMORC Rec Sim
-call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d gemorc -p %EPICS_KIT_ROOT%\ioc\master\GEMORC\iocBoot\iocGEMORC-IOC-01
-echo ---------------------------------------
-echo;
+REM 
+REM echo ---------------------------------------
+REM echo TESTING GEMORC Rec Sim
+REM call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d gemorc -p %EPICS_KIT_ROOT%\ioc\master\GEMORC\iocBoot\iocGEMORC-IOC-01
+REM echo ---------------------------------------
+REM echo;
