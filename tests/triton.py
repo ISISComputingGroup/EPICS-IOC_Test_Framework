3850c5a42880bab54db3c7fa4a2fe2f2e87978c7
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-12-19 12:41:14 +0000

    PID controls working

--- tests/triton.py
+++ tests/triton.py
@@ -22,3 +22,12 @@ class TritonTests(unittest.TestCase):
     @skipIf(IOCRegister.uses_rec_sim, "Not implemented in recsim")
     def test_WHEN_device_is_started_THEN_can_get_mixing_chamber_uid(self):
         self.ca.assert_that_pv_is("MC:UID", "mix_chamber_name")
+
+    def test_WHEN_P_setpoint_is_set_THEN_readback_updates(self):
+        self.ca.assert_setting_setpoint_sets_readback(5, "P")
+
+    def test_WHEN_I_setpoint_is_set_THEN_readback_updates(self):
+        self.ca.assert_setting_setpoint_sets_readback(5, "I")
+
+    def test_WHEN_D_setpoint_is_set_THEN_readback_updates(self):
+        self.ca.assert_setting_setpoint_sets_readback(5, "D")
