520ce6d46d3b9d82d4c05b8cb42d5e8f58693ca8
Adrian Potter <Adrian.Potter@tessella.com>
2018-02-07 13:26:00 +0000

    Add device to test framework

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -246,3 +246,15 @@ echo TESTING SKF G5 Chopper Rec Sim
 call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX% -r -d skf_g5_chopper -p %EPICS_KIT_ROOT%\ioc\master\SKFCHOPPER\iocBoot\iocSKFCHOPPER-IOC-01
 echo ---------------------------------------
 echo;
+
+echo ---------------------------------------
+echo TESTING ILM200 Dev Sim
+call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -pf %MYPVPREFIX% -d Ilm200 -p %EPICS_KIT_ROOT%\ioc\master\ILM200\iocBoot\iocILM200-IOC-01 -e %PYTHONDIR%\Scripts -ea %EPICS_KIT_ROOT%\support\DeviceEmulator\master -ek lewis_emulators
+echo ---------------------------------------
+echo;
+
+echo ---------------------------------------
+echo TESTING ILM200 Rec Sim
+call %PYTHON% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" -r -pf %MYPVPREFIX%  -d Ilm200 -p %EPICS_KIT_ROOT%\ioc\master\ILM200\iocBoot\iocILM200-IOC-01
+echo ---------------------------------------
+echo;
