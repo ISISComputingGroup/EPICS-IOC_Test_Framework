e6a20620751a809f285821514625ccc92ca7cc00
Adrian Potter <Adrian.Potter@tessella.com>
2018-02-12 12:14:15 +0000

    Fix hook up between system components

--- tests/ilm200.py
+++ tests/ilm200.py
@@ -11,8 +11,9 @@ class Ilm200Tests(unittest.TestCase):
     Tests for the Ilm200 IOC.
     """
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Ilm200")
+        self._lewis, self._ioc = get_running_lewis_and_ioc("ilm200")
         self.ca = ChannelAccess(device_prefix="ILM200_01")
+        self.ca.wait_for("VERSION", timeout=30)
 
     def test_that_fails(self):
         self.fail("You haven't implemented any tests!")
