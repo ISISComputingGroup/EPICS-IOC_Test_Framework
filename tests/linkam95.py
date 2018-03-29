0b05eb7b122a5ad547d655aa0a6f48b45eb6ef9b
John-Holt-Tessella <John.Holt@tessella.com>
2018-03-29 17:02:01 +0100

    Add tests for linkam95

--- tests/linkam95.py
+++ tests/linkam95.py
@@ -8,18 +8,18 @@ from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
 
 DEVICE_PREFIX = "LINKAM95_01"
 
-
+EMULATOR_NAME = "linkam_t95"
 IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("LINKAM95"),
         "macros": {},
-        "emulator": "Linkam95",
+        "emulator": EMULATOR_NAME,
+        "emulator_package": None
     },
 ]
 
-
-TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+TEST_MODES = [TestModes.DEVSIM]
 
 
 class Linkam95Tests(unittest.TestCase):
@@ -27,8 +27,13 @@ class Linkam95Tests(unittest.TestCase):
     Tests for the Linkam95 IOC.
     """
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Linkam95", DEVICE_PREFIX)
+        self._lewis, self._ioc = get_running_lewis_and_ioc(EMULATOR_NAME, DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+    def test_GIVEN_a_valid_temperature_to_set_WHEN_set_THEN_display_temperature_is_valid_temperature(self):
+
+        expected_temp = 10
+
+        self._lewis.backdoor_set_on_device("temperature", expected_temp)
+
+        self.ca.assert_that_pv_is("TEMP", expected_temp)
