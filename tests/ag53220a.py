ed8e1cbdb9f688df80ed1d94c0232d16b3a12474
Dominic Oram <dominic.oram@stfc.ac.uk>
2018-03-29 16:12:50 +0100

    Changed to proper name

--- tests/ag53220a.py
+++ tests/ag53220a.py
@@ -6,28 +6,27 @@ from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
 
 
-DEVICE_PREFIX = "AG5322A_01"
+DEVICE_PREFIX = "AG53220A_01"
 
 
 IOCS = [
     {
         "name": DEVICE_PREFIX,
-        "directory": get_default_ioc_dir("AG5322A"),
-        "macros": {},
-        "emulator": "Ag5322A",
+        "directory": get_default_ioc_dir("AG53220A"),
+        "emulator": "Ag53220A",
     },
 ]
 
 
-TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+TEST_MODES = [TestModes.RECSIM]
 
 
-class Ag5322ATests(unittest.TestCase):
+class Ag53220ATests(unittest.TestCase):
     """
-    Tests for the Ag5322A IOC.
+    Tests for the Ag53220A IOC.
     """
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Ag5322A", DEVICE_PREFIX)
+        self._lewis, self._ioc = get_running_lewis_and_ioc("Ag53220A", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
     def test_that_fails(self):
