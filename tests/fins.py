90aa172442f792e7c0759a42214b11422738a0b2
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2018-03-29 10:53:23 +0100

    Add device to test framework

---
+++ tests/fins.py
@@ -0,0 +1,34 @@
+import unittest
+
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+
+
+DEVICE_PREFIX = "FINS_01"
+
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("FINS"),
+        "macros": {},
+        "emulator": "Fins",
+    },
+]
+
+
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+
+
+class FinsTests(unittest.TestCase):
+    """
+    Tests for the Fins IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc("Fins", DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+
+    def test_that_fails(self):
+        self.fail("You haven't implemented any tests!")
