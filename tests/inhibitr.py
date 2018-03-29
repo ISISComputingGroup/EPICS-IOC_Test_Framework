b1bce99091238493e660c40faa4bb0c715b8a92a
esouthren <eilidhsouthren@gmail.com>
2018-03-29 18:14:16 +0100

    added and/or test

--- tests/inhibitr.py
+++ tests/inhibitr.py
@@ -1,34 +1,61 @@
 import unittest
 
 from utils.channel_access import ChannelAccess
-from utils.ioc_launcher import get_default_ioc_dir
+from utils.ioc_launcher import get_default_ioc_dir, IOCRegister, EPICS_TOP
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+import os
 
 
-DEVICE_PREFIX = "INHIBITR_01"
+IOC_PREFIX = "INHIBITR_01"
 
+SIMPLE_VALUE_ONE = "TE:NDW1801:SIMPLE:VALUE1:SP"
+SIMPLE_VALUE_TWO = "TE:NDW1801:SIMPLE:VALUE2:SP"
 
 IOCS = [
     {
-        "name": DEVICE_PREFIX,
+        "name": IOC_PREFIX,
         "directory": get_default_ioc_dir("INHIBITR"),
+        "macros": {
+            "PVONE": SIMPLE_VALUE_ONE,
+            "PVTWO": SIMPLE_VALUE_TWO,
+            "PVONE_DISP": "{}.DISP".format(SIMPLE_VALUE_ONE),
+            "PVTWO_DISP": "{}.DISP".format(SIMPLE_VALUE_TWO),
+        },
+    },
+    {
+        "name": "SIMPLE",
+        "directory": os.path.join(EPICS_TOP, "ISIS", "SimpleIoc", "master", "iocBoot", "iocsimple"),
         "macros": {},
-        "emulator": "Inhibitr",
     },
 ]
 
 
-TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+TEST_MODES = [TestModes.RECSIM]
 
 
 class InhibitrTests(unittest.TestCase):
     """
     Tests for the Inhibitr IOC.
     """
-    def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Inhibitr", DEVICE_PREFIX)
-        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+    def setUp(self):
+        self._ioc = IOCRegister.get_running(IOC_PREFIX)
+        self.ca = ChannelAccess(20)
+        self.values = ["SIMPLE:VALUE1:SP", "SIMPLE:VALUE2:SP"]
+
+    def reset_values_to_zero(self):
+        for val in self.values:
+            self.ca.set_pv_value(val, 0)
+
+    def test_GIVEN_both_inputs_are_zero_WHEN_setting_either_input_THEN_this_is_allowed(self):
+        self.reset_values_to_zero()
+        for val in self.values:
+            self.ca.assert_that_pv_is("{}.DISP".format(val), "0")
+
+    def test_GIVEN_one_input_is_one_WHEN_setting_other_value_to_one_THEN_this_is_not_allowed(self):
+        self.reset_values_to_zero()
+        self.ca.set_pv_value("SIMPLE:VALUE1:SP", 1)
+        # When value1 is set to zero, the disallowed value of value2 should be 1
+        # i.e 'Not allowed to set this value to 1'
+        self.ca.assert_that_pv_is("SIMPLE:VALUE2:SP.DISP", "1")
