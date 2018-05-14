7297ba93d59390e1f8549eb09d45641681fec475
esouthren <eilidhsouthren@gmail.com>
2018-05-14 10:35:36 +0100

    Updated test framework

--- tests/hfmagpsu.py
+++ tests/hfmagpsu.py
@@ -1,16 +1,33 @@
 import unittest
 from utils.channel_access import ChannelAccess
-from utils.testing import get_running_lewis_and_ioc
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+from utils.ioc_launcher import IOCRegister, get_default_ioc_dir
 from datetime import datetime, date, timedelta
 
+
+DEVICE_PREFIX = "HFMAGPSU_01"
+
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("HFMAGPSU"),
+        "macros": {},
+        "emulator": "HFMAGPSU"
+    },
+]
+
+#TEST_MODES = [TestModes.DEVSIM, TestModes.RECSIM]
+TEST_MODES = [TestModes.DEVSIM]
 
 class HfmagpsuTests(unittest.TestCase):
 
 
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("HFMAGPSU") # emulator name
-        self.ca = ChannelAccess(default_timeout=10)
-        self.ca.wait_for("HFMAGPSU_01:DIRECTION", timeout=10)
+        self._lewis, self._ioc = get_running_lewis_and_ioc("HFMAGPSU", DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_timeout=30)
+        self.ca.wait_for("DIRECTION")
 
     ''' Retrieve time from the log message PV and parse to a datetime object '''
 
@@ -21,92 +38,92 @@ class HfmagpsuTests(unittest.TestCase):
 
     def test_GIVEN_midTarget_set_WHEN_read_THEN_midTarget_is_as_expected(self):
         test_value = 2.325
-        self.ca.set_pv_value("HFMAGPSU_01:MID:SP", test_value)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:MID", test_value)
+        self.ca.set_pv_value("MID:SP", test_value)
+        self.ca.assert_that_pv_is("MID", test_value)
 
     def test_GIVEN_rampRate_set_WHEN_read_THEN_rampRate_is_as_expected(self):
         test_value = 0.51
-        self.ca.set_pv_value("HFMAGPSU_01:RAMPRATE:SP", test_value)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:RAMPRATE", test_value)
+        self.ca.set_pv_value("RAMPRATE:SP", test_value)
+        self.ca.assert_that_pv_is("RAMPRATE", test_value)
 
     def test_GIVEN_maxTarget_set_WHEN_read_THEN_maxTarget_is_as_expected(self):
         test_value = 4.0
-        self.ca.set_pv_value("HFMAGPSU_01:MAX:SP", test_value)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:MAX", test_value)
+        self.ca.set_pv_value("MAX:SP", test_value)
+        self.ca.assert_that_pv_is("MAX", test_value)
 
     def test_GIVEN_limit_set_WHEN_read_THEN_limit_is_as_expected(self):
         test_value = 35.5
-        self.ca.set_pv_value("HFMAGPSU_01:LIMIT:SP", test_value)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:LIMIT", test_value)
+        self.ca.set_pv_value("LIMIT:SP", test_value)
+        self.ca.assert_that_pv_is("LIMIT", test_value)
 
     def test_GIVEN_constant_set_WHEN_read_THEN_constant_is_as_expected(self):
         test_value = 0.0032
-        self.ca.set_pv_value("HFMAGPSU_01:CONSTANT:SP", test_value)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:CONSTANT", test_value)
+        self.ca.set_pv_value("CONSTANT:SP", test_value)
+        self.ca.assert_that_pv_is("CONSTANT", test_value)
 
     def test_GIVEN_heaterValue_set_WHEN_read_THEN_heaterValue_is_as_expected(self):
         test_value = 15.5
-        self.ca.set_pv_value("HFMAGPSU_01:HEATERVALUE:SP", test_value)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:HEATERVALUE", test_value)
+        self.ca.set_pv_value("HEATERVALUE:SP", test_value)
+        self.ca.assert_that_pv_is("HEATERVALUE", test_value)
 
     def test_GIVEN_pause_set_ON_WHEN_read_THEN_pause_is_as_expected(self):
         expected_output = 'ON'
-        self.ca.set_pv_value("HFMAGPSU_01:PAUSE:SP", 1)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:PAUSE", expected_output)
+        self.ca.set_pv_value("PAUSE:SP", 1)
+        self.ca.assert_that_pv_is("PAUSE", expected_output)
 
     def test_GIVEN_pause_set_OFF_WHEN_read_THEN_pause_is_as_expected(self):
         expected_output = 'OFF'
-        self.ca.set_pv_value("HFMAGPSU_01:PAUSE:SP", 0)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:PAUSE", expected_output)
+        self.ca.set_pv_value("PAUSE:SP", 0)
+        self.ca.assert_that_pv_is("PAUSE", expected_output)
 
     def test_GIVEN_outputMode_set_AMPS_WHEN_read_THEN_outputMode_is_as_expected(self):
         test_value = 'AMPS'
-        self.ca.set_pv_value("HFMAGPSU_01:OUTPUTMODE:SP", 0)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:OUTPUTMODE", test_value)
+        self.ca.set_pv_value("OUTPUTMODE:SP", 0)
+        self.ca.assert_that_pv_is("OUTPUTMODE", test_value)
 
     def test_GIVEN_outputMode_set_TESLA_WHEN_read_THEN_outputMode_is_as_expected(self):
         test_value = 'TESLA'
-        self.ca.set_pv_value("HFMAGPSU_01:OUTPUTMODE:SP", 1)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:OUTPUTMODE", test_value)
+        self.ca.set_pv_value("OUTPUTMODE:SP", 1)
+        self.ca.assert_that_pv_is("OUTPUTMODE", test_value)
 
     def test_GIVEN_heaterStatus_set_ON_WHEN_read_THEN_heaterStatus_is_as_expected(self):
         expected_heaterStatus = 'ON'
-        self.ca.set_pv_value("HFMAGPSU_01:HEATERSTATUS:SP", 1)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:HEATERSTATUS", expected_heaterStatus)
+        self.ca.set_pv_value("HEATERSTATUS:SP", 1)
+        self.ca.assert_that_pv_is("HEATERSTATUS", expected_heaterStatus)
 
     def test_GIVEN_heaterStatus_set_OFF_WHEN_read_THEN_heaterStatus_is_as_expected(self):
         expected_heaterStatus = 'OFF'
-        self.ca.set_pv_value("HFMAGPSU_01:HEATERSTATUS:SP", 0)
-        self.ca.assert_that_pv_is("HFMAGPSU_01:HEATERSTATUS", expected_heaterStatus)
+        self.ca.set_pv_value("HEATERSTATUS:SP", 0)
+        self.ca.assert_that_pv_is("HEATERSTATUS", expected_heaterStatus)
 
     def test_GIVEN_rampTarget_set_WHEN_read_THEN_rampTarget_is_as_expected(self):
         ramp_targets = ['ZERO', 'MID', 'MAX']
         for index, value in enumerate(ramp_targets):
-            self.ca.set_pv_value("HFMAGPSU_01:RAMPTARGET:SP", index)
+            self.ca.set_pv_value("RAMPTARGET:SP", index)
             expected_target = value
-            self.ca.assert_that_pv_is("HFMAGPSU_01:RAMPTARGET", expected_target)
+            self.ca.assert_that_pv_is("RAMPTARGET", expected_target)
 
     def test_GIVEN_direction_set_WHEN_read_THEN_direction_is_as_expected(self):
         directions = ["0", "-", "+"]
         for index, value in enumerate(directions):
-            self.ca.set_pv_value("HFMAGPSU_01:DIRECTION:SP", index)
+            self.ca.set_pv_value("DIRECTION:SP", index)
             expected_direction = value
-            self.ca.assert_that_pv_is("HFMAGPSU_01:DIRECTION", expected_direction)
-    '''
-     As the log message contains a timestamp and the setpoint/test timestamps will differ
-     by a few seconds, we're testing to see if the timestamp difference is within 4 seconds.
-    '''
+            self.ca.assert_that_pv_is("DIRECTION", expected_direction)
+    
+        #As the log message contains a timestamp and the setpoint/test timestamps will differ
+        #by a few seconds, we're testing to see if the timestamp difference is within 4 seconds.
+
     def test_GIVEN_outputmode_set_WHEN_read_then_logmessage_is_as_expected(self):
-        self.ca.set_pv_value("HFMAGPSU_01:OUTPUTMODE:SP", 0)
+        self.ca.set_pv_value("OUTPUTMODE:SP", 0)
         current_time = datetime.now().replace(year=1900, month=01, day=01)
-        log_message_time = self.get_log_message_time(self.ca.get_pv_value("HFMAGPSU_01:LOGMESSAGE"))
+        log_message_time = self.get_log_message_time(self.ca.get_pv_value("LOGMESSAGE"))
         time_difference = current_time - log_message_time
         self.assertLess(time_difference.seconds, 4)
 
     def test_GIVEN_limit_set_WHEN_read_then_logmessage_is_as_expected(self):
         test_value = 33.4
-        self.ca.set_pv_value("HFMAGPSU_01:LIMIT:SP", test_value)
+        self.ca.set_pv_value("LIMIT:SP", test_value)
         current_time = datetime.now().replace(year=1900, month=01, day=01)
-        log_message_time = self.get_log_message_time(self.ca.get_pv_value("HFMAGPSU_01:LOGMESSAGE"))
+        log_message_time = self.get_log_message_time(self.ca.get_pv_value("LOGMESSAGE"))
         time_difference = current_time - log_message_time
         self.assertLess(time_difference.seconds, 4)
