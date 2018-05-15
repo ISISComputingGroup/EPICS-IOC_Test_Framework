f7b5ee05106bd160127e7203d5f22db0d70e916f
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-05-15 16:10:09 +0100

    Add more tests

--- tests/itc503.py
+++ tests/itc503.py
@@ -1,6 +1,8 @@
 import unittest
 
 import itertools
+from time import sleep
+
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
@@ -22,6 +24,19 @@ TEST_MODES = [TestModes.DEVSIM, TestModes.RECSIM]
 
 TEST_VALUES = 0, 23.45
 
+MODES = [
+    "Man heater, man flow",
+    "Auto heater, man flow",
+    "Man heater, auto flow",
+    "Auto gas, auto flow",
+]
+
+CHANNELS = [
+    "Channel 1",
+    "Channel 2",
+    "Channel 3",
+]
+
 
 class Itc503Tests(unittest.TestCase):
     """
@@ -30,7 +45,6 @@ class Itc503Tests(unittest.TestCase):
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("itc503", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_timeout=20)
-        self.ca.wait_for("SIM")
         self.ca.wait_for("DISABLE")
 
     def test_WHEN_ioc_is_started_THEN_it_is_not_disabled(self):
@@ -47,8 +61,8 @@ class Itc503Tests(unittest.TestCase):
             self.ca.assert_that_pv_is_number("GASFLOW", value, tolerance=0.1)  # Only comes back to 1dp
 
     def test_WHEN_setting_mode_THEN_can_be_read_back(self):
-        for value in ("Auto", "Manual", "Auto"):
-            self.ca.assert_setting_setpoint_sets_readback(value, "ACTIVITY")
+        for mode in MODES:
+            self.ca.assert_setting_setpoint_sets_readback(mode, "MODE")
 
     def test_WHEN_temperature_is_set_THEN_temperature_and_setpoint_readbacks_update_to_new_value(self):
         for value in TEST_VALUES:
@@ -57,9 +71,14 @@ class Itc503Tests(unittest.TestCase):
             self.ca.assert_that_pv_is_number("TEMP:SP:RBV", value, tolerance=0.1)
 
     @skip_if_recsim("Comes back via record redirection which recsim can't handle easily")
+    def test_WHEN_control_channel_is_set_THEN_control_channel_can_be_read_back(self):
+        for chan in CHANNELS:
+            self.ca.assert_setting_setpoint_sets_readback(chan, "CTRLCHANNEL")
+
+    @skip_if_recsim("Comes back via record redirection which recsim can't handle easily")
     def test_WHEN_setting_control_mode_THEN_can_be_read_back(self):
-        for chan in ("FPLock", "EPICS+FPLock", "FPUnlock", "EPICS+FPUnlock"):
-            self.ca.assert_setting_setpoint_sets_readback(chan, "CTRL")
+        for mode in ("FPLock", "EPICS+FPLock", "FPUnlock", "EPICS+FPUnlock"):
+            self.ca.assert_setting_setpoint_sets_readback(mode, "CTRL")
 
     @skip_if_recsim("Backdoor does not exist in recsim")
     def test_WHEN_sweeping_mode_is_set_via_backdoor_THEN_it_updates_in_the_ioc(self):
@@ -68,3 +87,23 @@ class Itc503Tests(unittest.TestCase):
 
         self._lewis.backdoor_set_on_device("sweeping", True)
         self.ca.assert_that_pv_is("SWEEPING", "Sweeping")
+
+    @skip_if_recsim("Comes back via record redirection which recsim can't handle easily")
+    def test_WHEN_setting_autopid_THEN_readback_reflects_setting_just_sent(self):
+        for state in ["ON", "OFF"]:
+            self.ca.assert_setting_setpoint_sets_readback(state, "AUTOPID")
+
+    @skip_if_recsim("Backdoor does not exist in recsim")
+    def test_WHEN_heater_voltage_is_set_THEN_heater_voltage_updates(self):
+        # Ensure the heater range is high enough to handle the maximum test value.
+        heater_max = max(TEST_VALUES)
+        self.ca.set_pv_value("HEATERV:MAX", heater_max)
+        sleep(0.5)  # Give it time to be set
+        self.assertTrue(abs(float(self._lewis.backdoor_get_from_device("heater_v_max")) - heater_max) <= 0.1)
+
+        for val in TEST_VALUES:
+            self.ca.set_pv_value("HEATERV:SP", val)
+            self.ca.assert_that_pv_is_number("HEATERV", val, tolerance=0.1)
+
+            # Emulator responds with heater p == heater v. Test that heater p is also reading.
+            self.ca.assert_that_pv_is_number("HEATERP", val, tolerance=0.1)
