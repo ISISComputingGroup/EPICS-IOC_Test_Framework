f0c12c9bdd17ad580fcfa8f864e7701917cbb600
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-05-30 11:56:56 +0100

    Updated units test.

--- tests/tpg300.py
+++ tests/tpg300.py
@@ -21,8 +21,11 @@ IOCS = [
 
 TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 
-
-UNITS = ["mbar", "Torr", "Pa"]
+UNITS = {
+    1: "mbar",
+    2: "Torr",
+    3: "Pa"
+}
 
 
 class Tpg300Tests(unittest.TestCase):
@@ -43,15 +46,16 @@ class Tpg300Tests(unittest.TestCase):
 
     def _set_units(self, unit):
         self._lewis.backdoor_set_on_device("units", unit)
-        self._ioc.set_simulated_value("SIM:UNITS", unit)
+        self._ioc.set_simulated_value("SIM:UNITS", UNITS[unit])
 
     def test_WHEN_ioc_started_THEN_ioc_is_not_disabled(self):
         self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
 
     def test_WHEN_units_are_set_THEN_unit_is_the_same_as_backdoor(self):
-        for unit in UNITS:
-            self._set_units(unit)
-            self.ca.assert_that_pv_is("UNITS", unit)
+        for unit_flag, unit_string in UNITS.items():
+            expected_unit = unit_string
+            self._set_units(unit_flag)
+            self.ca.assert_that_pv_is("UNITS", expected_unit)
 
     def test_GIVEN_floating_point_pressure_value_WHEN_pressure_a1_is_read_THEN_pressure_a1_value_is_same_as_backdoor(self):
         expected_pressure = 1.23
