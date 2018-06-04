29fd763693c4dd1d781a1e9c8d19d16035812b72
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-06-04 16:51:55 +0100

    Changed Units to use Enum class.

--- tests/tpg300.py
+++ tests/tpg300.py
@@ -4,6 +4,7 @@ from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+from enum import Enum
 
 
 DEVICE_PREFIX = "TPG300_01"
@@ -21,11 +22,11 @@ IOCS = [
 
 TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 
-UNITS = {
-    1: "mbar",
-    2: "Torr",
-    3: "Pa"
-}
+
+class Units(Enum):
+    mbar = 1
+    Torr = 2
+    Pa = 3
 
 CHANNELS = ("A1", "A2", "B1", "B2")
 
@@ -54,8 +55,8 @@ class Tpg300Tests(unittest.TestCase):
         self._ioc.set_simulated_value(pv, expected_pressure)
 
     def _set_units(self, unit):
-        self._lewis.backdoor_set_on_device("units", unit)
-        self._ioc.set_simulated_value("SIM:UNITS", UNITS[unit])
+        self._lewis.backdoor_set_on_device("units", unit.value)
+        self._ioc.set_simulated_value("SIM:UNITS", unit.name)
 
     def _set_connected(self, connected):
         self._lewis.backdoor_set_on_device("connected", connected)
@@ -64,9 +65,9 @@ class Tpg300Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
 
     def test_WHEN_units_are_set_THEN_unit_is_the_same_as_backdoor(self):
-        for unit_flag, unit_string in UNITS.items():
-            expected_unit = unit_string
-            self._set_units(unit_flag)
+        for unit in Units:
+            expected_unit = unit.name
+            self._set_units(unit)
             self.ca.assert_that_pv_is("UNITS", expected_unit)
 
     def test_GIVEN_floating_point_pressure_value_WHEN_pressure_is_read_THEN_pressure_value_is_same_as_backdoor(self):
