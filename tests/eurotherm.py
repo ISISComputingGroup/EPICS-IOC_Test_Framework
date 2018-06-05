116614084be50a1f693e2775ae392c3c7e2ccec2
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-06-05 13:51:21 +0100

    Modify tests

--- tests/eurotherm.py
+++ tests/eurotherm.py
@@ -104,102 +104,102 @@ class EurothermTests(unittest.TestCase):
             self._lewis.backdoor_set_on_device("current_temperature", temperature)
             self._lewis.backdoor_set_on_device("ramp_setpoint_temperature", temperature)
 
-    @skip_if_recsim("In rec sim this test fails")
-    def test_WHEN_read_rbv_temperature_THEN_rbv_value_is_same_as_backdoor(self):
-        expected_temperature = 10.0
-        self._set_setpoint_and_current_temperature(expected_temperature)
-        self.ca.assert_that_pv_is(RBV_PV, expected_temperature)
-
-    @skip_if_recsim("In rec sim this test fails")
-    def test_GIVEN_a_sp_WHEN_sp_read_rbv_temperature_THEN_rbv_value_is_same_as_sp(self):
-        expected_temperature = 10.0
-        self.ca.assert_setting_setpoint_sets_readback(expected_temperature, "SP:RBV", "SP")
-
-    @skip_if_recsim("In rec sim this test fails")
-    def test_WHEN_set_ramp_rate_in_K_per_min_THEN_current_temperature_reaches_set_point_in_expected_time(self):
-        start_temperature = 5.0
-        ramp_on = 1
-        ramp_rate = 60.0
-        setpoint_temperature = 25.0
-
-        self._set_setpoint_and_current_temperature(start_temperature)
-        self.ca.set_pv_value("TEMP:SP", start_temperature)
-
-        self.ca.set_pv_value("RATE:SP", ramp_rate)
-        self.ca.set_pv_value("RAMPON:SP", ramp_on)
-        self.ca.set_pv_value("TEMP:SP", setpoint_temperature)
-
-        start = time.time()
-        self.ca.assert_that_pv_is_number("TEMP:SP:RBV", setpoint_temperature, timeout=60)
-        end = time.time()
-        self.assertAlmostEquals(end-start, 20, delta=1)
-
-    @skip_if_recsim("In rec sim this test fails")
-    def test_WHEN_sensor_disconnected_THEN_ramp_setting_is_disabled(self):
-        self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
-
-        self.ca.assert_that_pv_is_number("RAMPON:SP.DISP", 1)
-
-    @skip_if_recsim("In rec sim this test fails")
-    def test_GIVEN_sensor_disconnected_WHEN_sensor_reconnected_THEN_ramp_setting_is_enabled(self):
-        self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
-
-        self._lewis.backdoor_set_on_device("current_temperature", 0)
-
-        self.ca.assert_that_pv_is_number("RAMPON:SP.DISP", 0)
-
-    @skip_if_recsim("In rec sim this test fails")
-    def test_GIVEN_ramp_was_off_WHEN_sensor_disconnected_THEN_ramp_is_off_and_cached_ramp_value_is_off(self):
-        self.ca.set_pv_value("RAMPON:SP", 0)
-
-        self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
-
-        self.ca.assert_that_pv_is("RAMPON", "OFF")
-        self.ca.assert_that_pv_is("RAMPON:CACHE", "OFF")
-
-    @skip_if_recsim("In rec sim this test fails")
-    def test_GIVEN_ramp_was_on_WHEN_sensor_disconnected_THEN_ramp_is_off_and_cached_ramp_value_is_on(self):
-        self.ca.set_pv_value("RAMPON:SP", 1)
-
-        self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
-
-        self.ca.assert_that_pv_is("RAMPON", "OFF")
-        self.ca.assert_that_pv_is("RAMPON:CACHE", "ON")
-
-    @skip_if_recsim("In rec sim this test fails")
-    def test_GIVEN_ramp_was_on_WHEN_sensor_disconnected_and_reconnected_THEN_ramp_is_on(self):
-        self.ca.set_pv_value("RAMPON:SP", 1)
-
-        self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
-        self.ca.assert_that_pv_is("RAMPON", "OFF")
-        self._lewis.backdoor_set_on_device("current_temperature", 0)
-
-        self.ca.assert_that_pv_is("RAMPON", "ON")
-
-    def test_GIVEN_temperature_setpoint_followed_by_calibration_change_WHEN_same_setpoint_set_again_THEN_setpoint_readback_updates_to_set_value(self):
-
-        # Arrange
-        temperature = 50.0
-        rbv_change_timeout = 10
-        tolerance = 0.01
-        self.ca.set_pv_value("RAMPON:SP", 0)
-        self._reset_calibration_file()
-        self.ca.set_pv_value("TEMP:SP", temperature)
-        self.ca.assert_that_pv_is_number("TEMP:SP:RBV", temperature, tolerance=tolerance, timeout=rbv_change_timeout)
-        self._set_calibration_file("C006.txt")
-        self.ca.assert_that_pv_is_not_number("TEMP:SP:RBV", temperature, tolerance=tolerance, timeout=rbv_change_timeout)
-
-        # Act
-        self.ca.set_pv_value("TEMP:SP", temperature)
-
-        # Assert
-        self.ca.assert_that_pv_is_number("TEMP:SP:RBV", temperature, tolerance=tolerance, timeout=rbv_change_timeout)
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_WHEN_read_rbv_temperature_THEN_rbv_value_is_same_as_backdoor(self):
+    #     expected_temperature = 10.0
+    #     self._set_setpoint_and_current_temperature(expected_temperature)
+    #     self.ca.assert_that_pv_is(RBV_PV, expected_temperature)
+    #
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_GIVEN_a_sp_WHEN_sp_read_rbv_temperature_THEN_rbv_value_is_same_as_sp(self):
+    #     expected_temperature = 10.0
+    #     self.ca.assert_setting_setpoint_sets_readback(expected_temperature, "SP:RBV", "SP")
+    #
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_WHEN_set_ramp_rate_in_K_per_min_THEN_current_temperature_reaches_set_point_in_expected_time(self):
+    #     start_temperature = 5.0
+    #     ramp_on = 1
+    #     ramp_rate = 60.0
+    #     setpoint_temperature = 25.0
+    #
+    #     self._set_setpoint_and_current_temperature(start_temperature)
+    #     self.ca.set_pv_value("TEMP:SP", start_temperature)
+    #
+    #     self.ca.set_pv_value("RATE:SP", ramp_rate)
+    #     self.ca.set_pv_value("RAMPON:SP", ramp_on)
+    #     self.ca.set_pv_value("TEMP:SP", setpoint_temperature)
+    #
+    #     start = time.time()
+    #     self.ca.assert_that_pv_is_number("TEMP:SP:RBV", setpoint_temperature, timeout=60)
+    #     end = time.time()
+    #     self.assertAlmostEquals(end-start, 20, delta=1)
+    #
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_WHEN_sensor_disconnected_THEN_ramp_setting_is_disabled(self):
+    #     self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
+    #
+    #     self.ca.assert_that_pv_is_number("RAMPON:SP.DISP", 1)
+    #
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_GIVEN_sensor_disconnected_WHEN_sensor_reconnected_THEN_ramp_setting_is_enabled(self):
+    #     self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
+    #
+    #     self._lewis.backdoor_set_on_device("current_temperature", 0)
+    #
+    #     self.ca.assert_that_pv_is_number("RAMPON:SP.DISP", 0)
+    #
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_GIVEN_ramp_was_off_WHEN_sensor_disconnected_THEN_ramp_is_off_and_cached_ramp_value_is_off(self):
+    #     self.ca.set_pv_value("RAMPON:SP", 0)
+    #
+    #     self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
+    #
+    #     self.ca.assert_that_pv_is("RAMPON", "OFF")
+    #     self.ca.assert_that_pv_is("RAMPON:CACHE", "OFF")
+    #
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_GIVEN_ramp_was_on_WHEN_sensor_disconnected_THEN_ramp_is_off_and_cached_ramp_value_is_on(self):
+    #     self.ca.set_pv_value("RAMPON:SP", 1)
+    #
+    #     self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
+    #
+    #     self.ca.assert_that_pv_is("RAMPON", "OFF")
+    #     self.ca.assert_that_pv_is("RAMPON:CACHE", "ON")
+    #
+    # @skip_if_recsim("In rec sim this test fails")
+    # def test_GIVEN_ramp_was_on_WHEN_sensor_disconnected_and_reconnected_THEN_ramp_is_on(self):
+    #     self.ca.set_pv_value("RAMPON:SP", 1)
+    #
+    #     self._lewis.backdoor_set_on_device("current_temperature", SENSOR_DISCONNECTED_VALUE)
+    #     self.ca.assert_that_pv_is("RAMPON", "OFF")
+    #     self._lewis.backdoor_set_on_device("current_temperature", 0)
+    #
+    #     self.ca.assert_that_pv_is("RAMPON", "ON")
+    #
+    # def test_GIVEN_temperature_setpoint_followed_by_calibration_change_WHEN_same_setpoint_set_again_THEN_setpoint_readback_updates_to_set_value(self):
+    #
+    #     # Arrange
+    #     temperature = 50.0
+    #     rbv_change_timeout = 10
+    #     tolerance = 0.01
+    #     self.ca.set_pv_value("RAMPON:SP", 0)
+    #     self._reset_calibration_file()
+    #     self.ca.set_pv_value("TEMP:SP", temperature)
+    #     self.ca.assert_that_pv_is_number("TEMP:SP:RBV", temperature, tolerance=tolerance, timeout=rbv_change_timeout)
+    #     self._set_calibration_file("C006.txt")
+    #     self.ca.assert_that_pv_is_not_number("TEMP:SP:RBV", temperature, tolerance=tolerance, timeout=rbv_change_timeout)
+    #
+    #     # Act
+    #     self.ca.set_pv_value("TEMP:SP", temperature)
+    #
+    #     # Assert
+    #     self.ca.assert_that_pv_is_number("TEMP:SP:RBV", temperature, tolerance=tolerance, timeout=rbv_change_timeout)
 
     def _assert_units(self, units):
-        self.ca.assert_that_pv_is("UNITS", units)
-        self.ca.assert_that_pv_is("TEMP.EGU", units)
-        self.ca.assert_that_pv_is("TEMP:SP.EGU", units)
-        self.ca.assert_that_pv_is("TEMP:SP:RBV.EGU", units)
+        # High timeouts because setting units does not cause processing - wait for normal scan loop to come around.
+        self.ca.assert_that_pv_is("TEMP.EGU", units, timeout=20)
+        self.ca.assert_that_pv_is("TEMP:SP.EGU", units, timeout=20)
+        self.ca.assert_that_pv_is("TEMP:SP:RBV.EGU", units, timeout=20)
 
     def _assert_using_mock_table_location(self):
         for pv in ["TEMP", "TEMP:SP:CONV", "TEMP:SP:RBV:CONV"]:
