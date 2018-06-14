6f6ef83c77e78b203eff25b7541a9c2ba6b0acc4
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-06-14 15:25:44 +0100

    Remove unneeded import

--- common_tests/danfysik.py
+++ common_tests/danfysik.py
@@ -1,7 +1,5 @@
 import unittest
 
-import time
-
 from utils.test_modes import TestModes
 from utils.channel_access import ChannelAccess
 from utils.testing import skip_if_recsim, get_running_lewis_and_ioc
