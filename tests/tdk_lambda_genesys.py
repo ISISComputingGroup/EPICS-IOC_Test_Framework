e27fd74c382a7663ba6e05489a4ded59821e85e7
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-06-14 11:57:47 +0100

    Test that IOC recovers from power supply being reinitialized

--- tests/tdk_lambda_genesys.py
+++ tests/tdk_lambda_genesys.py
@@ -81,3 +81,16 @@ class TdkLambdaGenesysTests(unittest.TestCase):
     def test_GIVEN_state_set_via_number_WHEN_read_THEN_state_is_as_expected(self):
         self.ca.set_pv_value("1:POWER:SP", 1)
         self.ca.assert_that_pv_is("1:POWER", "ON")
+
+    @skip_if_recsim("Recsim is unable to simulate comms being uninitialized")
+    def test_GIVEN_power_supply_comms_become_uninitialized_THEN_ioc_recovers(self):
+        try:
+            for curr in [0.123, 0.456]:
+                self._lewis.backdoor_set_on_device("comms_initialized", False)
+                self._lewis.backdoor_set_on_device("current", curr)
+                # Should be able to re-initialize comms and read the new current
+                self.ca.assert_that_pv_is_number("1:CURR", curr, tolerance=0.01, timeout=30)
+
+        finally:
+            # If test fails, don't want it to affect other tests.
+            self._lewis.backdoor_set_on_device("comms_initialized", True)
