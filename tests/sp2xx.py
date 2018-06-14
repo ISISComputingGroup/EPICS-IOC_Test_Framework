adec8151faa231b630afb2186f090cc25e2e910f
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-06-14 13:55:39 +0100

    Add device to test framework

---
+++ tests/sp2xx.py
@@ -0,0 +1,34 @@
+import unittest
+
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+
+
+DEVICE_PREFIX = "SP2XX_01"
+
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("SP2XX"),
+        "macros": {},
+        "emulator": "Sp2Xx",
+    },
+]
+
+
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+
+
+class Sp2XxTests(unittest.TestCase):
+    """
+    Tests for the Sp2Xx IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc("Sp2Xx", DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+
+    def test_that_fails(self):
+        self.fail("You haven't implemented any tests!")
