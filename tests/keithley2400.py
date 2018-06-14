339a1eb23b7f3594594ef63d2b4ed93dae6c564d
Alistair McGann <alistair.mcgann@tessella.com>
2018-06-14 14:39:05 +0100

    Tests now check to make sure the range bands and exponential notation works

--- tests/keithley2400.py
+++ tests/keithley2400.py
@@ -35,6 +35,11 @@ class Keithley2400Tests(unittest.TestCase):
 
         self.log.info("Message")
 
+    def calculate_resistance_range(self, value):
+        for r in [2.1 * pow(10, i) for i in range(1, 8)]:
+            if value < r:
+                return r / 10
+
     def test_WHEN_ioc_is_started_THEN_ioc_is_not_disabled(self):
         self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
 
@@ -54,9 +59,12 @@ class Keithley2400Tests(unittest.TestCase):
         for val in ["Manual", "Auto"]:
             self.ca.assert_setting_setpoint_sets_readback(val, "RES:RANGE:AUTO")
 
-    def test_WHEN_range_is_set_THEN_readback_updates_with_the_value_just_set(self):
-        for val in [1.23, 456.789]:
-            self.ca.assert_setting_setpoint_sets_readback(val, "RES:RANGE")
+    @skip_if_recsim("Banded record behaviour too complex for recsim")
+    def test_WHEN_range_is_set_THEN_readback_updates_with_appropriate_range_for_value_just_set(self):
+        for val in [1.23, 456.789, 1e-3]:
+            ideal_range = self.calculate_resistance_range(val)
+            self.ca.set_pv_value("RES:RANGE:SP", val)
+            self.ca.assert_that_pv_is_number("RES:RANGE:SP:RBV", ideal_range, tolerance=0.05 * ideal_range)
 
     def test_WHEN_source_mode_is_set_THEN_readback_updates_with_the_value_just_set(self):
         for val in ["Current", "Voltage"]:
@@ -64,20 +72,19 @@ class Keithley2400Tests(unittest.TestCase):
             self.ca.assert_setting_setpoint_sets_readback(val, "SOURCE:MODE")
 
     def test_WHEN_current_compliance_is_set_THEN_readback_updates_with_the_value_just_set(self):
-        for val in [1.23, 456.789]:
+        for val in [1.23, 456.789, 1e-6, -1e-6]:
             self.ca.assert_setting_setpoint_sets_readback(val, "I:COMPLIANCE")
 
     def test_WHEN_voltage_compliance_is_set_THEN_readback_updates_with_the_value_just_set(self):
-        for val in [1.23, 456.789]:
+        for val in [1.23, 456.789, 1e-5, -1e-5]:
             self.ca.assert_setting_setpoint_sets_readback(val, "V:COMPLIANCE")
 
     def test_WHEN_source_voltage_is_set_THEN_readback_updates_with_the_value_just_set(self):
-        for val in [1.23, 456.789]:
-            self.log.info('Setpoint value: {}'.format(val))
+        for val in [1.23, 456.789, 1e-5, -1e-5]:
             self.ca.assert_setting_setpoint_sets_readback(val, "VOLT:SOURCE")
 
     def test_WHEN_source_current_is_set_THEN_readback_updates_with_the_value_just_set(self):
-        for val in [0.1, -0.1]:
+        for val in [0.1, -0.1, 1e-6, -1e-6]:
             self.ca.assert_setting_setpoint_sets_readback(val, "CURR:SOURCE")
 
     def test_WHEN_source_current_autoranging_is_set_THEN_readback_updates_with_the_value_just_set(self):
@@ -96,21 +103,26 @@ class Keithley2400Tests(unittest.TestCase):
         for val in ["On", "Off"]:
             self.ca.assert_setting_setpoint_sets_readback(val, "VOLT:MEAS:AUTORANGE")
 
+    @skip_if_recsim("Banded record behaviour too complex for recsim")
     def test_WHEN_source_current_range_is_set_THEN_readback_updates_with_the_appropriate_range_for_value_just_set(self):
-        for val in [pow(10, i) for i in range(-6, 1)]:
-            self.ca.assert_setting_setpoint_sets_readback(val, "CURR:SOURCE:RANGE")
+        for val in [1.0*pow(10, i) for i in range(-6, 1)]:
+            self.ca.set_pv_value("CURR:SOURCE:RANGE:SP", val)
+            self.ca.assert_that_pv_is_number("CURR:SOURCE:RANGE:SP:RBV", val, tolerance=0.05 * val)
 
+    @skip_if_recsim("Banded record behaviour too complex for recsim")
     def test_WHEN_source_voltage_range_is_set_THEN_readback_updates_with_the_appropriate_range_for_value_just_set(self):
-        for val in [2.1*pow(10, i) for i in range(-6, 1)]:
+        for val in [2.0*pow(10, i) for i in range(-6, 1)]:
             self.ca.set_pv_value("VOLT:SOURCE:RANGE:SP", val)
             self.ca.assert_that_pv_is_number("VOLT:SOURCE:RANGE:SP:RBV", val, tolerance=0.05 * val)
 
+    @skip_if_recsim("Banded record behaviour too complex for recsim")
     def test_WHEN_volts_measurement_range_is_set_THEN_readback_updates_with_appropriate_range_for_value_just_set(self):
-        for val in [2.1 * pow(10, i) for i in range(-6, 1)]:
+        for val in [2.0 * pow(10, i) for i in range(-6, 1)]:
             self.ca.set_pv_value("VOLT:MEAS:RANGE:SP", val)
             self.ca.assert_that_pv_is_number("VOLT:MEAS:RANGE:SP:RBV", val, tolerance=0.05 * val)
 
+    @skip_if_recsim("Banded record behaviour too complex for recsim")
     def test_WHEN_current_measurement_range_is_set_THEN_readback_updates_with_appropriate_range_for_value_just_set(self):
-        for val in [1.05 * pow(10, i) for i in range(-6, 1)]:
+        for val in [1.0 * pow(10, i) for i in range(-6, 1)]:
             self.ca.set_pv_value("CURR:MEAS:RANGE:SP", val)
             self.ca.assert_that_pv_is_number("CURR:MEAS:RANGE:SP:RBV", val, tolerance=0.05 * val)
