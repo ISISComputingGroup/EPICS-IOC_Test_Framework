03e365ad9713ec04c5984770a62b991aafa9fe3b
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-06-22 16:03:53 +0100

    Hello world test passes for cpp linked record

--- tests/kynctm3k.py
+++ tests/kynctm3k.py
@@ -5,6 +5,7 @@ from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
 
+EMULATOR_NAME = "kynctm3k"
 
 DEVICE_PREFIX = "KYNCTM3K_01"
 
@@ -13,8 +14,7 @@ IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("KYNCTM3K"),
-        "macros": {},
-        "emulator": "Kynctm3K",
+        "emulator": EMULATOR_NAME,
     },
 ]
 
@@ -34,44 +34,47 @@ class Kynctm3KTests(unittest.TestCase):
                      "even_numbers": [2*x for x in range(1, 9)]}
 
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Kynctm3K", DEVICE_PREFIX)
+        self._lewis, self._ioc = get_running_lewis_and_ioc(EMULATOR_NAME, DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
-        for channel in range(16):
-            self._lewis.backdoor_set_on_device("OUT{}_value".format(channel), 0.)
-            self._lewis.backdoor_set_on_device("program", "none")
+        # for channel in range(16):
+        #     self._lewis.backdoor_set_on_device("OUT{}_value".format(channel), 0.)
+        #     self._lewis.backdoor_set_on_device("program", "none")
 
     def test_GIVEN_device_is_started_THEN_raw_status_is_hello_world(self):
         self.ca.assert_that_pv_is("STATUS:RAW", "Hello, world!")
 
-    @skip_if_recsim("Backdoor behaviour too complex for RECSIM")
-    def test_GIVEN_input_program_WHEN_measurement_value_is_requested_THEN_appropriate_number_of_output_values_are_returned(self):
+    def test_GIVEN_device_is_started_THEN_status_is_hello_world(self):
+        self.ca.assert_that_pv_is("STATUS", "Hello, world!")
 
-        for program in self.program_modes:
-
-            expected_values = []
-            # Assign test values to only the active OUTs in the emulator
-            for channel_to_set in range(16):
-
-                if channel_to_set in self.program_modes[program]:
-                    channel_value = 2.*channel_to_set
-                else:
-                    channel_value = False
-
-                expected_values.append(channel_value)
-
-                self._lewis.backdoor_set_on_device("OUT{}_value".format(channel_to_set), channel_value)
-
-            # Read in the measurement values just set
-            for channel_to_test, expected_value in enumerate(expected_values):
-                if not expected_value:
-                    continue
-
-                self.ca.assert_that_pv_is_number("MEAS:OUT{}".format(channel_to_test),
-                                                 expected_value, tolerance=0.01*expected_value)
-
-    def test_WHEN_head_configuration_is_set_THEN_device_updates_to_given_setup(self):
-        for screen_type in [0, 15, "HA"]:
-            self.ca.set_pv_value(screen_type, "MEAS:SCREEN")
-            self.ca.assert_that_pv_is("MEAS:SCREEN", screen_type)
-
+            # @skip_if_recsim("Backdoor behaviour too complex for RECSIM")
+    # def test_GIVEN_input_program_WHEN_measurement_value_is_requested_THEN_appropriate_number_of_output_values_are_returned(self):
+    #
+    #     for program in self.program_modes:
+    #
+    #         expected_values = []
+    #         # Assign test values to only the active OUTs in the emulator
+    #         for channel_to_set in range(16):
+    #
+    #             if channel_to_set in self.program_modes[program]:
+    #                 channel_value = 2.*channel_to_set
+    #             else:
+    #                 channel_value = False
+    #
+    #             expected_values.append(channel_value)
+    #
+    #             self._lewis.backdoor_set_on_device("OUT{}_value".format(channel_to_set), channel_value)
+    #
+    #         # Read in the measurement values just set
+    #         for channel_to_test, expected_value in enumerate(expected_values):
+    #             if not expected_value:
+    #                 continue
+    #
+    #             self.ca.assert_that_pv_is_number("MEAS:OUT{}".format(channel_to_test),
+    #                                              expected_value, tolerance=0.01*expected_value)
+    #
+    # def test_WHEN_head_configuration_is_set_THEN_device_updates_to_given_setup(self):
+    #     for screen_type in [0, 15, "HA"]:
+    #         self.ca.set_pv_value(screen_type, "MEAS:SCREEN")
+    #         self.ca.assert_that_pv_is("MEAS:SCREEN", screen_type)
+    #
