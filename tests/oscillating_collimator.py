e0e716448671f8e59d7b379695efe3cab102b865
Dominic Oram <dominic.oram@stfc.ac.uk>
2018-06-22 10:26:09 +0100

    Removed redundant tests

--- tests/oscillating_collimator.py
+++ tests/oscillating_collimator.py
@@ -13,7 +13,7 @@ PREFIX = "MOT:OSCCOL"
 # Commonly used PVs
 ANGLE = "ANGLE:SP"
 FREQUENCY = "FREQ:SP"
-RADIUS = "RADIUS:SP"
+RADIUS = "RADIUS"
 VELOCITY = "VEL:SP"
 DISTANCE = "DIST:SP"
 DISCRIMINANT = "VEL:SP:DISC:CHECK"
@@ -31,6 +31,7 @@ IOCS = [
     },
 ]
 
+COLLIMATOR_RADIUS_DEFAULT = 284
 
 TEST_MODES = [TestModes.DEVSIM]
 
@@ -102,10 +103,6 @@ class OscillatingCollimatorTests(unittest.TestCase):
     def test_WHEN_frq_set_greater_than_two_THEN_angle_is_two(self):
         self.ca.set_pv_value(ANGLE, 5.0)
         self.ca.assert_that_pv_is_number(ANGLE, 2.0)
-
-    def test_WHEN_mounting_radius_set_negative_THEN_mounting_radius_is_zero(self):
-        self.ca.set_pv_value(RADIUS, -1.0)
-        self.ca.assert_that_pv_is_number(RADIUS, 0.0)
 
     def test_WHEN_input_values_cause_discriminant_to_be_negative_THEN_discriminant_pv_is_one(self):
 
