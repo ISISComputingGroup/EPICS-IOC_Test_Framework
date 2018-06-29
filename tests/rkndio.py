e68c143b35c2013a051dc528a4fdf1aa0999e18c
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-06-29 12:06:14 +0100

    Fixed tests to work with device changes.

--- tests/rkndio.py
+++ tests/rkndio.py
@@ -36,10 +36,9 @@ class RkndioVersionTests(unittest.TestCase):
         self._connect_emulator()
         self.ca.assert_that_pv_exists("IDN")
 
-        self._lewis.backdoor_set_on_device("status", "No Error")
-        self.ca.assert_that_pv_is("STATUS", "No Error")
+        self._lewis.backdoor_run_function_on_device("reset_error")
 
-        self._lewis.backdoor_set_on_device("error", "No Error")
+        self.ca.assert_that_pv_is("STATUS", "No Error")
         self.ca.assert_that_pv_is("ERROR", "No Error")
 
     def _connect_emulator(self):
@@ -88,16 +87,16 @@ class RkndioVersionTests(unittest.TestCase):
     def test_that_we_can_read_a_digital_input(self, _, pin):
         # Given
         pv = "PIN_{}".format(pin)
-        self._lewis.backdoor_run_function_on_device("set_input_state_via_the_backdoor", [pin, "False"])
-        self.ca.assert_that_pv_is(pv, "False")
+        self._lewis.backdoor_run_function_on_device("set_input_state_via_the_backdoor", [pin, "FALSE"])
+        self.ca.assert_that_pv_is(pv, "FALSE")
 
-        self._lewis.backdoor_run_function_on_device("set_input_state_via_the_backdoor", [pin, "True"])
+        self._lewis.backdoor_run_function_on_device("set_input_state_via_the_backdoor", [pin, "TRUE"])
 
         # When:
         self.ca.process_pv(pv)
 
         # Then:
-        self.ca.assert_that_pv_is(pv, "True")
+        self.ca.assert_that_pv_is(pv, "TRUE")
 
     @parameterized.expand([
         ("Pin_{}".format(i), i) for i in range(8, 14)
@@ -107,12 +106,12 @@ class RkndioVersionTests(unittest.TestCase):
         pv = "PIN_{}".format(pin)
         self.ca.set_pv_value(pv, "FALSE")
         reset_check = self._lewis.backdoor_run_function_on_device("get_output_state_via_the_backdoor", [pin])[0]
-        self.assertEqual(reset_check, "False")
+        self.assertEqual(reset_check, "FALSE")
 
         # When:
         self.ca.set_pv_value(pv, "TRUE")
 
         # Then:
         result = self._lewis.backdoor_run_function_on_device("get_output_state_via_the_backdoor", [pin])[0]
-        self.assertEqual(result, "True")
+        self.assertEqual(result, "TRUE")
 
