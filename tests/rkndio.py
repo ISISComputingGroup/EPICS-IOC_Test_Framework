db67379e4dfb92024a6c54289fed9da60a5fccfb
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-06-29 11:05:24 +0100

    Added writing to DO.

--- tests/rkndio.py
+++ tests/rkndio.py
@@ -85,9 +85,8 @@ class RkndioVersionTests(unittest.TestCase):
     @parameterized.expand([
         ("Pin_{}".format(i), i) for i in range(2, 8)
     ])
-    def test_that_we_can_read_a_digital_input(self, _, i):
+    def test_that_we_can_read_a_digital_input(self, _, pin):
         # Given
-        pin = i
         pv = "PIN_{}".format(pin)
         self._lewis.backdoor_run_function_on_device("set_input_state_via_the_backdoor", [pin, "False"])
         self.ca.assert_that_pv_is(pv, "False")
@@ -99,4 +98,21 @@ class RkndioVersionTests(unittest.TestCase):
 
         # Then:
         self.ca.assert_that_pv_is(pv, "True")
+
+    @parameterized.expand([
+        ("Pin_{}".format(i), i) for i in range(8, 14)
+    ])
+    def test_that_we_can_write_to_a_digital_output(self, _, pin):
+        # Given
+        pv = "PIN_{}".format(pin)
+        self.ca.set_pv_value(pv, "FALSE")
+        reset_check = self._lewis.backdoor_run_function_on_device("get_output_state_via_the_backdoor", [pin])[0]
+        self.assertEqual(reset_check, "False")
+
+        # When:
+        self.ca.set_pv_value(pv, "TRUE")
+
+        # Then:
+        result = self._lewis.backdoor_run_function_on_device("get_output_state_via_the_backdoor", [pin])[0]
+        self.assertEqual(result, "True")
 
