a5620f071b6b8cd9bfea539084a25ebb5f5a01f4
Rory-Potter-Tessella <rory.potter@stfc.ac.uk>
2018-07-25 11:21:39 +0100

    Added stop device tests.

--- tests/ngpspsu.py
+++ tests/ngpspsu.py
@@ -65,10 +65,34 @@ class NgpspsuStartTests(unittest.TestCase):
     def setUp(self):
         self._lewis, self._ioc, self.ca = reset_device()
 
-    def test_that_WHEN_started_THEN_the_device_is_on(self):
+    def test_that_WHEN_started_THEN_the_device_turns_on(self):
         # When:
-        self.ca.set_pv_value("ON:SP", 1)
+        _start_device(self.ca)
 
         # Then:
+        check_device_status_is("On", self._lewis)
+
+
+class NgpspsuStopTests(unittest.TestCase):
+    """
+    Tests for the Ngpspsu IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc, self.ca = reset_device()
+
+    def test_that_WHEN_on_setup_THEN_the_device_is_off(self):
+        # When/Then:
         status = self._lewis.backdoor_run_function_on_device("get_status_via_the_backdoor")[0]
-        assert_that(status, is_(equal_to("On")))
+        assert_that(status, is_(equal_to("Off")))
+
+    def test_that_GIVEN_a_device_which_is_running_THEN_the_device_turns_off(self):
+        # Given:
+        _start_device(self.ca)
+        check_device_status_is("On", self._lewis)
+
+        # When
+        _stop_device(self.ca)
+
+        # Then:
+        check_device_status_is("Off", self._lewis)
+
