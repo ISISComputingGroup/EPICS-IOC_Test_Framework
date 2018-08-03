b768c5852c8bb7b52847eb1a96eee9590d5b8594
LiamPanchaud <LiamPanchaud@ndw1801.isis.cclrc.ac.uk>
2018-08-03 09:21:26 +0100

    added test to check for idle motor when POSN=POSN:SP

--- tests/rotsc.py
+++ tests/rotsc.py
@@ -3,7 +3,7 @@ import unittest
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import IOCRegister
+from utils.testing import IOCRegister, skip_if_recsim
 
 
 DEVICE_PREFIX = "ROTSC_01"
@@ -31,7 +31,20 @@ class RotscTests(unittest.TestCase):
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
         self._ioc = IOCRegister.get_running(DEVICE_PREFIX)
         self.ca.assert_that_pv_exists("POSN")
+        self.ca.set_pv_value("INIT", 1)
 
     def test_WHEN_position_set_to_value_THEN_readback_set_to_value(self):
         for val in [1, 16]:
             self.ca.assert_setting_setpoint_sets_readback(val, "POSN", "POSN:SP", val)
+
+    @skip_if_recsim("Recsim cannot model complex behaviour (motor motion)")
+    def test_WHEN_position_setpoint_changed_THEN_motor_active(self):
+        self.ca.set_pv_value("POSN", 01)
+        self.ca.set_pv_value("POSN:SP", 19)
+        self.ca.assert_that_pv_is("MOTOR_0_ACTIVE", "ACTIVE")
+
+    @skip_if_recsim("Recsim cannot model complex behaviour (motor motion)")
+    def test_WHEN_position_set_to_current_position_THEN_nothing_happens(self):
+        self.ca.set_pv_value("POSN", 03)
+        self.ca.set_pv_value("POSN:SP", 03)
+        self.ca.set_pv_value("MOTOR_0_ACTIVE", "IDLE")
