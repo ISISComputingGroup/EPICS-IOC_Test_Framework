8e0bf27a07404e0a3e1343abb8c35f87abc1097b
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-08-28 11:35:41 +0100

    Tidied up tests.

--- tests/kicker/test_basic_commands.py
+++ tests/kicker/test_basic_commands.py
@@ -7,14 +7,14 @@ from hamcrest import assert_that, is_, equal_to
 
 DEVICE_PREFIX = "KICKER_01"
 
-# VOLTAGE CALIBRATION CONTSTANTS FOR TESTS
+# VOLTAGE CALIBRATION CONSTANTS FOR TESTS
 DAQ_MAX_VOLTAGE = 10.0
-PSU_MAX_VOLTAGE = 45.0  # This is set as a macro in IOC st.cmd
+PSU_MAX_VOLTAGE = 45.0  # This is the default value set as a macro in IOC st.cmd
 VOLTAGE_CALIBRATION_RATIO = PSU_MAX_VOLTAGE / DAQ_MAX_VOLTAGE
 
-# CURRENT CALIBRATION CONTSTANTS FOR TESTS
+# CURRENT CALIBRATION CONSTANTS FOR TESTS
 DAQ_MAX_CURRENT = 10.0
-PSU_MAX_CURRENT = 15.0  # This is set as a macro in IOC st.cmd
+PSU_MAX_CURRENT = 15.0  # This is the default value set as a macro in IOC st.cmd
 CURRENT_CALIBRATION_RATIO = PSU_MAX_CURRENT / DAQ_MAX_CURRENT
 
 
@@ -24,14 +24,9 @@ class BaseTests(unittest.TestCase):
 
     def setUp(self):
         self.ca = ChannelAccess(20, device_prefix=DEVICE_PREFIX)
+        self._simulate_value(0)
 
-        pv_root = "DAQ:{}".format(self.record)
-        self.ca.set_pv_value("DAQ:{}:SIM".format(self.record), 0)
-
-        value_to_check = self.ca.get_pv_value("{}:_RAW".format(pv_root))[0]
-        assert_that(value_to_check, is_(equal_to(0)))
-
-    def simulate_value(self, value):
+    def _simulate_value(self, value):
         pv_root = "DAQ:{}".format(self.record)
         self.ca.set_pv_value("{}:SIM".format(pv_root), value)
         self.ca.assert_that_pv_is("{}:SIM".format(pv_root), value)
@@ -40,11 +35,11 @@ class BaseTests(unittest.TestCase):
         assert_that(value_to_check, is_(equal_to(value)))
 
     @parameterized.expand(
-        parameterized_list([4.68, 10, 0, 4e-3])
+        parameterized_list([4.68, 6, 10, 0, 4e-3])
     )
     def test_that_GIVEN_a_value_THEN_the_calibrated_value_is_read(self, _, value_to_set):
         # Given:
-        self.simulate_value(value_to_set)
+        self._simulate_value(value_to_set)
 
         # Then:
         expected_calibrated_value = self.calibration * value_to_set
@@ -53,9 +48,9 @@ class BaseTests(unittest.TestCase):
     @parameterized.expand(
         parameterized_list([15, -5])
     )
-    def test_that_GIVEN_a_value_out_of_range_THEN_pv_is_in_alarm(self, _, value_to_set):
+    def test_that_GIVEN_a_value_out_of_range_THEN_the_pv_is_in_alarm(self, _, value_to_set):
         # Given:
-        self.simulate_value(value_to_set)
+        self._simulate_value(value_to_set)
         self.ca.assert_that_pv_is_number(self.record, value_to_set * self.calibration, 0.01)
 
         # Then:
