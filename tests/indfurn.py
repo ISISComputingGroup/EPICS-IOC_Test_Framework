4a66613bcffbcd86335b1b6006af7851d9942815
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-09-11 18:09:47 +0100

    Implement more commands

--- tests/indfurn.py
+++ tests/indfurn.py
@@ -1,4 +1,5 @@
 import unittest
+from time import sleep
 
 from parameterized import parameterized
 
@@ -27,8 +28,11 @@ TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 TEST_TEMPERATURES = [0.01, 123.45, 9999.99]  # 2 d.p. precision, up to 9999.99
 TEST_DIAGNOSTIC_TEMPERATURES = [0.1, 123.7, 999.9]  # 1 d.p. presision, up to 999.9
 TEST_PID_VALUES = TEST_DIAGNOSTIC_TEMPERATURES
+TEST_OUTPUTS = TEST_DIAGNOSTIC_TEMPERATURES
 TEST_SAMPLE_TIMES = [0, 1, 999999]
 TEST_PID_LIMITS = TEST_TEMPERATURES
+TEST_PSU_VOLTAGES = [0.01, 123.45, 999.99]
+TEST_PSU_CURRENTS = TEST_PSU_VOLTAGES
 
 
 class IndfurnTests(unittest.TestCase):
@@ -100,9 +104,56 @@ class IndfurnTests(unittest.TestCase):
     def test_GIVEN_pid_upper_limit_is_set_THEN_it_can_be_read_back(self, _, pid_limit):
         self.ca.assert_setting_setpoint_sets_readback(pid_limit, "PID:LIMIT:UPPER")
 
+    @parameterized.expand(parameterized_list(TEST_PSU_VOLTAGES))
+    def test_GIVEN_psu_voltage_is_set_THEN_it_can_be_read_back(self, _, psu_volt):
+        self.ca.assert_setting_setpoint_sets_readback(psu_volt, "PSU:VOLT")
+
+    @parameterized.expand(parameterized_list(TEST_PSU_CURRENTS))
+    def test_GIVEN_psu_current_is_set_THEN_it_can_be_read_back(self, _, psu_curr):
+        self.ca.assert_setting_setpoint_sets_readback(psu_curr, "PSU:CURR")
+
+    @parameterized.expand(parameterized_list(TEST_OUTPUTS))
+    def test_GIVEN_output_is_set_THEN_it_can_be_read_back(self, _, output):
+        self.ca.assert_setting_setpoint_sets_readback(output, "OUTPUT")
+
     def test_GIVEN_pid_mode_is_set_THEN_it_can_be_read_back(self):
         for mode in ["Automatic", "Manual", "Automatic"]:  # Check both transitions
             self.ca.assert_setting_setpoint_sets_readback(mode, "PID:MODE")
+
+    def _wait_for_lewis_backdoor(self, variable_name, expected, max_attempts=10, sleep_interval=0.1):
+        for _ in range(max_attempts):
+            try:
+                self.assertEqual(self._lewis.backdoor_get_from_device(variable_name), str(expected))
+                break
+            except AssertionError as e:
+                err = e
+                sleep(sleep_interval)
+        else:
+            raise err
+
+    @skip_if_recsim("Lewis backdoor not available in recsim")
+    def test_GIVEN_power_supply_mode_is_set_to_either_local_or_remote_THEN_it_sets_successfully_in_emulator(self):
+        for remote in [False, True, False]:  # Check both transitions
+            self.ca.set_pv_value("PSU:CONTROLMODE:SP", "Remote" if remote else "Local")
+            self._wait_for_lewis_backdoor("remote_mode", remote)
+
+    @skip_if_recsim("Lewis backdoor not available in recsim")
+    def test_GIVEN_power_supply_output_is_set_to_either_on_or_off_THEN_it_sets_successfully_in_emulator(self):
+        for output in [False, True, False]:  # Check both transitions
+            self.ca.set_pv_value("PSU:POWER:SP", "On" if output else "Off")
+            self._wait_for_lewis_backdoor("power_supply_on", output)
+
+    @skip_if_recsim("Lewis backdoor not available in recsim")
+    def test_GIVEN_power_supply_fan_is_set_to_either_on_or_off_THEN_it_sets_successfully_in_emulator(self):
+        for fan_on in [False, True, False]:  # Check both transitions
+            self.ca.set_pv_value("PSU:FAN:SP", "On" if fan_on else "Off")
+            self._wait_for_lewis_backdoor("power_supply_fan_on", fan_on)
+
+    @skip_if_recsim("Lewis backdoor not available in recsim")
+    def test_GIVEN_power_supply_hf_is_set_to_either_on_or_off_THEN_it_sets_successfully_in_emulator(self):
+        for hf_on in [False, True, False]:  # Check both transitions
+            self.ca.set_pv_value("PSU:HF:SP", "On" if hf_on else "Off")
+            self._wait_for_lewis_backdoor("hf_on", hf_on)
 
     @skip_if_recsim("Can't use lewis backdoor in recsim")
     def test_GIVEN_psu_goes_over_temperature_THEN_alarm_comes_on_AND_can_reset_via_pv(self):
