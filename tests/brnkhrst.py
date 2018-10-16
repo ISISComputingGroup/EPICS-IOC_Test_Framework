aa7ab8f3b7498aac4fe4155e4094098402e6f38d
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-10-16 10:39:18 +0100

    Add device to test framework

---
+++ tests/brnkhrst.py
@@ -0,0 +1,34 @@
+import unittest
+
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+
+
+DEVICE_PREFIX = "BRNKHRST_01"
+
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("BRNKHRST"),
+        "macros": {},
+        "emulator": "Brnkhrst",
+    },
+]
+
+
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+
+
+class BrnkhrstTests(unittest.TestCase):
+    """
+    Tests for the Brnkhrst IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc("Brnkhrst", DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+
+    def test_that_fails(self):
+        self.fail("You haven't implemented any tests!")
