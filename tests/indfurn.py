eebab7676530dcb1dcdc88a0f803ff2093dc26fb
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-10-22 16:26:25 +0100

    Update tests for real device

--- tests/indfurn.py
+++ tests/indfurn.py
@@ -24,12 +24,20 @@ IOCS = [
 
 TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 
-TEST_TEMPERATURES = [0.01, 123.45, 9999.99]  # 2 d.p. precision, up to 9999.99
-TEST_DIAGNOSTIC_TEMPERATURES = [0.1, 123.7, 999.9]  # 1 d.p. presision, up to 999.9
-TEST_PID_VALUES = TEST_DIAGNOSTIC_TEMPERATURES
-TEST_OUTPUTS = TEST_DIAGNOSTIC_TEMPERATURES
+TEST_TEMPERATURES = [
+    (123.45, ChannelAccess.Alarms.NONE),
+    (9999.99, ChannelAccess.Alarms.MAJOR),
+]
+
+TEST_DIAGNOSTIC_TEMPERATURES = [
+    (23.4, ChannelAccess.Alarms.NONE),
+    (999.9, ChannelAccess.Alarms.MAJOR),
+]
+
+TEST_PID_VALUES = [23.4, 999.9]
+TEST_OUTPUTS = TEST_PID_VALUES
 TEST_SAMPLE_TIMES = [0, 1, 999999]
-TEST_PID_LIMITS = TEST_TEMPERATURES
+TEST_PID_LIMITS = [0.0, 0.1, 99.9]
 TEST_PSU_VOLTAGES = [0.01, 123.45, 999.99]
 TEST_PSU_CURRENTS = TEST_PSU_VOLTAGES
 
@@ -40,40 +48,45 @@ class IndfurnTests(unittest.TestCase):
     """
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc(EMULATOR_NAME, DEVICE_PREFIX)
-        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_timeout=30)
 
     def test_that_disable_pv_exists(self):
         self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
 
     @skip_if_recsim("Recsim does not emulate version command")
     def test_that_version_pv_exists(self):
-        self.ca.assert_that_pv_is("VERSION", "EMULATED INDUCTION FURNACE")
+        self.ca.assert_that_pv_is("VERSION", "EMULATED FURNACE")
 
     @parameterized.expand(parameterized_list(TEST_TEMPERATURES))
-    def test_GIVEN_a_setpoint_WHEN_ask_for_the_setpoint_readback_THEN_get_the_value_just_set(self, _, temp):
-        self.ca.assert_setting_setpoint_sets_readback(temp, set_point_pv="TEMP:SP", readback_pv="TEMP:SP:RBV")
+    def test_GIVEN_a_setpoint_WHEN_ask_for_the_setpoint_readback_THEN_get_the_value_just_set(self, _, temp, alarm):
+        self.ca.assert_setting_setpoint_sets_readback(
+            temp, set_point_pv="TEMP:SP", readback_pv="TEMP:SP:RBV", expected_alarm=alarm)
 
     @parameterized.expand(parameterized_list(TEST_TEMPERATURES))
-    def test_GIVEN_a_setpoint_WHEN_ask_for_the_current_temperature_THEN_get_the_value_just_set(self, _, temp):
-        self.ca.assert_setting_setpoint_sets_readback(temp, set_point_pv="TEMP:SP", readback_pv="TEMP")
+    def test_GIVEN_a_setpoint_WHEN_ask_for_the_current_temperature_THEN_get_the_value_just_set(self, _, temp, alarm):
+        self.ca.assert_setting_setpoint_sets_readback(
+            temp, set_point_pv="TEMP:SP", readback_pv="TEMP", expected_alarm=alarm)
 
     @parameterized.expand(parameterized_list(TEST_DIAGNOSTIC_TEMPERATURES))
     @skip_if_recsim("Lewis backdoor not available in recsim")
-    def test_GIVEN_pipe_temperature_set_via_backdoor_when_read_pipe_temperature_THEN_get_value_just_set(self, _, temp):
+    def test_GIVEN_pipe_temperature_set_via_backdoor_when_read_pipe_temperature_THEN_get_value_just_set(self, _, temp, alarm):
         self._lewis.backdoor_set_on_device("pipe_temperature", temp)
         self.ca.assert_that_pv_is_number("PIPE:TEMP", temp, tolerance=0.1)
+        self.ca.assert_that_pv_alarm_is("PIPE:TEMP", alarm)
 
     @parameterized.expand(parameterized_list(TEST_DIAGNOSTIC_TEMPERATURES))
     @skip_if_recsim("Lewis backdoor not available in recsim")
-    def test_GIVEN_capacitor_temperature_set_via_backdoor_when_read_capacitor_temperature_THEN_get_value_just_set(self, _, temp):
+    def test_GIVEN_capacitor_temperature_set_via_backdoor_when_read_capacitor_temperature_THEN_get_value_just_set(self, _, temp, alarm):
         self._lewis.backdoor_set_on_device("capacitor_bank_temperature", temp)
         self.ca.assert_that_pv_is_number("CAPACITOR:TEMP", temp, tolerance=0.1)
+        self.ca.assert_that_pv_alarm_is("CAPACITOR:TEMP", alarm)
 
     @parameterized.expand(parameterized_list(TEST_DIAGNOSTIC_TEMPERATURES))
     @skip_if_recsim("Lewis backdoor not available in recsim")
-    def test_GIVEN_fet_temperature_set_via_backdoor_when_read_fet_temperature_THEN_get_value_just_set(self, _, temp):
+    def test_GIVEN_fet_temperature_set_via_backdoor_when_read_fet_temperature_THEN_get_value_just_set(self, _, temp, alarm):
         self._lewis.backdoor_set_on_device("fet_temperature", temp)
         self.ca.assert_that_pv_is_number("FET:TEMP", temp, tolerance=0.1)
+        self.ca.assert_that_pv_alarm_is("FET:TEMP", alarm)
 
     @parameterized.expand(parameterized_list(TEST_PID_VALUES))
     def test_GIVEN_p_parameter_changed_WHEN_read_p_THEN_value_can_be_read_back(self, _, val):
@@ -123,40 +136,27 @@ class IndfurnTests(unittest.TestCase):
         for mode in ["Automatic", "Manual", "Automatic"]:  # Check both transitions
             self.ca.assert_setting_setpoint_sets_readback(mode, "PID:MODE")
 
-    def _wait_for_lewis_backdoor(self, variable_name, expected, max_attempts=10, sleep_interval=0.1):
-        for _ in range(max_attempts):
-            try:
-                self.assertEqual(self._lewis.backdoor_get_from_device(variable_name), str(expected))
-                break
-            except AssertionError as e:
-                err = e
-                sleep(sleep_interval)
-        else:
-            raise err
-
     @skip_if_recsim("Lewis backdoor not available in recsim")
     def test_GIVEN_power_supply_mode_is_set_to_either_local_or_remote_THEN_it_sets_successfully_in_emulator(self):
         for remote in [False, True, False]:  # Check both transitions
-            self.ca.set_pv_value("PSU:CONTROLMODE:SP", "Remote" if remote else "Local")
-            self._wait_for_lewis_backdoor("remote_mode", remote)
+            self.ca.assert_setting_setpoint_sets_readback("Remote" if remote else "Local", "PSU:CONTROLMODE",
+                                                          expected_alarm=self.ca.Alarms.NONE if remote else self.ca.Alarms.MAJOR)
 
     @skip_if_recsim("Lewis backdoor not available in recsim")
     def test_GIVEN_power_supply_output_is_set_to_either_on_or_off_THEN_it_sets_successfully_in_emulator(self):
         for output in [False, True, False]:  # Check both transitions
-            self.ca.set_pv_value("PSU:POWER:SP", "On" if output else "Off")
-            self._wait_for_lewis_backdoor("power_supply_on", output)
+            self.ca.assert_setting_setpoint_sets_readback("On" if output else "Off", "PSU:POWER")
 
     @skip_if_recsim("Lewis backdoor not available in recsim")
     def test_GIVEN_power_supply_fan_is_set_to_either_on_or_off_THEN_it_sets_successfully_in_emulator(self):
         for fan_on in [False, True, False]:  # Check both transitions
-            self.ca.set_pv_value("PSU:FAN:SP", "On" if fan_on else "Off")
-            self._wait_for_lewis_backdoor("power_supply_fan_on", fan_on)
+            self.ca.assert_setting_setpoint_sets_readback("On" if fan_on else "Off", "PSU:FAN",
+                                                          expected_alarm=self.ca.Alarms.NONE if fan_on else self.ca.Alarms.MAJOR)
 
     @skip_if_recsim("Lewis backdoor not available in recsim")
     def test_GIVEN_power_supply_hf_is_set_to_either_on_or_off_THEN_it_sets_successfully_in_emulator(self):
         for hf_on in [False, True, False]:  # Check both transitions
-            self.ca.set_pv_value("PSU:HF:SP", "On" if hf_on else "Off")
-            self._wait_for_lewis_backdoor("hf_on", hf_on)
+            self.ca.assert_setting_setpoint_sets_readback("On" if hf_on else "Off", "PSU:HF")
 
     @skip_if_recsim("Can't use lewis backdoor in recsim")
     def test_GIVEN_psu_goes_over_temperature_THEN_alarm_comes_on_AND_can_reset_via_pv(self):
@@ -178,15 +178,18 @@ class IndfurnTests(unittest.TestCase):
 
     @skip_if_recsim("Can't use lewis backdoor in recsim")
     def test_GIVEN_cooling_water_flow_turns_off_THEN_this_is_visible_from_ioc_and_causes_alarm(self):
-        self._lewis.backdoor_set_on_device("cooling_water_flow", False)
-        self.ca.assert_that_pv_is("COOLINGWATERFLOW", "ALARM")
-        self.ca.assert_that_pv_alarm_is("COOLINGWATERFLOW", self.ca.Alarms.MAJOR)
 
-        self._lewis.backdoor_set_on_device("cooling_water_flow", True)
-        self.ca.assert_that_pv_is("COOLINGWATERFLOW", "OK")
-        self.ca.assert_that_pv_alarm_is("COOLINGWATERFLOW", self.ca.Alarms.NONE)
+        self._lewis.backdoor_set_on_device("cooling_water_flow", 0)
+        self.ca.assert_that_pv_is("COOLINGWATER:FLOW", 0)
+        self.ca.assert_that_pv_is("COOLINGWATER:STATUS", "ALARM")
+        self.ca.assert_that_pv_alarm_is("COOLINGWATER:STATUS", self.ca.Alarms.MAJOR)
+
+        self._lewis.backdoor_set_on_device("cooling_water_flow", 500)
+        self.ca.assert_that_pv_is("COOLINGWATER:FLOW", 500)
+        self.ca.assert_that_pv_is("COOLINGWATER:STATUS", "OK")
+        self.ca.assert_that_pv_alarm_is("COOLINGWATER:STATUS", self.ca.Alarms.NONE)
 
     @skip_if_recsim("Recsim can't handle arbitrary commands")
     def test_GIVEN_an_arbitrary_command_THEN_get_a_response(self):
         self.ca.set_pv_value("ARBITRARY:SP", "?ver")
-        self.ca.assert_that_pv_is("ARBITRARY", "<EMULATED INDUCTION FURNACE")
+        self.ca.assert_that_pv_is("ARBITRARY", "<EMULATED FURNACE\r\n<EMULATED FURNACE\r\n")
