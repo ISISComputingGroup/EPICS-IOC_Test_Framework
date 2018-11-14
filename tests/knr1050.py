30985bed077543fdb1b633e2fcb98f980e895402
aaronlong <aaron.long@stfc.ac.uk>
2018-11-14 13:59:03 +0000

    Created initial test for KNR1050

--- tests/knr1050.py
+++ tests/knr1050.py
@@ -8,13 +8,14 @@ from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
 
 DEVICE_PREFIX = "KNR1050_01"
 
+device_name = "knr1050"
 
 IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("KNR1050"),
         "macros": {},
-        "emulator": "Knr1050",
+        "emulator": device_name,
     },
 ]
 
@@ -27,8 +28,15 @@ class Knr1050Tests(unittest.TestCase):
     Tests for the Knr1050 IOC.
     """
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Knr1050", DEVICE_PREFIX)
+        self._lewis, self._ioc = get_running_lewis_and_ioc(device_name, DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+        #self._lewis.backdoor_run_function_on_device("reset")
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+    @skip_if_recsim("In rec sim this test fails")
+    def test_GIVEN_an_IOC_WHEN_stop_issued_THEN_device_stops(self):
+        stopped_status = self._lewis.backdoor_get_from_device("is_stopped")
+        self.assertEqual(stopped_status, "False")
+        self.ca.set_pv_value("STOP:SP", 1)
+
+        stopped_status = self._lewis.backdoor_get_from_device("is_stopped")
+        self.assertEqual(stopped_status, "True")
