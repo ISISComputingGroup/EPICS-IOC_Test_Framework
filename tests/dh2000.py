9a713fae3e107eb277707ca7c7ea28b4da0c0a6b
Alistair McGann <alistair.mcgann@tessella.com>
2018-12-05 17:12:59 +0000

    Added interlock test

--- tests/dh2000.py
+++ tests/dh2000.py
@@ -31,6 +31,13 @@ class Dh2000Tests(unittest.TestCase):
         self._lewis, self._ioc = get_running_lewis_and_ioc("dh2000", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
+        self._lewis.backdoor_set_on_device("shutter_is_open", False)
+        self._lewis.backdoor_set_on_device("interlock_is_triggered", False)
+
+        self.ca.assert_that_pv_is("SHUTTER:STATUS", "OPEN")
+        self.ca.assert_that_pv_is("INTERLOCK", "OKAY")
+
+
     @parameterized.expand([
         ("shutter_open_interlock_off", True, False),
         ("shutter_closed_interlock_off", False, False),
@@ -43,9 +50,18 @@ class Dh2000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("INTERLOCK", "TRIGGERED" if interlock_is_triggered else "OKAY")
         self.ca.assert_that_pv_is("SHUTTER:STATUS", "OPEN" if shutter_is_open else "CLOSED")
 
-#    def test_GIVEN_shutter_open_WHEN_interlock_triggered_THEN_shutter_closes(self):
-#        self.fail("Test not written yet")
-#
+    def test_GIVEN_shutter_open_WHEN_interlock_triggered_THEN_shutter_closes(self):
+        self._lewis.backdoor_set_on_device("shutter_is_open", True)
+        self._lewis.backdoor_set_on_device("interlock_is_triggered", False)
+
+        self.ca.assert_that_pv_is("SHUTTER:STATUS", "OPEN")
+        self.ca.assert_that_pv_is("INTERLOCK", "OKAY")
+
+        self._lewis.backdoor_set_on_device("interlock_is_triggered", True)
+
+        self.ca.assert_that_pv_is("INTERLOCK", "TRIGGERED")
+        self.ca.assert_that_pv_is("SHUTTER:STATUS", "CLOSED")
+
     def test_GIVEN_shutter_open_WHEN_shutter_close_requested_THEN_shutter_closes(self):
         self._lewis.backdoor_set_on_device("shutter_is_open", True)
 
@@ -54,6 +70,6 @@ class Dh2000Tests(unittest.TestCase):
         self.ca.process_pv("SHUTTER:CLOSE")
 
         self.ca.assert_that_pv_is("SHUTTER:STATUS", "CLOSED")
-#
+
 #    def test_GIVEN_interlock_state_WHEN_shutter_open_requested_THEN_shutter_opens_if_interlock_not_triggered(self):
 #        self.fail("Test not written yet")
