5e8a931663a9fa7597874accb8a793a079da09f0
Alistair McGann <alistair.mcgann@tessella.com>
2019-01-10 17:14:56 +0000

    Add test for moxa 1240

---
+++ tests/moxa1240.py
@@ -0,0 +1,57 @@
+from __future__ import division
+import unittest
+
+from utils.test_modes import TestModes
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.testing import get_running_lewis_and_ioc
+from parameterized import parameterized
+
+
+# Device prefix
+DEVICE_PREFIX = "MOXA1210_01"
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("MOXA1210"),
+        "emulator": "moxa12xx",
+        "emulator_protocol": "modbus",
+        "macros": {
+            "IEOS": r"\\r\\n",
+            "OEOS": r"\\r\\n",
+            "MODELNO": "1240"
+        }
+    },
+]
+
+TEST_MODES = [TestModes.DEVSIM, ]
+CHANNELS = range(7)
+
+TEST_VALUE = 1.23
+
+
+class Moxa1240Tests(unittest.TestCase):
+    """
+    Tests for a moxa ioLogik e1240. (7x DC Voltage/Current measurements)
+    """
+
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc("moxa12xx", DEVICE_PREFIX)
+
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+
+        # Sends a backdoor command to the device to set an analogue input (AI) value
+        self._lewis.backdoor_run_function_on_device("set_ai", (0, [0.0]*16))
+        for test_channel in CHANNELS:
+            self.ca.assert_that_pv_is("CH{:02d}:DI".format(test_channel), 0.0)
+
+    @parameterized.expand([
+        ("CH{:02d}".format(channel), channel) for channel in CHANNELS
+    ])
+    def test_WHEN_an_AI_input_is_changed_THEN_that_channel_readback_updates(self, _, channel):
+        self._lewis.backdoor_run_function_on_device("set_ai", (channel, TEST_VALUE))
+
+        # Verify that the new value has been written and read back
+        for test_channel in CHANNELS:
+            self.ca.assert_that_pv_is("CH{:02d}:AI".format(test_channel), TEST_VALUE)
