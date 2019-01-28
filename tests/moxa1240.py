e050065ad1bd01a6cc4009542ae2063d9c5c0629
Alistair McGann <alistair.mcgann@tessella.com>
2019-01-28 17:05:36 +0000

    Remove duplicated tests

--- tests/moxa1240.py
+++ tests/moxa1240.py
@@ -4,11 +4,7 @@ import unittest
 from common_tests.moxa12XX import Moxa12XXBase
 
 from utils.test_modes import TestModes
-from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
-from utils.testing import get_running_lewis_and_ioc
-from parameterized import parameterized
-from itertools import product
 
 
 # Device prefix
@@ -47,19 +43,12 @@ TEST_MODES = [TestModes.DEVSIM, ]
 NUMBER_OF_CHANNELS = 8
 CHANNELS = range(NUMBER_OF_CHANNELS)
 
-RANGE_STATUSES = {0: "NORMAL",
-                  1: "BURNOUT",
-                  2: "OVERRANGE",
-                  3: "UNDERRANGE"}
-
 MIN_VOLT_MEAS = 0.0
 MAX_VOLT_MEAS = 10.0
 
 TEST_VALUES = [MIN_VOLT_MEAS,
                MAX_VOLT_MEAS,
                0.5*(MIN_VOLT_MEAS + MAX_VOLT_MEAS)]
-
-FLUSH_VALUE = 0
 
 
 class Moxa1240TestsFromBase(Moxa12XXBase, unittest.TestCase):
@@ -100,63 +89,3 @@ class Moxa1240TestsFromBase(Moxa12XXBase, unittest.TestCase):
     def get_channel_format(self):
         return CHANNEL_FORMAT
 
-
-class Moxa1240Tests(unittest.TestCase):
-    """
-    Tests for a moxa ioLogik e1240. (8x DC Voltage/Current measurements)
-    """
-
-    def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("moxa12xx", DEVICE_PREFIX)
-
-        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
-
-        # Write zero to each input register to clear the data in the emulator
-        flush_value = 0
-        self._lewis.backdoor_run_function_on_device("set_ir", (AI_REGISTER_OFFSET, [flush_value]*NUMBER_OF_CHANNELS))
-        for test_channel in CHANNELS:
-            self.ca.assert_that_pv_is_number("CH{:01d}:AI:RAW".format(test_channel), flush_value, tolerance=0.1)
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel, test_value) for channel, test_value in product(CHANNELS, TEST_VALUES)
-    ])
-    def test_WHEN_an_AI_input_is_changed_THEN_that_channel_readback_updates(self, _, channel, test_value):
-        self._lewis.backdoor_run_function_on_device("set_1240_voltage", (channel + AI_REGISTER_OFFSET, test_value))
-
-        self.ca.assert_that_pv_is_number("CH{:01d}:AI:RBV".format(channel), test_value, tolerance=0.1)
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel) for channel in CHANNELS
-    ])
-    def test_WHEN_device_voltage_is_below_low_limit_THEN_PV_shows_major_alarm(self, _, channel):
-        voltage_to_set = LOW_ALARM_LIMIT - 1.0
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:AI:RBV".format(channel), self.ca.Alarms.NONE)
-
-        self._lewis.backdoor_run_function_on_device("set_1240_voltage", (channel + AI_REGISTER_OFFSET, voltage_to_set))
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:AI:RBV".format(channel), self.ca.Alarms.MAJOR)
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel) for channel in CHANNELS
-    ])
-    def test_WHEN_device_voltage_is_above_high_limit_THEN_PV_shows_major_alarm(self, _, channel):
-        valid_voltage = 0.5*(HIGH_ALARM_LIMIT + LOW_ALARM_LIMIT)
-
-        self._lewis.backdoor_run_function_on_device("set_1240_voltage", (channel + AI_REGISTER_OFFSET, valid_voltage))
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:AI:RBV".format(channel), self.ca.Alarms.NONE)
-
-        voltage_to_set = HIGH_ALARM_LIMIT + 1.0
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:AI:RBV".format(channel), self.ca.Alarms.NONE)
-
-        self._lewis.backdoor_run_function_on_device("set_1240_voltage", (channel + AI_REGISTER_OFFSET, voltage_to_set))
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:AI:RBV".format(channel), self.ca.Alarms.MAJOR)
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel) for channel in CHANNELS
-    ])
-    def test_WHEN_a_channel_is_aliased_THEN_a_PV_with_that_alias_exists(self, _, channel):
-        self.ca.assert_that_pv_exists(IOCS[0]["macros"]["CHAN{:01d}NAME".format(channel)])
--- tests/moxa1262.py
+++ tests/moxa1262.py
@@ -2,11 +2,7 @@ from __future__ import division
 import unittest
 
 from utils.test_modes import TestModes
-from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
-from utils.testing import get_running_lewis_and_ioc
-from parameterized import parameterized
-from itertools import product
 
 from common_tests.moxa12XX import Moxa12XXBase
 
@@ -44,14 +40,7 @@ TEST_MODES = [TestModes.DEVSIM, ]
 NUMBER_OF_CHANNELS = 8
 REGISTERS_PER_CHANNEL = 2
 
-CHANNELS = range(NUMBER_OF_CHANNELS)
-
 AI_REGISTER_OFFSET = 0x810
-
-RANGE_STATUSES = {0: "NORMAL",
-                  1: "BURNOUT",
-                  2: "OVERRANGE",
-                  3: "UNDERRANGE"}
 
 LARGEST_OBSERVABLE_VALUE = 3.4e38
 SMALLEST_TYPE_K_MEAS = -180.0
@@ -97,79 +86,3 @@ class Moxa1262TestsFromBase(Moxa12XXBase, unittest.TestCase):
 
     def get_channel_format(self):
         return CHANNEL_FORMAT
-
-
-class Moxa1262Tests(unittest.TestCase):
-    """
-    Tests for a moxa ioLogik e1262. (8x Thermocopule measurements)
-    """
-
-    def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("moxa12xx", DEVICE_PREFIX)
-
-        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
-
-        # Sends a backdoor command to the device to reset all input registers (IRs) to 0
-        reset_value = 0
-        self._lewis.backdoor_run_function_on_device("set_ir",
-                                                    (AI_REGISTER_OFFSET,
-                                                     [reset_value]*NUMBER_OF_CHANNELS*REGISTERS_PER_CHANNEL))
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel, test_value) for channel, test_value in product(CHANNELS, TEST_VALUES)
-    ])
-    def test_WHEN_an_AI_input_is_changed_THEN_that_channel_readback_updates(self, _, channel, test_value):
-        channel_address = AI_REGISTER_OFFSET + REGISTERS_PER_CHANNEL * channel
-
-        self._lewis.backdoor_run_function_on_device("set_1262_temperature", (channel_address, test_value))
-
-        self.ca.assert_that_pv_is_number("CH{:01d}:TEMP".format(channel), test_value, tolerance=max(0.1, 0.1*test_value))
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel) for channel in CHANNELS
-    ])
-    def test_WHEN_device_temperature_is_below_low_limit_THEN_PV_shows_major_alarm(self, _, channel):
-        channel_address = AI_REGISTER_OFFSET + REGISTERS_PER_CHANNEL * channel
-
-        valid_temperature = 0.5*(HIGH_ALARM_LIMIT + LOW_ALARM_LIMIT)
-
-        self._lewis.backdoor_run_function_on_device("set_1262_temperature", (channel_address, valid_temperature))
-
-        self.ca.assert_that_pv_is_number("CH{:01d}:TEMP".format(channel), valid_temperature, tolerance=0.1*valid_temperature)
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:TEMP".format(channel), self.ca.Alarms.NONE)
-
-        temperature_to_set = LOW_ALARM_LIMIT - 10.0
-
-        self._lewis.backdoor_run_function_on_device("set_1262_temperature", (channel_address, temperature_to_set))
-
-        self.ca.assert_that_pv_is_number("CH{:01d}:TEMP".format(channel), temperature_to_set, tolerance=0.01*temperature_to_set)
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:TEMP".format(channel), self.ca.Alarms.MAJOR)
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel) for channel in CHANNELS
-    ])
-    def test_WHEN_device_temperature_is_above_high_limit_THEN_PV_shows_major_alarm(self, _, channel):
-        channel_address = AI_REGISTER_OFFSET + REGISTERS_PER_CHANNEL * channel
-
-        valid_temperature = 0.5*(HIGH_ALARM_LIMIT + LOW_ALARM_LIMIT)
-
-        self._lewis.backdoor_run_function_on_device("set_1262_temperature", (channel_address, valid_temperature))
-
-        self.ca.assert_that_pv_is_number("CH{:01d}:TEMP".format(channel), valid_temperature, tolerance=0.1*valid_temperature)
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:TEMP".format(channel), self.ca.Alarms.NONE)
-
-        temperature_to_set = HIGH_ALARM_LIMIT + 10.0
-
-        self._lewis.backdoor_run_function_on_device("set_1262_temperature", (channel_address, temperature_to_set))
-
-        self.ca.assert_that_pv_is_number("CH{:01d}:TEMP".format(channel), temperature_to_set, tolerance=0.01*temperature_to_set)
-
-        self.ca.assert_that_pv_alarm_is("CH{:01d}:TEMP".format(channel), self.ca.Alarms.MAJOR)
-
-    @parameterized.expand([
-        ("CH{:01d}".format(channel), channel) for channel in CHANNELS
-    ])
-    def test_WHEN_a_channel_is_aliased_THEN_a_PV_with_that_alias_exists(self, _, channel):
-        self.ca.assert_that_pv_exists(CHANNEL_FORMAT.format(channel))
