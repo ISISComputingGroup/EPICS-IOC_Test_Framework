84de92faa28642160555e85182cd6309c26fa361
aaronlong <aaron.long@stfc.ac.uk>
2019-02-15 18:53:19 +0000

    Removed Devsim components

--- tests/nimatro.py
+++ tests/nimatro.py
@@ -3,22 +3,19 @@ import unittest
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc
 
 
 DEVICE_PREFIX = "NIMATRO_01"
-DEVICE_NAME = "nimatro"
 
 IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("NIMATRO"),
         "macros": {},
-        "emulator": DEVICE_NAME,
     },
 ]
 
-TEST_MODES = [TestModes.RECSIM]
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 
 
 class NimatroTests(unittest.TestCase):
@@ -26,18 +23,23 @@ class NimatroTests(unittest.TestCase):
     Tests for the NIMA Trough IOC.
     """
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc(DEVICE_NAME, DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
         self.ca.assert_that_pv_exists("CONTROL:START", timeout=30)
 
     def test_GIVEN_running_ioc_WHEN_set_target_area_sp_THEN_target_area_updated(self):
-        expected_value = 100
-        self.ca.set_pv_value("AREA:SP", expected_value)
+        expected_value = 41.15
+        self.ca.set_pv_value("AREA:TARGET:SP", expected_value)
 
-        self.ca.assert_that_pv_is("AREA", expected_value)
+        self.ca.assert_that_pv_is("AREA:TARGET:SP:RBV", expected_value)
 
     def test_GIVEN_running_ioc_WHEN_set_target_pressure_sp_THEN_target_pressure_updated(self):
-        expected_value = 100
-        self.ca.set_pv_value("PRESSURE:SP", expected_value)
+        expected_value = 55.6
+        self.ca.set_pv_value("PRESSURE:TARGET:SP", expected_value)
 
-        self.ca.assert_that_pv_is("PRESSURE", expected_value)
+        self.ca.assert_that_pv_is("PRESSURE:TARGET", expected_value)
+
+    def test_GIVEN_running_ioc_WHEN_set_target_speed_sp_THEN_target_speed_updated(self):
+        expected_value = 12.0
+        self.ca.set_pv_value("SPEED:SP", expected_value)
+
+        self.ca.assert_that_pv_is("SPEED", expected_value)
