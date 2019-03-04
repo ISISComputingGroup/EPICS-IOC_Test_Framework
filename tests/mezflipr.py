6541585c3cbb59027076f4317fb0cf624aaa8818
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-03-04 13:47:09 +0000

    Run in recsim

--- tests/mezflipr.py
+++ tests/mezflipr.py
@@ -29,7 +29,7 @@ IOCS = [
 ]
 
 
-TEST_MODES = [TestModes.DEVSIM]
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 
 
 class MezfliprTests(unittest.TestCase):
@@ -37,7 +37,7 @@ class MezfliprTests(unittest.TestCase):
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc(EMULATOR_NAME, DEVICE_PREFIX)
 
-        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_timeout=30)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
     def test_WHEN_ioc_is_started_THEN_ioc_is_not_disabled(self):
         self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
