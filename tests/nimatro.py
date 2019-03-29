6a7a6baab38b40771e7ebbc5cf31a6d3e82ed514
aaronlong <aaron.long@stfc.ac.uk>
2019-03-29 09:49:09 +0000

    Added parameterized tests

--- tests/nimatro.py
+++ tests/nimatro.py
@@ -32,35 +32,35 @@ class NimatroTests(unittest.TestCase):
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
         self.ca.assert_that_pv_exists("CONTROL:START", timeout=30)
 
-    @parameterized.expand([('low limit', 36), ('random1', 93), ('random2', 230), ('high limit', 247)])
+    @parameterized.expand([('low limit', 36), ('test_value_1', 93), ('test_value_2', 230), ('high limit', 247)])
     def test_GIVEN_running_ioc_WHEN_set_target_area_sp_THEN_target_area_updated(self, _, area):
         expected_value = area
-        self.ca.set_pv_value("AREA:TARGET:SP", expected_value)
+        self.ca.set_pv_value("AREA:SP", expected_value)
 
-        self.ca.assert_that_pv_is("AREA:TARGET:SP:RBV", expected_value)
+        self.ca.assert_that_pv_is("AREA:SP:RBV", expected_value)
 
     @parameterized.expand([('low limit check', 35.0), ('high limit check', 278.0)])
     def test_GIVEN_running_ioc_WHEN_set_invalid_target_area_sp_THEN_record_bounded(self, _, area):
         expected_value = area
-        self.ca.set_pv_value("AREA:TARGET:SP", expected_value)
+        self.ca.set_pv_value("AREA:SP", expected_value)
 
-        self.ca.assert_that_pv_is_within_range("AREA:TARGET:SP:RBV", 36, 247)
+        self.ca.assert_that_pv_is_within_range("AREA:SP:RBV", 36, 247)
 
-    @parameterized.expand([('low limit', -75.0), ('random1', 10.0), ('random2', 34.2), ('high limit', 75.0)])
+    @parameterized.expand([('low limit', -75.0), ('test_value_1', 10.0), ('test_value_2', 34.2), ('high limit', 75.0)])
     def test_GIVEN_running_ioc_WHEN_set_target_pressure_sp_THEN_target_pressure_updated(self, _, pressure):
         expected_value = pressure
-        self.ca.set_pv_value("PRESSURE:TARGET:SP", expected_value)
+        self.ca.set_pv_value("PRESSURE:SP", expected_value)
 
-        self.ca.assert_that_pv_is("PRESSURE:TARGET", expected_value)
+        self.ca.assert_that_pv_is("PRESSURE:SP:RBV", expected_value)
 
     @parameterized.expand([('low limit check', -76.0), ('high limit check', 76.0)])
     def test_GIVEN_running_ioc_WHEN_set_invalid_target_pressure_sp_THEN_record_bounded(self, _, pressure):
         expected_value = pressure
-        self.ca.set_pv_value("PRESSURE:TARGET:SP", expected_value)
+        self.ca.set_pv_value("PRESSURE:SP", expected_value)
 
-        self.ca.assert_that_pv_is_within_range("PRESSURE:TARGET:SP:RBV", -75, 75)
+        self.ca.assert_that_pv_is_within_range("PRESSURE:SP", -75, 75)
 
-    @parameterized.expand([('low limit', -174.2), ('random1', -23.5), ('random2', 34.2), ('high limit', 174.2)])
+    @parameterized.expand([('low limit', -174.2), ('test_value_1', -23.5), ('test_value_2', 34.2), ('high limit', 174.2)])
     def test_GIVEN_running_ioc_WHEN_set_target_speed_sp_THEN_target_speed_updated(self, _, speed):
         expected_value = speed
         self.ca.set_pv_value("SPEED:SP", expected_value)
