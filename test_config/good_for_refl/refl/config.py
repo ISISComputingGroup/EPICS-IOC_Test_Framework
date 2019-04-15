de8560cc6e0840edf730f6716b06af0df4fa7545
ThomasLohnert <thomas.lohnert@stfc.ac.uk>
2019-04-15 16:11:52 +0100

    Tests for inbeam parameters

--- test_config/good_for_refl/refl/config.py
+++ test_config/good_for_refl/refl/config.py
@@ -17,14 +17,19 @@ def get_beamline():
     """
     Returns: a beamline object describing the current beamline setup
     """
-    # components
+    # COMPONENTS
     s1 = Component("s1", PositionAndAngle(0.0, 1*SPACING, 90))
     s3 = Component("s3", PositionAndAngle(0.0, 3*SPACING, 90))
     detector = TiltingComponent("Detector", PositionAndAngle(0.0, 4*SPACING, 90))
     theta = ThetaComponent("ThetaComp", PositionAndAngle(0.0, 2*SPACING, 90), [detector])
-    detector_for_init = TiltingComponent("det_init_comp", PositionAndAngle(0.0, 7*SPACING, 90))
-    theta_for_init = ThetaComponent("theta_init_comp", PositionAndAngle(0.0, 6*SPACING, 90), [detector_for_init])
-    comps = [s1, theta, s3, detector, theta_for_init, detector_for_init]
+
+    # for init tests
+    out_comp = Component("out_comp", PositionAndAngle(0.0, 6*SPACING, 90))
+    in_comp = Component("in_comp", PositionAndAngle(0.0, 7*SPACING, 90))
+    detector_for_init = TiltingComponent("det_init_comp", PositionAndAngle(0.0, 9*SPACING, 90))
+    theta_for_init = ThetaComponent("theta_init_comp", PositionAndAngle(0.0, 8*SPACING, 90), [detector_for_init])
+
+    comps = [s1, theta, s3, detector, out_comp, in_comp, theta_for_init, detector_for_init]
 
     # BEAMLINE PARAMETERS
     slit1_pos = TrackingPosition("S1", s1, True)
@@ -35,12 +40,16 @@ def get_beamline():
     s3_enabled = InBeamParameter("s3_enabled", s3)
 
     # for init tests
+    is_out = InBeamParameter("is_out", out_comp, autosave=False)
+    out_pos = TrackingPosition("out_pos", out_comp, autosave=False)
+    is_in = InBeamParameter("is_in", in_comp, autosave=False)
+    in_pos = TrackingPosition("in_pos", in_comp, autosave=False)
     theta_auto = AngleParameter("theta_auto", theta_for_init, autosave=True)
     init = TrackingPosition("init", detector_for_init, autosave=False)
     init_auto = TrackingPosition("init_auto", detector_for_init, autosave=True)
 
     params_all = [s3_enabled, slit1_pos, theta_ang, slit3_pos, detector_position, detector_angle,
-                  theta_auto, init, init_auto]
+                  is_out, out_pos, is_in, in_pos, theta_auto, init, init_auto]
 
     # Do not want parameters for init tests to be moved by other tests.
     params_without_init = [s3_enabled, slit1_pos, theta_ang, slit3_pos, detector_position, detector_angle,
@@ -49,8 +58,10 @@ def get_beamline():
     drivers = [DisplacementDriver(s1, MotorPVWrapper("MOT:MTR0101")),
                DisplacementDriver(s3, MotorPVWrapper("MOT:MTR0102"), OUT_POSITION),
                DisplacementDriver(detector, MotorPVWrapper("MOT:MTR0103")),
-               DisplacementDriver(detector_for_init, MotorPVWrapper("MOT:MTR0105")),
-               AngleDriver(detector, MotorPVWrapper("MOT:MTR0104"))]
+               AngleDriver(detector, MotorPVWrapper("MOT:MTR0104")),
+               DisplacementDriver(out_comp, MotorPVWrapper("MOT:MTR0105"), OUT_POSITION, tolerance_on_out_of_beam_position=1),
+               DisplacementDriver(in_comp, MotorPVWrapper("MOT:MTR0106"), OUT_POSITION, tolerance_on_out_of_beam_position=1),
+               DisplacementDriver(detector_for_init, MotorPVWrapper("MOT:MTR0107"))]
 
     # MODES
     nr_inits = {}
--- tests/refl.py
+++ tests/refl.py
@@ -9,8 +9,10 @@ from utils.test_modes import TestModes
 
 GALIL_ADDR = "128.0.0.0"
 DEVICE_PREFIX = "REFL"
-DET_INIT_OFFSET = 5.0
-DET_INIT_OFFSET_AUTOSAVE = 1.0
+OUT_COMP_INIT_POS = -5.5
+IN_COMP_INIT_POS = 1.0
+DET_INIT_POS = 5.0
+DET_INIT_POS_AUTOSAVE = 1.0
 
 REFL_PATH = os.path.join(EPICS_TOP, "ISIS", "inst_servers", "master")
 GALIL_PREFIX = "GALIL_01"
@@ -25,7 +27,9 @@ IOCS = [
             "MTRCTRL": "1",
         },
         "inits": {
-            "MTR0105.VAL": DET_INIT_OFFSET
+            "MTR0105.VAL": OUT_COMP_INIT_POS,
+            "MTR0106.VAL": IN_COMP_INIT_POS,
+            "MTR0107.VAL": DET_INIT_POS
         }
     },
     {
@@ -161,11 +165,25 @@ class ReflTests(unittest.TestCase):
                 self.ca.set_pv_value("BL:MODE:SP", expected_value)
 
     def test_GIVEN_theta_init_to_non_zero_and_det_pos_not_autosaved_WHEN_initialising_det_pos_THEN_det_pos_sp_is_initialised_to_rbv_minus_offset_from_theta(self):
-        expected_value = DET_INIT_OFFSET - SPACING  # angle between theta component and detector is 45 deg
+        expected_value = DET_INIT_POS - SPACING  # angle between theta component and detector is 45 deg
 
         self.ca.assert_that_pv_is_number("PARAM:INIT:SP:RBV", expected_value)
 
     def test_GIVEN_theta_is_non_zero_and_param_is_autosaved_WHEN_initialising_detector_height_param_THEN_param_sp_is_initialised_to_autosave_value(self):
-        expected_value = DET_INIT_OFFSET_AUTOSAVE
+        expected_value = DET_INIT_POS_AUTOSAVE
 
         self.ca.assert_that_pv_is_number("PARAM:INIT_AUTO:SP:RBV", expected_value)
+
+    def test_GIVEN_component_out_of_beam_WHEN_starting_up_ioc_THEN_inbeam_sp_false_and_pos_sp_zero(self):
+        expected_inbeam = "OUT"
+        expected_pos = 0.0
+
+        self.ca.assert_that_pv_is("PARAM:IS_OUT:SP:RBV", expected_inbeam)
+        self.ca.assert_that_pv_is("PARAM:OUT_POS:SP:RBV", expected_pos)
+
+    def test_GIVEN_component_in_beam_WHEN_starting_up_ioc_THEN_inbeam_sp_true_and_pos_sp_accurate(self):
+        expected_inbeam = "IN"
+        expected_pos = IN_COMP_INIT_POS
+
+        self.ca.assert_that_pv_is("PARAM:IS_IN:SP:RBV", expected_inbeam)
+        self.ca.assert_that_pv_is("PARAM:IN_POS:SP:RBV", expected_pos)
