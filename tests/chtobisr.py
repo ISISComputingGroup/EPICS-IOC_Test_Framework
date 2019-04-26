41f0d2e4295e7014da635321acb2e55b381285cd
David Keymer <david.keymer@stfc.ac.uk>
2019-04-26 12:21:00 +0100

    Added tests for RST command

--- tests/chtobisr.py
+++ tests/chtobisr.py
@@ -44,3 +44,23 @@ class ChtobisrTests(unittest.TestCase):
     def test_GIVEN_ID_requested_WHEN_device_disconnected_THEN_alarm_is_raised(self):
         self._lewis.backdoor_set_on_device("connected", False)
         self.ca.assert_that_pv_alarm_is("ID", self.ca.Alarms.INVALID)
+
+    @skip_if_recsim("Lewis backdoor not available in RecSim")
+    def test_GIVEN_reset_requested_THEN_emulator_is_reset(self):
+        self._lewis.backdoor_set_on_device("interlock", "ON")
+        self.ca.assert_that_pv_is("INTERLOCK:STAT", "ON")
+        self._lewis.backdoor_run_function_on_device("reset")
+        self.ca.assert_that_pv_is("INTERLOCK:STAT", "OFF")
+
+    @skip_if_recsim("Lewis backdoor not available in RecSim")
+    def test_GIVEN_reset_requested_WHEN_device_connected_THEN_device_is_reset(self):
+        self._lewis.backdoor_set_on_device("interlock", "ON")
+        self.ca.assert_that_pv_is("INTERLOCK:STAT", "ON")
+        self.ca.set_pv_value("RESET", "1")
+        self.ca.assert_that_pv_is("INTERLOCK:STAT", "OFF")
+
+    @skip_if_recsim("Lewis backdoor not available in RecSim")
+    def test_GIVEN_interlock_status_requested_WHEN_device_connected_THEN_interlock_status_is_returned(self):
+        expected_value = "ON"
+        self._lewis.backdoor_set_on_device("interlock", expected_value)
+        self.ca.assert_that_pv_is("INTERLOCK:STAT", expected_value)
