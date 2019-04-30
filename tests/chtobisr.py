f5e2887cae897568dead89438b467bc595261958
David Keymer <david.keymer@stfc.ac.uk>
2019-04-30 16:17:46 +0100

    Added basic tests for status and fault codes

--- tests/chtobisr.py
+++ tests/chtobisr.py
@@ -1,10 +1,11 @@
 import unittest
 
+from parameterized import parameterized
+
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
-
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, parameterized_list
 
 DEVICE_PREFIX = "CHTOBISR_01"
 EMULATOR = "chtobisr"
@@ -64,3 +65,31 @@ class ChtobisrTests(unittest.TestCase):
         expected_value = "ON"
         self._lewis.backdoor_set_on_device("interlock", expected_value)
         self.ca.assert_that_pv_is("INTERLOCK:STAT", expected_value)
+
+    @skip_if_recsim("Lewis backdoor not available in RecSim")
+    def test_GIVEN_status_requested_WHEN_device_connected__AND_laser_fault_set_THEN_correct_status_code_is_returned(self):
+        expected_value = "00000001"
+        self._lewis.backdoor_run_function_on_device("backdoor_set_status", ["laser_fault", True])
+        self.ca.assert_that_pv_is("STAT", expected_value)
+
+    @skip_if_recsim("Lewis backdoor not available in RecSim")
+    def test_GIVEN_faults_requested_WHEN_device_connected__AND_base_plate_temp_fault_set_THEN_correct_fault_code_is_returned(self):
+        expected_value = "00000001"
+        self._lewis.backdoor_run_function_on_device("backdoor_set_fault", ["base_plate_temp_fault", True])
+        self.ca.assert_that_pv_is("FAULT", expected_value)
+
+    # @skip_if_recsim("Lewis backdoor not available in RecSim")
+    # def test_GIVEN_base_plate_temperature_fault_active_WHEN_read_faults_THEN_pv_reads_fault_active(self):
+    #     for state in [False, True]:
+    #         self._lewis.backdoor_run_function_on_device("backdoor_set_fault", ["base_plate_temp_fault", state])
+    #         self.ca.assert_that_pv_is("FAULT:BASEPLATETEMP", state)
+    #
+    # @parameterized.expand(parameterized_list([
+    #     ("base_plate_temp_fault", "FAULT:BASEPLATETEMP"),
+    #     ("...", "FAULT:..."),
+    # ]))
+    # @skip_if_recsim("Lewis backdoor not available in RecSim")
+    # def test_GIVEN_fault_active_WHEN_read_faults_THEN_pv_reads_fault_active(self, _, backdoor_name, pv_name):
+    #     for state in [False, True]:
+    #         self._lewis.backdoor_run_function_on_device("backdoor_set_fault", ["backdoor_name", state])
+    #         self.ca.assert_that_pv_is(pv_name, state)
