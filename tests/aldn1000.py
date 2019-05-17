c93190bfc83b924007c86ce40b0b0d12b2d44899
aaronlong <aaron.long@stfc.ac.uk>
2019-05-17 10:31:49 +0100

    Added RECSIM tests

--- tests/aldn1000.py
+++ tests/aldn1000.py
@@ -21,7 +21,7 @@ IOCS = [
 ]
 
 
-TEST_MODES = [TestModes.DEVSIM]
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 
 
 class Aldn1000Tests(unittest.TestCase):
@@ -34,6 +34,7 @@ class Aldn1000Tests(unittest.TestCase):
         self._lewis.backdoor_run_function_on_device("reset")
 
     @parameterized.expand([('Value 1', 12.12), ('Value 2', 1.123), ('Value 3', 123.0)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_diameter_WHEN_set_diameter_THEN_new_diameter_set(self, _, value):
         expected_diameter = value
         self.ca.set_pv_value("DIAMETER:SP", expected_diameter)
@@ -41,6 +42,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("DIAMETER", expected_diameter, timeout=2)
 
     @parameterized.expand([('Value 1', 12345), ('Value 2', 1234), ('Value 3', 77424)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_invalid_high_diameter_WHEN_set_diameter_THEN_diameter_set_limit_returned(self, _, value):
         invalid_diameter = value
         expected_diameter = 1000.00
@@ -49,6 +51,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("DIAMETER", expected_diameter, timeout=2)
 
     @parameterized.expand([('Value 1', -2345), ('Value 2', -1234), ('Value 3', -0676424)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_invalid_low_diameter_WHEN_set_diameter_THEN_diameter_set_limit_returned(self, _, value):
         invalid_diameter = value
         expected_diameter = 0.00
@@ -57,6 +60,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("DIAMETER", expected_diameter, timeout=2)
 
     @parameterized.expand([('Value 1', 14.1), ('Value 2', 24.23), ('Value 3', 30)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_diameter_above_14mm_WHEN_set_diameter_THEN_volume_units_changed(self, _, value):
         set_diameter = value
         expected_units = 'mL'
@@ -65,6 +69,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("VOLUME:UNITS", expected_units, timeout=2)
 
     @parameterized.expand([('Value 1', 14.0), ('Value 2', 10.0), ('Value 3', 5.0)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_diameter_below_or_eq_14mm_WHEN_set_diameter_THEN_volume_units_changed(self, _, value):
         set_diameter = value
         expected_units = 'uL'
@@ -73,6 +78,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("VOLUME:UNITS", expected_units, timeout=2)
 
     @parameterized.expand([('Value 1', 0.123), ('Value 2', 1.342), ('Value 3', 12.34)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_volume_WHEN_set_volume_THEN_new_volume_set(self, _, value):
         expected_volume = value
         self.ca.set_pv_value("VOLUME:SP", expected_volume)
@@ -86,6 +92,7 @@ class Aldn1000Tests(unittest.TestCase):
 
         self.ca.assert_that_pv_is("DIRECTION", expected_direction)
 
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_existing_direction_WHEN_set_reverse_direction_THEN_direction_reversed(self):
         initial_direction = self.ca.get_pv_value("DIRECTION")
         if initial_direction == 'Infuse':
@@ -97,6 +104,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("DIRECTION", expected_direction, timeout=2)
 
     @parameterized.expand([('Value 1', 0.123), ('Value 2', 1.342), ('Value 3', 12.34)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_rate_WHEN_set_rate_THEN_new_rate_set(self, _, value):
         expected_rate = value
         self.ca.set_pv_value("RATE:SP", expected_rate)
@@ -104,6 +112,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("RATE", expected_rate)
 
     @parameterized.expand([('Value 1', 2123), ('Value 2', 1411.342), ('Value 3', 1222.34)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_invalid_high_rate_WHEN_set_rate_THEN_rate_high_limit_set(self, _, value):
         invalid_rate = value
         expected_rate = 1000.0
@@ -112,6 +121,7 @@ class Aldn1000Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("RATE", expected_rate)
 
     @parameterized.expand([('Value 1', -9085), ('Value 2', -0.123342), ('Value 3', -5226.31234)])
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_new_invalid_low_rate_WHEN_set_rate_THEN_rate_high_limit_set(self, _, value):
         invalid_rate = value
         expected_rate = 0.0
@@ -119,12 +129,14 @@ class Aldn1000Tests(unittest.TestCase):
 
         self.ca.assert_that_pv_is("RATE", expected_rate)
 
+    @skip_if_recsim("Unable to use lewis backdoor in RECSIM")
     def test_GIVEN_an_infused_volume_WHEN_get_volume_dispensed_THEN_infused_volume_returned(self):
         expected_infusion_volume = 1.123
         self._lewis.backdoor_set_on_device("volume_infused", expected_infusion_volume)
 
         self.ca.assert_that_pv_is("VOLUME:INF", expected_infusion_volume, timeout=2)
 
+    @skip_if_recsim("Unable to use lewis backdoor in RECSIM")
     def test_GIVEN_infusion_volume_dispensed_WHEN_clear_infused_volume_dispensed_THEN_volume_cleared(self):
         infused_volume_dispensed = 2.342
         expected_volume_dispensed = 0.0
@@ -134,6 +146,7 @@ class Aldn1000Tests(unittest.TestCase):
 
         self.ca.assert_that_pv_is("VOLUME:INF", expected_volume_dispensed)
 
+    @skip_if_recsim("Unable to use lewis backdoor in RECSIM")
     def test_GIVEN_withdrawn_volume_dispensed_WHEN_clear_withdrawn_volume_dispensed_THEN_volume_cleared(self):
         withdrawn_volume_dispensed = 93.12
         expected_volume_dispensed = 0.0
@@ -143,12 +156,14 @@ class Aldn1000Tests(unittest.TestCase):
 
         self.ca.assert_that_pv_is("VOLUME:WDR", expected_volume_dispensed)
 
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_pump_off_WHEN_set_pump_on_THEN_pump_turned_on(self):
         status_mode = 'Pumping Program Stopped'
         self.ca.set_pv_value("RUN:SP", "Run")
 
         self.ca.assert_that_pv_is_not("STATUS", status_mode)
 
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_pump_on_WHEN_set_pump_off_THEN_pump_paused(self):
         status_mode = 'Pumping Program Paused'
         self.ca.set_pv_value("RUN:SP", "Run")
@@ -156,6 +171,7 @@ class Aldn1000Tests(unittest.TestCase):
 
         self.ca.assert_that_pv_is("STATUS", status_mode)
 
+    @skip_if_recsim("Unable to use lewis backdoor in RECSIM")
     def test_GIVEN_given_program_function_WHEN_program_function_changed_THEN_program_function_updated(self):
         expected_function = 'INCR'
         self._lewis.backdoor_set_on_device('program_function', expected_function)
@@ -171,6 +187,7 @@ class Aldn1000Tests(unittest.TestCase):
 
         self.ca.assert_that_pv_is("ERROR", expected_value, timeout=2)
 
+    @skip_if_recsim("Not supported in RECSIM")
     def test_GIVEN_device_connected_WHEN_get_status_THEN_device_status_returned(self):
         expected_status = 'Pumping Program Stopped'
 
