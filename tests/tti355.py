311e591b79d45e17699e8aebec17134fd312fc50
Kirkham <sophie.kirkham@stfc.ac.uk>
2019-05-28 13:36:50 +0100

    added additional tests

--- tests/tti355.py
+++ tests/tti355.py
@@ -58,42 +58,77 @@ class Tti355Tests(unittest.TestCase):
         self.ca.set_pv_value("OUTPUTSTATUS:SP", status)
         self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", status)
     
-    @skip_if_recsim("Relies on emulator logic")
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
     def test_WHEN_voltage_setpoint_is_set_outside_max_limits_THEN_device_in_error_state(self):
         self.ca.set_pv_value("VOLTAGE:SP", 38.0)
         self.ca.assert_that_pv_is("ERROR", "Cmd outside limits")
     
-    @skip_if_recsim("Relies on emulator logic")
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
     def test_WHEN_current_setpoint_is_set_outside_max_limits_THEN_device_in_error_state(self):
         self.ca.set_pv_value("CURRENT:SP", 7.0)
         self.ca.assert_that_pv_is("ERROR", "Cmd outside limits")
-       
-
+    
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
     def test_WHEN_identity_requested_THEN_correct_identity_returned(self):
         expected_identity = "THURLBY EX355P, <version>"
         self.ca.assert_that_pv_is("IDENT", expected_identity)
 
-    def test_WHEN_ioc_in_error_state
-    xpected_value = 300
-        self._lewis.backdoor_set_on_device("pressure", expected_value)
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
+    def test_WHEN_ioc_in_error_state_2_THEN_correct_error_state_returned(self):
+        expected_value = "Cmd outside limits"
+        self._lewis.backdoor_set_on_device("error", "ERR 2")
+        self.ca.assert_that_pv_is("ERROR", expected_value, timeout=1)
 
-        self.ca.assert_that_pv_is("PRESSURE", expected_value, timeout=1)
-    """
-    def test_GIVEN_set_output_conditions_WHEN_the_output_is_on_THEN_readback_voltage_is_close_to_the_voltage_setpoint(self):
-        self.set_init_state(10, 0.1, 20, 1, "On")
-        self.ca.assert_that_pv_is("OUTPUT", "On")
-        for volt in [0, 5, 10]:
-            self.ca.set_pv_value("VOLTAGE:SP", volt)
-            self.ca.assert_that_pv_is("OUTPUT", "On")
-            self.ca.assert_that_pv_is("VOLTAGE:SP:RBV", volt)
-            self.ca.assert_that_pv_is_number("VOLTAGE", volt, tolerance=0.1)
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
+    def test_WHEN_ioc_in_error_state_1_THEN_correct_error_state_returned(self):
+        expected_value = "Cmd not recognised"
+        self._lewis.backdoor_set_on_device("error", "ERR 1")
+        self.ca.assert_that_pv_is("ERROR", expected_value, timeout=1)
+    
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
+    def test_WHEN_ioc_not_in_error_state_THEN_correct_error_state_returned(self):
+        expected_value = "No error"
+        self._lewis.backdoor_set_on_device("error", "ERR 0")
+        self.ca.assert_that_pv_is("ERROR", expected_value, timeout=1)
 
-    def test_GIVEN_set_output_conditions_WHEN_the_output_is_on_THEN_readback_current_is_close_to_the_current_setpoint(self):
-        self.set_init_state(10, 0.1, 20, 1, "On")
-        self.ca.assert_that_pv_is("OUTPUT", "On")
-        for curr in [0, 0.1, 0.2]:
-            self.ca.set_pv_value("CURRENT:SP", curr)
-            self.ca.assert_that_pv_is("OUTPUT", "On")
-            self.ca.assert_that_pv_is("CURRENT:SP:RBV", curr)
-            self.ca.assert_that_pv_is_number("CURRENT", curr, tolerance=0.01)
-    """
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
+    def test_WHEN_ioc_in_constant_current_mode_THEN_correct_mode_returned(self):
+        expected_value = "Constant Current"
+        self._lewis.backdoor_set_on_device("output_mode", "M CC")
+        self.ca.assert_that_pv_is("OUTPUTMODE", expected_value, timeout=1)
+
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
+    def test_WHEN_ioc_in_constant_voltage_mode_THEN_correct_mode_returned(self):
+        expected_value = "Constant Voltage"
+        self._lewis.backdoor_set_on_device("output_mode", "M CV")
+        self.ca.assert_that_pv_is("OUTPUTMODE", expected_value, timeout=1)
+    
+    @parameterized.expand([
+        [0,],
+        [5,],
+        [10,]
+    ])
+    def test_GIVEN_set_output_conditions_WHEN_the_output_is_on_THEN_readback_voltage_is_close_to_the_voltage_setpoint(self, voltage):
+        
+        self.ca.set_pv_value("OUTPUTSTATUS:SP", "ON")
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "ON")
+        self.ca.set_pv_value("VOLTAGE:SP", voltage)
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "ON")
+        self.ca.assert_that_pv_is("VOLTAGE:SP:RBV", voltage)
+        self.ca.assert_that_pv_is_number("VOLTAGE", voltage, tolerance=0.1)
+  
+    @parameterized.expand([
+        [0],
+        [0.1],
+        [0.2]
+    ])
+    def test_GIVEN_set_output_conditions_WHEN_the_output_is_on_THEN_readback_current_is_close_to_the_current_setpoint(self, current):
+        self.ca.set_pv_value("OUTPUTSTATUS:SP", "ON")
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "ON")
+
+        self.ca.set_pv_value("CURRENT:SP", current)
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "ON")
+        self.ca.assert_that_pv_is("CURRENT:SP:RBV", current)
+        self.ca.assert_that_pv_is_number("CURRENT", current, tolerance=0.01)
+  
+ 
