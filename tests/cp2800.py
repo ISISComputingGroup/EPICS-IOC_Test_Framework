74f59470a7d27a103fdbf475724220eef15fe5a1
Liam <02liamp@gmail.com>
2019-05-31 16:24:47 +0100

    Added CP2800 tests

---
+++ tests/cp2800.py
@@ -0,0 +1,57 @@
+import unittest
+
+from parameterized import parameterized
+
+from utils.test_modes import TestModes
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+
+from utils.ioc_launcher import IOCRegister, IocLauncher
+from utils.testing import get_running_lewis_and_ioc, parameterized_list
+
+DEVICE_PREFIX = "CP2800_01"
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("CP2800"),
+        "macros": {},
+    },
+]
+
+TEST_MODES = [TestModes.RECSIM]
+
+
+class CP2800StatusTests(unittest.TestCase):
+
+    def setUp(self):
+        _, self._ioc = get_running_lewis_and_ioc(None, DEVICE_PREFIX)
+        self.assertIsNotNone(self._ioc)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+
+    def test_GIVEN_compressor_elapsed_minutes_THEN_alarm_correct(self):
+        tests = [(-1, self.ca.Alarms.MAJOR),
+                 (0, self.ca.Alarms.NONE),
+                 (1, self.ca.Alarms.NONE),
+                 (500001, self.ca.Alarms.MINOR),
+                 (1000001, self.ca.Alarms.MAJOR),
+                 ]
+        for test in tests:
+            self.ca.set_pv_value("SIM:ELAPSED", test[0])
+            self.ca.assert_that_pv_alarm_is("ELAPSED", test[1], 10)
+
+    def test_GIVEN_compressor_state_on_THEN_readback_correct(self):
+        self.ca.set_pv_value("SIM:POWER", 1)
+        self.ca.assert_that_pv_is("POWER", "On")
+
+    def test_GIVEN_compressor_state_off_THEN_readback_correct(self):
+        self.ca.set_pv_value("SIM:POWER", 0)
+        self.ca.assert_that_pv_is("POWER", "Off")
+
+    def test_GIVEN_error_value_THEN_readback_correct(self):
+        self.ca.set_pv_value("SIM:ERR", 1)
+        self.ca.assert_that_pv_is("ERR", 1, 10)
+
+    def test_GIVEN_error_value_THEN_alarm_correct(self):
+        self.ca.set_pv_value("SIM:ERR", 1)
+        self.ca.assert_that_pv_alarm_is("ERR", self.ca.Alarms.MINOR, 10)
