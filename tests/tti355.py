7ac14faedc9f574600a479898ccecb03187130a4
Kirkham <sophie.kirkham@stfc.ac.uk>
2019-06-25 16:49:33 +0100

    Amended tests for new funtionality

--- tests/tti355.py
+++ tests/tti355.py
@@ -125,17 +125,43 @@ class Tti355Tests(unittest.TestCase):
     def test_GIVEN_set_output_conditions_WHEN_the_output_is_on_THEN_readback_current_is_close_to_the_current_setpoint(self, current):
         self.ca.set_pv_value("OUTPUTSTATUS:SP", "ON")
         self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "ON")
-
         self._lewis.backdoor_set_on_device("output_mode", "M CI")
-
         self.ca.set_pv_value("CURRENT:SP", current)
         self.ca.assert_that_pv_is("CURRENT:SP:RBV", current)
         self.ca.assert_that_pv_is_number("CURRENT", current, tolerance=0.01)
 
     @skip_if_recsim("Behaviour cannot be simulated in Recsim")
-    def test_GIVEN_voltage_WHEN_current_limit_is_lower_than_potential_current_THEN_mode_is_CI(self):
+    def test_GIVEN_voltage_WHEN_current_limit_is_lower_than_potential_current_and_output_is_on_THEN_mode_is_CI_and_voltage_is_actual(self):
+        expected_voltage = 20
+        self.ca.set_pv_value("OUTPUTSTATUS:SP", "ON")
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "ON")
         self.ca.assert_that_pv_is("OUTPUTMODE", "Constant Voltage")
         self._lewis.backdoor_set_on_device("load_resistance", 8.00)
         self.ca.set_pv_value("CURRENT:SP", 2.5)
         self.ca.set_pv_value("VOLTAGE:SP", 25)
         self.ca.assert_that_pv_is("OUTPUTMODE", "Constant Current")
+        self.ca.assert_that_pv_is("VOLTAGE", expected_voltage)
+
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
+    def test_GIVEN_voltage_WHEN_current_limit_is_lower_than_potential_current_but_output_off_THEN_mode_is_CV_and_voltage_is_not_actual(self):
+        expected_voltage = 0
+        self.ca.set_pv_value("OUTPUTSTATUS:SP", "OFF")
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "OFF")
+        self.ca.assert_that_pv_is("OUTPUTMODE", "Constant Voltage")
+        self._lewis.backdoor_set_on_device("load_resistance", 8.00)
+        self.ca.set_pv_value("CURRENT:SP", 2.5)
+        self.ca.set_pv_value("VOLTAGE:SP", 25)
+        self.ca.assert_that_pv_is("OUTPUTMODE", "Constant Voltage")
+        self.ca.assert_that_pv_is("VOLTAGE", expected_voltage)
+
+    @skip_if_recsim("Behaviour cannot be simulated in Recsim")
+    def test_GIVEN_voltage_WHEN_current_limit_is_lower_than_potential_current_but_output_off_THEN_mode_is_CV_and_voltage_is_not_actual_but_close_to_sp(self):
+        expected_voltage = 10
+        self.ca.set_pv_value("OUTPUTSTATUS:SP", "ON")
+        self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", "ON")
+        self.ca.assert_that_pv_is("OUTPUTMODE", "Constant Voltage")
+        self._lewis.backdoor_set_on_device("load_resistance", 8.00)
+        self.ca.set_pv_value("CURRENT:SP", 2.5)
+        self.ca.set_pv_value("VOLTAGE:SP", expected_voltage)
+        self.ca.assert_that_pv_is("OUTPUTMODE", "Constant Voltage")
+        self.ca.assert_that_pv_is_number("VOLTAGE", expected_voltage, tolerance=0.1)
