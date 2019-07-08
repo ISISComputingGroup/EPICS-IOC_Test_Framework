a4fcc1fc698151cb60e4a6464b68404f6b6d3ca7
aaronlong <aaron.long@stfc.ac.uk>
2019-07-08 10:51:27 +0100

    Added IOC macro's to test cases

--- tests/tti355.py
+++ tests/tti355.py
@@ -9,11 +9,21 @@ from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
 DEVICE_PREFIX = "TTI355_01"
 DEVICE_NAME = "tti355"
 
+VOLT_LOW_LIMIT = 0.0
+VOLT_HIGH_LIMIT = 35.0
+CURR_LOW_LIMIT = 0.01
+CURR_HIGH_LIMIT = 5.0
+
 IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("TTI355"),
-        "macros": {},
+        "macros": {
+            "MIN_VOLT": VOLT_LOW_LIMIT,
+            "MAX_VOLT": VOLT_HIGH_LIMIT,
+            "MIN_CURR": CURR_LOW_LIMIT,
+            "MAX_CURR": CURR_HIGH_LIMIT,
+        },
         "emulator": DEVICE_NAME,
     },
 ]
@@ -58,16 +68,16 @@ class Tti355Tests(unittest.TestCase):
         self.ca.assert_that_pv_is("OUTPUTSTATUS:SP:RBV", status)
 
     @parameterized.expand([
-        ("lt_low_limit", -123.0, "low_limit", 0.0),
-        ("gt_high_limit", 123.2, "high_limit", 35.0)])
+        ("lt_low_limit", VOLT_LOW_LIMIT-1.0, "low_limit", VOLT_LOW_LIMIT),
+        ("gt_high_limit", VOLT_HIGH_LIMIT+1, "high_limit", VOLT_HIGH_LIMIT)])
     @skip_if_recsim("Behaviour cannot be simulated in Recsim")
     def test_WHEN_voltage_setpoint_is_set_outside_max_limits_THEN_setpoint_within(self, case, case_value, limit, limit_value):
         self.ca.set_pv_value("VOLTAGE:SP", case_value)
         self.ca.assert_that_pv_is("VOLTAGE:SP", limit_value)
 
     @parameterized.expand([
-        ("lt_low_limit", -31.0, "low_limit", 0.01),
-        ("gt_high_limit", 34.2, "high_limit", 5.0)])
+        ("lt_low_limit", CURR_LOW_LIMIT-1, "low_limit", CURR_LOW_LIMIT),
+        ("gt_high_limit", CURR_HIGH_LIMIT+1, "high_limit", CURR_HIGH_LIMIT)])
     @skip_if_recsim("Behaviour cannot be simulated in Recsim")
     def test_WHEN_voltage_setpoint_is_set_outside_max_limits_THEN_setpoint_within(self, case, case_value, limit, limit_value):
         self.ca.set_pv_value("CURRENT:SP", case_value)
