b2b6bc9f5a66b142fe4ed97609a0d1625e5cd6f1
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-07-18 15:04:16 +0100

    Add initial heliox suite

---
+++ tests/heliox.py
@@ -0,0 +1,32 @@
+import unittest
+
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+
+DEVICE_PREFIX = "HELIOX_01"
+EMULATOR_NAME = "heliox"
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("HELIOX"),
+        "emulator": EMULATOR_NAME,
+    },
+]
+
+
+TEST_MODES = [TestModes.DEVSIM, TestModes.RECSIM]
+
+
+class HelioxTests(unittest.TestCase):
+    """
+    Tests for the heliox IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc(EMULATOR_NAME, DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_timeout=10)
+
+    def test_WHEN_ioc_is_started_THEN_it_is_not_disabled(self):
+        self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
