eb81fe259fdeee16564849193c20f1a5bf8dfd91
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-07-19 15:20:55 +0100

    Initial tests

--- tests/heliox.py
+++ tests/heliox.py
@@ -1,9 +1,11 @@
 import unittest
 
+from parameterized import parameterized
+
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, parameterized_list
 
 DEVICE_PREFIX = "HELIOX_01"
 EMULATOR_NAME = "heliox"
@@ -19,6 +21,8 @@ IOCS = [
 
 TEST_MODES = [TestModes.DEVSIM, TestModes.RECSIM]
 
+TEST_TEMPERATURES = [0.0, 0.01, 0.333, 300]
+
 
 class HelioxTests(unittest.TestCase):
     """
@@ -30,3 +34,11 @@ class HelioxTests(unittest.TestCase):
 
     def test_WHEN_ioc_is_started_THEN_it_is_not_disabled(self):
         self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
+
+    @parameterized.expand(parameterized_list(TEST_TEMPERATURES))
+    def test_WHEN_temperature_setpoint_is_set_THEN_setpoint_readback_updates(self, _, temp):
+        self.ca.assert_setting_setpoint_sets_readback(temp, set_point_pv="TEMP:SP", readback_pv="TEMP:SP:RBV")
+
+    # @parameterized.expand(parameterized_list(TEST_TEMPERATURES))
+    # def test_WHEN_temperature_setpoint_is_set_THEN_actual_temperature_updates(self, _, temp):
+    #     self.ca.assert_setting_setpoint_sets_readback(temp, set_point_pv="TEMP:SP", readback_pv="TEMP")
