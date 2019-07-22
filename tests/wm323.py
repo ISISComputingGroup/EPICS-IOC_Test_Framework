17e272aa1674bcfdc1ae08e23a60335d4fee1e45
adamb56789 <adamb56789@gmail.com>
2019-07-22 16:25:07 +0100

    Add basic ioc test

---
+++ tests/wm323.py
@@ -0,0 +1,33 @@
+import unittest
+
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, skip_if_devsim
+
+DEVICE_PREFIX = "WM323_01"
+
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("WM323"),
+        "emulator": "wm323",
+    },
+]
+
+
+TEST_MODES = [TestModes.DEVSIM, TestModes.RECSIM]
+
+
+class Itc503Tests(unittest.TestCase):
+    """
+    Tests for the wm323 IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc("wm323", DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_timeout=20)
+        self.ca.assert_that_pv_exists("DISABLE")
+
+    def test_WHEN_ioc_is_started_THEN_it_is_not_disabled(self):
+        self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
