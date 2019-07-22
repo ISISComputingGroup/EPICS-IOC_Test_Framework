d169ae9b58dc9185c826d4fd976bcf2dcbaf6755
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-07-22 09:45:07 +0100

    Add tests

--- tests/heliox_concise.py
+++ tests/heliox_concise.py
@@ -23,8 +23,10 @@ TEST_MODES = [TestModes.DEVSIM, TestModes.RECSIM]
 
 TEST_TEMPERATURES = [0.0, 0.01, 0.333, 300]
 
+CHANNELS = ["HE3SORB", "HE4POT", "HELOW", "HEHIGH"]
 
-class HelioxTests(unittest.TestCase):
+
+class HelioxConciseTests(unittest.TestCase):
     """
     Tests for the heliox IOC.
     """
@@ -39,6 +41,12 @@ class HelioxTests(unittest.TestCase):
     def test_WHEN_temperature_setpoint_is_set_THEN_setpoint_readback_updates(self, _, temp):
         self.ca.assert_setting_setpoint_sets_readback(temp, set_point_pv="TEMP:SP", readback_pv="TEMP:SP:RBV")
 
-    # @parameterized.expand(parameterized_list(TEST_TEMPERATURES))
-    # def test_WHEN_temperature_setpoint_is_set_THEN_actual_temperature_updates(self, _, temp):
-    #     self.ca.assert_setting_setpoint_sets_readback(temp, set_point_pv="TEMP:SP", readback_pv="TEMP")
+    @parameterized.expand(parameterized_list(TEST_TEMPERATURES))
+    def test_WHEN_temperature_setpoint_is_set_THEN_actual_temperature_updates(self, _, temp):
+        self.ca.assert_setting_setpoint_sets_readback(temp, set_point_pv="TEMP:SP", readback_pv="TEMP")
+
+    @skip_if_recsim("Lewis backdoor is not available in recsim")
+    def test_WHEN_temperature_fluctuates_between_stable_and_unstable_THEN_readback_updates(self):
+        for stable in [True, False, True]:  # Check both transitions
+            self._lewis.backdoor_set_on_device("temperature_stable", stable)
+            self.ca.assert_that_pv_is("STABILITY", "Stable" if stable else "Unstable")
