e1886c20a1f8a29d9ad43e7df75bb516fdb42bea
adamb56789 <adamb56789@gmail.com>
2019-07-24 15:31:34 +0100

    Parameterized tests

--- tests/wm323.py
+++ tests/wm323.py
@@ -1,9 +1,12 @@
 import unittest
 
+from parameterized import parameterized
+
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, skip_if_devsim
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+
 
 DEVICE_PREFIX = "WM323_01"
 
@@ -38,14 +41,17 @@ class Itc503Tests(unittest.TestCase):
     def test_WHEN_ioc_is_started_THEN_pump_type_pv_correct(self):
         self.ca.assert_that_pv_is("TYPE", "323Du")
 
-    def test_WHEN_speed_setpoint_is_sent_THEN_readback_updates(self):
-        self.ca.assert_setting_setpoint_sets_readback(42, 'SPEED')
+    @parameterized.expand([('On low limit', SPEED_LOW_LIMIT),
+                           ('Intermediate value', 42),
+                           ('On high limit', SPEED_HIGH_LIMIT)])
+    def test_WHEN_speed_setpoint_is_sent_THEN_readback_updates(self, _, value):
+        self.ca.assert_setting_setpoint_sets_readback(value, 'SPEED')
 
-    def test_WHEN_speed_setpoint_is_set_outside_max_limits_THEN_setpoint_within(self):
-        self.ca.set_pv_value("SPEED:SP", SPEED_LOW_LIMIT - 1)
-        self.ca.assert_that_pv_is("SPEED:SP", SPEED_LOW_LIMIT)
-        self.ca.set_pv_value("SPEED:SP", SPEED_HIGH_LIMIT + 1)
-        self.ca.assert_that_pv_is("SPEED:SP", SPEED_HIGH_LIMIT)
+    @parameterized.expand([('Low limit', SPEED_LOW_LIMIT, SPEED_LOW_LIMIT - 1),
+                           ('High limit', SPEED_HIGH_LIMIT, SPEED_HIGH_LIMIT + 1)])
+    def test_WHEN_speed_setpoint_is_set_outside_max_limits_THEN_setpoint_within(self, _, limit, value):
+        self.ca.set_pv_value("SPEED:SP", value)
+        self.ca.assert_that_pv_is("SPEED:SP", limit)
 
     def test_WHEN_direction_setpoint_is_sent_THEN_readback_updates(self):
         for mode in ["Clockwise", "Anti-Clockwise"]:
@@ -64,7 +70,7 @@ class Itc503Tests(unittest.TestCase):
     @skip_if_recsim("Requires emulator logic so not supported in RECSIM")
     def test_GIVEN_pump_on_WHEN_set_pump_off_THEN_pump_paused(self):
         self.ca.set_pv_value("RUN:SP", "Run")
-        self.ca.assert_that_pv_is("STATUS", "Running", timeout=2)
+        self.ca.assert_that_pv_is("STATUS", "Running")
 
         self.ca.set_pv_value("STOP:SP", "Stop")
 
