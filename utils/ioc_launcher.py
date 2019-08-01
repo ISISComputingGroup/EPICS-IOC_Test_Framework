79c71dd6e006ecf04ac72b12aa0451cefda0980d
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-08-01 16:23:37 +0100

    Print full PV when existence PV check fails

--- utils/ioc_launcher.py
+++ utils/ioc_launcher.py
@@ -63,7 +63,8 @@ class check_existence_pv:
         try:
             self.ca.assert_that_pv_exists(self.test_pv)
         except AssertionError as ex:
-            print("Warning, {} still does not exist after IOC start".format(self.test_pv))
+            full_pv = self.ca.create_pv_with_prefix(self.test_pv)
+            print("Warning, {} still does not exist after IOC start".format(full_pv))
 
 
 class IOCRegister(object):
