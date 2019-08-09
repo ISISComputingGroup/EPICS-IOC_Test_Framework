9672aea753f6fd3394045d2377d438df1010fc3e
ChrisColeExpControl <52697922+ChrisColeExpControl@users.noreply.github.com>
2019-08-09 13:44:04 +0100

    tests now check to see comms go back to invalid after a motor pv is updated

--- tests/attocube.py
+++ tests/attocube.py
@@ -4,7 +4,8 @@ from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc
-from time import sleep
+import time
+
 
 DEVICE_PREFIX = "ATTOCUBE_01"
 EMULATOR = "attocube_anc350"
@@ -40,12 +41,20 @@ class AttocubeTests(unittest.TestCase):
         self.ca.assert_that_pv_exists(MOTOR_RBV)
 
     def test_WHEN_moved_to_position_THEN_position_reached(self):
-        position_setpoint = 10
+        position_setpoint = 20
         self.ca.set_pv_value(MOTOR_PV, position_setpoint)
         self.ca.assert_that_pv_value_is_increasing(MOTOR_RBV, 1)
-        self.ca.assert_that_pv_is_number(MOTOR_RBV, position_setpoint, timeout=10)
+        self.ca.assert_that_pv_is_number(MOTOR_RBV, position_setpoint, timeout=20)
 
-    @unittest.skipIf(True, 'This should work but comms alarms seem to be somewhat broken see #4618')
     def test_GIVEN_device_not_connected_THEN_pv_in_alarm(self):
         self._lewis.backdoor_set_on_device('connected', False)
-        self.ca.assert_that_pv_alarm_is(MOTOR_PV, ChannelAccess.Alarms.INVALID, timeout=60)
+        self.ca.assert_that_pv_alarm_is(MOTOR_PV, ChannelAccess.Alarms.INVALID, timeout=30)
+        self._lewis.backdoor_set_on_device('connected', True)
+
+    def test_GIVEN_device_not_connected_WHEN_motor_val_set_THEN_pv_returns_to_alarm(self):
+        self._lewis.backdoor_set_on_device('connected', False)
+        time.sleep(10)
+        position_setpoint = 5
+        self.ca.set_pv_value(MOTOR_PV, position_setpoint)
+        self.ca.assert_that_pv_alarm_is(MOTOR_PV, ChannelAccess.Alarms.INVALID, timeout=30)
+        self._lewis.backdoor_set_on_device('connected', True)
