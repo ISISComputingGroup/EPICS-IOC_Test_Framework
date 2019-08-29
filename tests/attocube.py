2609941df4c4b170b894095d20bdd8320edeed72
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-08-29 14:33:15 +0100

    Added test to confirm axis turns on

--- tests/attocube.py
+++ tests/attocube.py
@@ -57,3 +57,12 @@ class AttocubeTests(unittest.TestCase):
         self.ca.set_pv_value(MOTOR_PV, position_setpoint)
         self.ca.assert_that_pv_alarm_is(MOTOR_PV, ChannelAccess.Alarms.INVALID, timeout=30)
         self._lewis.backdoor_set_on_device('connected', True)
+
+    def test_GIVEN_axis_not_on_WHEN_motor_moved_THEN_axis_turned_on_and_moves(self):
+        self._lewis.backdoor_set_on_device('axis_on', False)
+        position_setpoint = 5
+        self.ca.set_pv_value(MOTOR_PV, position_setpoint)
+        self.ca.assert_that_pv_value_is_increasing(MOTOR_RBV, 1)
+        self.ca.assert_that_pv_is_number(MOTOR_RBV, position_setpoint, timeout=20)
+
+        self.assertTrue(self._lewis.backdoor_get_from_device('axis_on'))
