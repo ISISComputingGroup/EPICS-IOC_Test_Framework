e6d86bf6328bfef3f1892d7f048e1bd55be628de
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-09-24 18:34:08 +0100

    Use PV prefix correctly

--- tests/rknps.py
+++ tests/rknps.py
@@ -1,12 +1,11 @@
 import unittest
 
-from unittest import skip
-from parameterized import parameterized
-
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import IOCRegister, get_default_ioc_dir
 from utils.test_modes import TestModes
 from utils.testing import parameterized_list, get_running_lewis_and_ioc, skip_if_devsim, skip_if_recsim
+
+from common_tests.danfysik import DanfysikCommon
 
 from parameterized import parameterized
 
@@ -78,11 +77,13 @@ class RknpsTests(unittest.TestCase):
     # Runs before every test.
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("rknps", PREFIX)
-        self.ca = ChannelAccess(default_timeout=60)
+        self.ca = ChannelAccess(device_prefix=PREFIX, default_timeout=60)
         self._lewis.backdoor_set_on_device("connected", True)
 
-        for ID in IDS:
-            self.ca.assert_that_pv_exists("{0}:{1}:ADDRESS".format(PREFIX, ID), timeout=30)
+        self.id_prefixes = [ID + ":" for ID in IDS]
+
+        for id_prefix in self.id_prefixes:
+            self.ca.assert_that_pv_exists("{}ADDRESS".format(id_prefix), timeout=30)
 
     def _pv_alarms_when_disconnected(self, pv):
         """
@@ -91,12 +92,12 @@ class RknpsTests(unittest.TestCase):
             pv (Str): name of the pv to check
         """
         for IDN in IDS:
-            self.ca.assert_that_pv_alarm_is_not("{0}:{1}:{2}".format(PREFIX, IDN, pv), ChannelAccess.Alarms.INVALID, 30)
+            self.ca.assert_that_pv_alarm_is_not("{0}:{1}".format(IDN, pv), ChannelAccess.Alarms.INVALID, 30)
 
         self._lewis.backdoor_set_on_device("connected", False)
 
         for IDN in IDS:
-            self.ca.assert_that_pv_alarm_is("{0}:{1}:{2}".format(PREFIX, IDN, pv), ChannelAccess.Alarms.INVALID, 30)
+            self.ca.assert_that_pv_alarm_is("{0}:{1}".format(IDN, pv), ChannelAccess.Alarms.INVALID, 30)
 
     def _activate_interlocks(self):
         """
@@ -118,76 +119,76 @@ class RknpsTests(unittest.TestCase):
     def test_WHEN_interlocks_are_active_THEN_ilk_is_Interlocked(self):
         self._activate_interlocks()
         for IDN in IDS:
-            self.ca.assert_that_pv_is("{0}:{1}:ILK".format(PREFIX, IDN), "Tripped")
+            self.ca.assert_that_pv_is("{0}:ILK".format(IDN), "Tripped")
 
     @skip_if_recsim("Interlock statuses now depend on emulator")
     def test_WHEN_interlocks_are_inactive_THEN_ilk_is_not_Interlocked(self):
         self._disable_interlocks()
         for IDN in IDS:
-            self.ca.assert_that_pv_is("{0}:{1}:ILK".format(PREFIX, IDN), "OK")
+            self.ca.assert_that_pv_is("{0}:ILK".format(IDN), "OK")
 
     @skip_if_recsim("In rec sim this test fails as recsim does not set any of the related values "
                     "which are set by the emulator")
     def test_WHEN_reset_is_sent_THEN_readbacks_and_power_are_off(self):
         for IDN in IDS:
-            self.ca.set_pv_value("{0}:{1}:RESET".format(PREFIX, IDN), 1)
-            self.ca.assert_that_pv_is("{0}:{1}:POWER".format(PREFIX, IDN), "Off")
-            self.ca.assert_that_pv_is("{0}:{1}:CURR".format(PREFIX, IDN), 0)
-            self.ca.assert_that_pv_is("{0}:{1}:VOLT".format(PREFIX, IDN), 0)
+            self.ca.set_pv_value("{0}:RESET".format(IDN), 1)
+            self.ca.assert_that_pv_is("{0}:POWER".format(IDN), "Off")
+            self.ca.assert_that_pv_is("{0}:CURR".format(IDN), 0)
+            self.ca.assert_that_pv_is("{0}:VOLT".format(IDN), 0)
 
     @skip_if_recsim("In rec sim this test fails as there is no link between the status and power")
     def test_GIVEN_emulator_in_use_WHEN_power_is_turned_on_THEN_value_is_as_expected(self):
         for IDN in IDS:
-            self.ca.assert_setting_setpoint_sets_readback(1, "{0}:{1}:POWER".format(PREFIX, IDN),
-                                                          "{0}:{1}:POWER:SP".format(PREFIX, IDN), "On")
+            self.ca.assert_setting_setpoint_sets_readback(1, "{0}:POWER".format(IDN),
+                                                          "{0}:POWER:SP".format(IDN), "On")
 
     @skip_if_recsim("In rec sim this test fails as there is no link between the status and power")
     def test_GIVEN_emulator_in_use_WHEN_power_is_turned_off_THEN_value_is_as_expected(self):
         for IDN in IDS:
-            self.ca.assert_setting_setpoint_sets_readback(0, "{0}:{1}:POWER".format(PREFIX, IDN),
-                                                          "{0}:{1}:POWER:SP".format(PREFIX, IDN), "Off")
+            self.ca.assert_setting_setpoint_sets_readback(0, "{0}:POWER".format(IDN),
+                                                          "{0}:POWER:SP".format(IDN), "Off")
 
     def test_WHEN_polarity_is_positive_THEN_value_is_as_expected(self):
         for IDN in IDS:
-            self.ca.assert_setting_setpoint_sets_readback(0, "{0}:{1}:POL".format(PREFIX, IDN),
-                                                          "{0}:{1}:POL:SP".format(PREFIX, IDN), "+")
+            self.ca.assert_setting_setpoint_sets_readback(0, "{0}:POL".format(IDN),
+                                                          "{0}:POL:SP".format(IDN), "+")
 
     def test_WHEN_polarity_is_negative_THEN_value_is_as_expected(self):
         for IDN in IDS:
-            self.ca.assert_setting_setpoint_sets_readback(1, "{0}:{1}:POL".format(PREFIX, IDN),
-                                                          "{0}:{1}:POL:SP".format(PREFIX, IDN), "-")
+            self.ca.assert_setting_setpoint_sets_readback(1, "{0}:POL".format(IDN),
+                                                          "{0}:POL:SP".format(IDN), "-")
 
     @skip_if_recsim("In rec sim this test fails as it requires a lewis backdoor command")
     def test_GIVEN_emulator_in_use_WHEN_voltage_is_read_THEN_value_is_as_expected(self):
         expected_value = 22
         self._lewis.backdoor_set_on_device("set_all_volt_values", expected_value)
         for IDN in IDS:
-            self.ca.assert_that_pv_is("{0}:{1}:VOLT".format(PREFIX, IDN), expected_value)
+            self.ca.assert_that_pv_is("{0}:VOLT".format(IDN), expected_value)
 
     @skip_if_devsim("In dev sim this test fails as the simulated records are not used")
     def test_GIVEN_emulator_not_in_use_WHEN_voltage_is_read_THEN_value_is_as_expected(self):
         expected_value = 12
         for IDN in IDS:
-            self.ca.set_pv_value("{0}:{1}:SIM:VOLT".format(PREFIX, IDN), expected_value)
-            self.ca.assert_that_pv_is("{0}:{1}:VOLT".format(PREFIX, IDN), expected_value)
+            self.ca.set_pv_value("{0}:SIM:VOLT".format(IDN), expected_value)
+            self.ca.assert_that_pv_is("{0}:VOLT".format(IDN), expected_value)
 
     @skip_if_recsim("In rec sim this test fails as the changes are not propagated to all appropriate PVs")
     def test_GIVEN_a_positive_value_and_emulator_in_use_WHEN_current_is_set_THEN_values_are_as_expected(self):
         expected_value = 480
         for IDN in IDS:
-            self.ca.set_pv_value("{0}:{1}:CURR:SP".format(PREFIX, IDN), expected_value)
-            self.ca.assert_that_pv_is("{0}:{1}:CURR".format(PREFIX, IDN), expected_value)
-            self.ca.assert_that_pv_is("{0}:{1}:RA".format(PREFIX, IDN), expected_value)
-            self.ca.assert_that_pv_is("{0}:{1}:POL".format(PREFIX, IDN), "+")
+            self.ca.set_pv_value("{0}:CURR:SP".format(IDN), expected_value)
+            self.ca.assert_that_pv_is("{0}:CURR".format(IDN), expected_value)
+            self.ca.assert_that_pv_is("{0}:RA".format(IDN), expected_value)
+            self.ca.assert_that_pv_is("{0}:POL".format(IDN), "+")
 
     @skip_if_recsim("In rec sim this test fails as the changes are not propagated to all appropriate PVs")
     def test_GIVEN_a_negative_value_and_emulator_in_use_WHEN_current_is_set_THEN_values_are_as_expected(self):
         expected_value = -123
         for IDN in IDS:
-            self.ca.set_pv_value("{0}:{1}:CURR:SP".format(PREFIX, IDN), expected_value)
-            self.ca.assert_that_pv_is("{0}:{1}:CURR".format(PREFIX, IDN), expected_value)
-            self.ca.assert_that_pv_is("{0}:{1}:RA".format(PREFIX, IDN), abs(expected_value))
-            self.ca.assert_that_pv_is("{0}:{1}:POL".format(PREFIX, IDN), "-")
+            self.ca.set_pv_value("{0}:CURR:SP".format(IDN), expected_value)
+            self.ca.assert_that_pv_is("{0}:CURR".format(IDN), expected_value)
+            self.ca.assert_that_pv_is("{0}:RA".format(IDN), abs(expected_value))
+            self.ca.assert_that_pv_is("{0}:POL".format(IDN), "-")
 
     @skip_if_devsim("In dev sim this test fails as the emulator "
                     "handles the difference in values between write and read")
@@ -195,9 +196,9 @@ class RknpsTests(unittest.TestCase):
         set_value = 480
         return_value = set_value*1000
         for IDN in IDS:
-            self.ca.set_pv_value("{0}:{1}:CURR:SP".format(PREFIX, IDN), set_value)
-            self.ca.assert_that_pv_is("{0}:{1}:CURR".format(PREFIX, IDN), return_value)
-            self.ca.assert_that_pv_is("{0}:{1}:RA".format(PREFIX, IDN), return_value)
+            self.ca.set_pv_value("{0}:CURR:SP".format(IDN), set_value)
+            self.ca.assert_that_pv_is("{0}:CURR".format(IDN), return_value)
+            self.ca.assert_that_pv_is("{0}:RA".format(IDN), return_value)
 
     @skip_if_devsim("In dev sim this test fails as the emulator "
                     "handles the difference in values between write and read")
@@ -205,9 +206,9 @@ class RknpsTests(unittest.TestCase):
         set_value = -123
         return_value = set_value*1000
         for IDN in IDS:
-            self.ca.set_pv_value("{0}:{1}:CURR:SP".format(PREFIX, IDN), set_value)
-            self.ca.assert_that_pv_is("{0}:{1}:CURR".format(PREFIX, IDN), return_value)
-            self.ca.assert_that_pv_is("{0}:{1}:RA".format(PREFIX, IDN), return_value)
+            self.ca.set_pv_value("{0}:CURR:SP".format(IDN), set_value)
+            self.ca.assert_that_pv_is("{0}:CURR".format(IDN), return_value)
+            self.ca.assert_that_pv_is("{0}:RA".format(IDN), return_value)
 
     @skip_if_recsim("Power updates through protocol redirection")
     def test_GIVEN_rb3_status_changes_THEN_rb3_banner_pv_updates_correctly(self):
@@ -215,8 +216,8 @@ class RknpsTests(unittest.TestCase):
             self.fail("Didn't find RB3 for test.")
 
         for powered_on in (True, False):
-            self.ca.set_pv_value("{}:RB3:POWER:SP".format(PREFIX), powered_on)
-            self.ca.assert_that_pv_is("{}:RB3:BANNER".format(PREFIX),
+            self.ca.set_pv_value("RB3:POWER:SP", powered_on)
+            self.ca.assert_that_pv_is("RB3:BANNER",
                                       "on; beam to ports 1,2" if powered_on else "off; ports 1,2 safe")
 
     @skip_if_recsim("Power updates through protocol redirection")
@@ -225,8 +226,8 @@ class RknpsTests(unittest.TestCase):
             self.fail("Didn't find RB4 for test.")
 
         for powered_on in (True, False):
-            self.ca.set_pv_value("{}:RB4:POWER:SP".format(PREFIX), powered_on)
-            self.ca.assert_that_pv_is("{}:RB4:BANNER".format(PREFIX),
+            self.ca.set_pv_value("RB4:POWER:SP", powered_on)
+            self.ca.assert_that_pv_is("RB4:BANNER",
                                       "on; beam to ports 3,4" if powered_on else "off; ports 3,4 safe")
 
     @skip_if_recsim("Cannot test connection in recsim")
@@ -246,8 +247,8 @@ class RknpsTests(unittest.TestCase):
                 self._lewis.backdoor_run_function_on_device("set_{0}".format(interlock), (boolean_value, ADDR))
 
                 # THEN
-                self.ca.assert_that_pv_is("{0}:{1}:ILK:{2}".format(PREFIX, IDN, interlock), expected_value)
-                self.ca.assert_that_pv_alarm_is("{0}:{1}:ILK:{2}".format(PREFIX, IDN, interlock), self.ca.Alarms.NONE)
+                self.ca.assert_that_pv_is("{0}:ILK:{1}".format(IDN, interlock), expected_value)
+                self.ca.assert_that_pv_alarm_is("{0}:ILK:{1}".format(IDN, interlock), self.ca.Alarms.NONE)
 
     @parameterized.expand(INTERLOCKS)
     @skip_if_recsim("Test requires emulator")
@@ -257,7 +258,7 @@ class RknpsTests(unittest.TestCase):
 
         # THEN
         for IDN, ADDR in zip(IDS, PSU_ADDRESSES):
-            self.ca.assert_that_pv_alarm_is("{0}:{1}:ILK:{2}".format(PREFIX, IDN, interlock), self.ca.Alarms.INVALID)
+            self.ca.assert_that_pv_alarm_is("{0}:ILK:{1}".format(IDN, interlock), self.ca.Alarms.INVALID)
 
     @parameterized.expand(parameterized_list([
         ("FAULT STATE", 0, 0),
@@ -267,9 +268,9 @@ class RknpsTests(unittest.TestCase):
     ]))
     @skip_if_devsim("DAQ does not exist in devsim")
     def test_GIVEN_mock_DAQ_inputs_THEN_RB2_mode_is_correct(self, _, state, val1, val2):
-        self.ca.set_pv_value("{}:DAQ:R04:DATA:SIM".format(PREFIX), val1)
-        self.ca.set_pv_value("{}:DAQ:R05:DATA:SIM".format(PREFIX), val2)
-        self.ca.assert_that_pv_is("{}:RB2:MODE".format(PREFIX), state)
+        self.ca.set_pv_value("DAQ:R04:DATA:SIM", val1)
+        self.ca.set_pv_value("DAQ:R05:DATA:SIM", val2)
+        self.ca.assert_that_pv_is("RB2:MODE", state)
 
     @parameterized.expand(parameterized_list([
         ("FAULT (LOW)", 0, 0),
@@ -279,25 +280,25 @@ class RknpsTests(unittest.TestCase):
     ]))
     @skip_if_devsim("DAQ does not exist in devsim")
     def test_GIVEN_mock_DAQ_inputs_THEN_PORT3_4_mode_is_correct(self, _, state, val1, val2):
-        self.ca.set_pv_value("{}:DAQ:R02:DATA:SIM".format(PREFIX), val1)
-        self.ca.set_pv_value("{}:DAQ:R03:DATA:SIM".format(PREFIX), val2)
-        self.ca.assert_that_pv_is("{}:PORT3_4:MODE".format(PREFIX), state)
+        self.ca.set_pv_value("DAQ:R02:DATA:SIM", val1)
+        self.ca.set_pv_value("DAQ:R03:DATA:SIM", val2)
+        self.ca.assert_that_pv_is("PORT3_4:MODE", state)
 
     @skip_if_devsim("DAQ does not exist in devsim")
     def test_GIVEN_fault_condition_THEN_RB2_alarms_correct(self):
-        self.ca.set_pv_value("{}:DAQ:R04:DATA:SIM".format(PREFIX), 0)
-        self.ca.set_pv_value("{}:DAQ:R05:DATA:SIM".format(PREFIX), 0)
-        self.ca.assert_that_pv_alarm_is("{}:RB2:MODE".format(PREFIX), ChannelAccess.Alarms.MAJOR)
+        self.ca.set_pv_value("DAQ:R04:DATA:SIM", 0)
+        self.ca.set_pv_value("DAQ:R05:DATA:SIM", 0)
+        self.ca.assert_that_pv_alarm_is("RB2:MODE", ChannelAccess.Alarms.MAJOR)
 
     @skip_if_devsim("DAQ does not exist in devsim")
     def test_GIVEN_high_fault_condition_THEN_PORT3_4_alarms_correct(self):
-        self.ca.set_pv_value("{}:DAQ:R02:DATA:SIM".format(PREFIX), 1)
-        self.ca.set_pv_value("{}:DAQ:R03:DATA:SIM".format(PREFIX), 1)
-        self.ca.assert_that_pv_alarm_is("{}:PORT3_4:MODE".format(PREFIX), ChannelAccess.Alarms.MAJOR)
+        self.ca.set_pv_value("DAQ:R02:DATA:SIM", 1)
+        self.ca.set_pv_value("DAQ:R03:DATA:SIM", 1)
+        self.ca.assert_that_pv_alarm_is("PORT3_4:MODE", ChannelAccess.Alarms.MAJOR)
 
     @skip_if_devsim("DAQ does not exist in devsim")
     def test_GIVEN_low_fault_condition_THEN_PORT3_4_alarms_correct(self):
-        self.ca.set_pv_value("{}:DAQ:R02:DATA:SIM".format(PREFIX), 0)
-        self.ca.set_pv_value("{}:DAQ:R03:DATA:SIM".format(PREFIX), 0)
-        self.ca.assert_that_pv_alarm_is("{}:PORT3_4:MODE".format(PREFIX), ChannelAccess.Alarms.MAJOR)
+        self.ca.set_pv_value("DAQ:R02:DATA:SIM", 0)
+        self.ca.set_pv_value("DAQ:R03:DATA:SIM", 0)
+        self.ca.assert_that_pv_alarm_is("PORT3_4:MODE", ChannelAccess.Alarms.MAJOR)
 
