7d3affdc33793684169ad1b5461c93f593fbdb4f
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-10-04 17:01:08 +0100

    Removed unused imports

--- tests/tpg361.py
+++ tests/tpg361.py
@@ -1,9 +1,8 @@
 import unittest
 
-from common_tests.tpgx6x import TpgBase, ErrorFlags, ErrorStrings
+from common_tests.tpgx6x import TpgBase
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from parameterized import parameterized
 
 
 DEVICE_PREFIX = "TPG36X_01"
