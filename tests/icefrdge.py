ba0890d706e915380aba73db2f1444a258f10173
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-21 16:05:09 +0100

    added basic tests

---
+++ tests/icefrdge.py
@@ -0,0 +1,36 @@
+import unittest
+
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+from utils.test_modes import TestModes
+from utils.testing import get_running_lewis_and_ioc
+
+
+DEVICE_PREFIX = "ICEFRDGE_01"
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("ICEFRDGE"),
+        "macros": {},
+        "emulator": "icefrdge",
+    },
+]
+
+
+TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+
+
+class IceFridgeTests(unittest.TestCase):
+    """
+    Tests for the IceFrdge IOC.
+    """
+    def setUp(self):
+        self._lewis, self._ioc = get_running_lewis_and_ioc(IOCS[0]["emulator"], DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+
+    def test_WHEN_device_is_started_THEN_it_is_not_disabled(self):
+        self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
+
+    def test_WHEN_setpoint_THEN_set_readback_identical(self):
+        self.ca.assert_setting_setpoint_sets_readback(1, "TEMP:SP:RBV", "TEMP:SP")
