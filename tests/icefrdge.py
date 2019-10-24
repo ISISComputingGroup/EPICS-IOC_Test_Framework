37df10045659169b8463458deecac75781a11dee
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-24 13:17:12 +0100

    parameterized vti temp tests

--- tests/icefrdge.py
+++ tests/icefrdge.py
@@ -1,9 +1,11 @@
 import unittest
 
+from parameterized import parameterized
+
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, parameterized_list
 
 DEVICE_PREFIX = "ICEFRDGE_01"
 
@@ -43,7 +45,8 @@ class IceFridgeTests(unittest.TestCase):
     def test_WHEN_manual_setpoint_THEN_temperature_identical(self):
         self.ca.assert_setting_setpoint_sets_readback(1, "MANUAL:TEMP", "MANUAL:TEMP:SP")
 
+    @parameterized.expand(parameterized_list([1, 2, 3, 4]))
     @skip_if_recsim("Lewis backdoor not available in recsim")
-    def test_WHEN_VTI_temp_set_backdoor_THEN_ioc_read_correctly(self):
-        self._lewis.backdoor_set_on_device("vti_temp1", 3.6)
-        self.ca.assert_that_pv_is_number("VTI:TEMP1", 3.6, 0.001)
+    def test_WHEN_VTI_temp_set_backdoor_THEN_ioc_read_correctly(self, _, temp_num):
+        self._lewis.backdoor_set_on_device("vti_temp{}".format(temp_num), 3.6)
+        self.ca.assert_that_pv_is_number("VTI:TEMP{}".format(temp_num), 3.6, 0.001)
