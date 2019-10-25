5e43b683cbaee53aac1dc64e8c438b99df06297c
aaronlong <aaron.long@stfc.ac.uk>
2019-10-25 09:56:28 +0100

    Altered test to try different velocity

--- tests/refl.py
+++ tests/refl.py
@@ -228,6 +228,7 @@ class ReflTests(unittest.TestCase):
         self.ca_galil.assert_that_pv_is("MTR0104.VELO", expected)
 
     def test_GIVEN_motor_velocity_altered_by_move_WHEN_moving_THEN_velocity_altered(self):
+        # Given a known initial velocity, confirm that on a move the velocity has changed for the axes
         self.set_up_velocity_tests(INITIAL_VELOCITY)
 
         self.ca.set_pv_value("PARAM:THETA:SP", 15)
@@ -270,12 +271,14 @@ class ReflTests(unittest.TestCase):
         # The by-design behaviour (but maybe not expected by the user) is that if a velocity is sent during a move
         # then we ignore this and restore the cached value we had for the currently issued move.
         initial = INITIAL_VELOCITY
+        altered = INITIAL_VELOCITY + 5
         expected = INITIAL_VELOCITY
         self.set_up_velocity_tests(initial)
 
         self.ca.set_pv_value("PARAM:THETA:SP", 22.5)
         self.ca_galil.assert_that_pv_is("MTR0102.DMOV", 0, timeout=1)
-        self.ca_galil.set_pv_value("MTR0102.VELO", expected)
+
+        self.ca_galil.set_pv_value("MTR0102.VELO", altered)
 
         self.ca_galil.assert_that_pv_is("MTR0102.DMOV", 1, timeout=30)
         self.ca_galil.assert_that_pv_is("MTR0102.VELO", expected)
