9311d7168d9e4f1ea619742e71541aeb1f17273d
ChrisColeExpControl <christopher.cole@stfc.ac.uk>
2019-11-01 14:36:33 +0000

    more accurate expected values

--- tests/cryosms.py
+++ tests/cryosms.py
@@ -6,13 +6,33 @@ from utils.ioc_launcher import get_default_ioc_dir
 from utils.testing import get_running_lewis_and_ioc
 
 DEVICE_PREFIX = "CRYOSMS_01"
-EMULATOR_NAME = "HFMAGPSU"
+EMULATOR_NAME = "cryogenic_sms"
 
 IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("CRYOSMS"),
-        "emulator" : EMULATOR_NAME,
+        "emulator": EMULATOR_NAME,
+        "macros": {
+            "MAX_CURR": 135,
+            "T_TO_A": 0.037,
+            "MAX_VOLT": 9.9,
+            "WRITE_UNIT": "Amps",
+            "DISPLAY_UNIT": "Gauss",
+            "RAMP_FILE": "C:\\Instrument\\Apps\\EPICS\\support\\cryosms\\master\\testramp\\test.txt",
+            "MID_TOLERANCE": 0.1,
+            "TARGET_TOLERANCE": 0.01,
+            "ALLOW_PERSIST": "No",
+            "USE_SWITCH": "No",
+            "USE_MAGNET_TEMP": "No",
+            "COMP_OFF_ACT": "No",
+            "HOLD_TIME_ZERO": 12,
+            "HOLD_TIME": 30,
+            "VOLT_STABILITY_DURATION": 300,
+            "VOLT_TOLERANCE": 0.2,
+            "FAST_RATE": 0.5,
+            "RESTORE_WRITE_UNIT_TIMEOUT": 10,
+        }
     },
 ]
 
@@ -27,8 +47,6 @@ class CryoSMSTests(unittest.TestCase):
 
         self.ca.assert_that_pv_exists("DISABLE", timeout=30)
 
-    def test_WHEN_ioc_is_started_THEN_it_is_not_disabled(self):
-        self.ca.assert_that_pv_is("DISABLE", "COMMS ENABLED")
 
     def test_GIVEN_outputmode_sp_correct_WHEN_outputmode_sp_written_to_THEN_outputmode_changes(self):
         # GIVEN
@@ -41,3 +59,31 @@ class CryoSMSTests(unittest.TestCase):
 
         # THEN
         self.ca.assert_that_pv_is("OUTPUTMODE:SP", new_outputmode, timeout=10)
+
+    def test_GIVEN_certain_macros_WHEN_IOC_loads_THEN_correct_values_initialised(self):
+        expectedValues = {"OUTPUT:SP": 0,
+                          "OUTPUT": 0,
+                          "OUTPUT:COIL": 0,
+                          "OUTPUT:PERSIST": 0,
+                          "OUTPUT:VOLT": 0,
+                          "RAMP:RATE": 1.12,
+                          "READY": 1,
+                          "RAMP:RAMPING": 0,
+                          "TARGET:TIME": 0,
+                          "STAT": "",
+                          "HEATER:STAT": "Off",
+                          "START:SP.DISP": "0",
+                          "PAUSE:SP.DISP": "0",
+                          "ABORT:SP.DISP": "0",
+                          "OUTPUT:SP.DISP": "0",
+                          "MAGNET:MODE.DISP": "1",
+                          "RAMP:LEADS.DISP": "1",
+                          }
+        failedPVs = []
+        for PV in expectedValues:
+            try:
+                self.ca.assert_that_pv_is(PV, expectedValues[PV], timeout=5)
+            except Exception as e:
+                failedPVs.append(e.message)
+        if failedPVs:
+            self.fail("The following PVs generated errors:\n{}".format("\n".join(failedPVs)))
