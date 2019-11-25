7d25465c91a5ace8984588d0127fec0b66176f94
Alistair McGann <alistair.mcgann@tessella.com>
2019-11-25 11:51:22 +0000

    First test for ZFMAGFLD

---
+++ tests/zfmagfld.py
@@ -0,0 +1,27 @@
+import unittest
+
+from parameterized import parameterized
+from utils.test_modes import TestModes
+from utils.channel_access import ChannelAccess
+from utils.ioc_launcher import get_default_ioc_dir
+
+DEVICE_PREFIX = "ZFMAGFLD_01"
+
+TEST_MODES = [TestModes.RECSIM]
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("ZFMAGFLD"),
+    },
+]
+
+
+class ZeroFieldMagFieldTests(unittest.TestCase):
+    def setUp(self):
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+        self.ca.assert_that_pv_exists("DISABLE", timeout=30)
+
+    def test_GIVEN_X_field_strength_THEN_field_strength_read_back(self):
+        field_strength = 12.3
+        self.ca.assert_setting_setpoint_sets_readback(field_strength, "DAQ:X:_RAW", "SIM:DAQ:X")
