712aae6501bd1914b78e19714508628503d169ce
Alistair McGann <alistair.mcgann@tessella.com>
2019-11-26 11:50:22 +0000

    Add test for asub record

--- tests/zfmagfld.py
+++ tests/zfmagfld.py
@@ -30,6 +30,8 @@ AXES = {"X": "L",
 
 FIELD_STRENGTHS = [0.0, 1.1, 12.3, -1.1, -12.3]
 
+SENSOR_MATRIX_PVS = "SENSORMATRIX:{row}{column}"
+
 
 class ZeroFieldMagFieldTests(unittest.TestCase):
     def setUp(self):
@@ -41,3 +43,19 @@ class ZeroFieldMagFieldTests(unittest.TestCase):
         self.ca.assert_setting_setpoint_sets_readback(field_strength, "{}:APPLYOFFSET".format(hw_axis),
                                                       "SIM:DAQ:{}".format(hw_axis),
                                                       expected_value=field_strength-OFFSET)
+
+    def test_GIVEN_offset_corrected_field_WHEN_sensor_matrix_is_identity_THEN_input_field_returned_by_matrix_multiplier(self):
+        offset_corrected_field = [1.1, 2.2, 3.3]
+        # GIVEN
+        for value, hw_axis in zip(offset_corrected_field, AXES.keys()):
+            self.ca.set_pv_value("{}:OFFSET".format(hw_axis), 0)
+            self.ca.set_pv_value("SIM:DAQ:{}".format(hw_axis), value)
+
+        # WHEN
+        for i in range(3):
+            for j in range(3):
+                self.ca.set_pv_value(SENSOR_MATRIX_PVS.format(row=i+1, column=j+1), 1 if i == j else 0)
+
+        # THEN
+        for value, hw_axis in zip(offset_corrected_field, AXES.keys()):
+            self.ca.assert_that_pv_is_number("{}:CORRECTEDFIELD".format(hw_axis), value, tolerance=0.1*abs(value))
