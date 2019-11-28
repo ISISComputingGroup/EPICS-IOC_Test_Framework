838ade1e5ac284f30fbcd028ca1a394fdac7617a
David Keymer <david.keymer@stfc.ac.uk>
2019-11-28 11:55:35 +0000

    Added test for calculation of remaining volume.

--- tests/jsco4180.py
+++ tests/jsco4180.py
@@ -133,6 +133,29 @@ class Jsco4180Tests(unittest.TestCase):
         # 4. check calculated time is based on flowrate setpoint readback (:SP:RBV rather than :SP)
         self.ca.assert_that_pv_is("TIME:VOL:CALCRUN", expected_time_value)
 
+    @skip_if_recsim("LeWIS backdoor not supported in RECSIM")
+    def test_GIVEN_an_ioc_WHEN_set_flowrate_and_pump_time_THEN_ioc_uses_rbv_for_calculation_of_remaining_volume(self):
+        expected_sp_value = 0.000
+        expected_rbv_value = 1.000
+        pump_for_time = 120
+        expected_volume_value = (pump_for_time * expected_rbv_value) / 60
+
+        # 1. set invalid flowrate setpoint (FLOWRATE:SP)
+        self.ca.set_pv_value("FLOWRATE:SP", expected_sp_value)
+        self.ca.assert_that_pv_is("FLOWRATE:SP", expected_sp_value, timeout=5)
+
+        # 2. set valid hardware flowrate (FLOWRATE:SP:RBV) via backdoor command
+        self._lewis.backdoor_set_on_device("flowrate_rbv", expected_rbv_value)
+        self.ca.assert_that_pv_is("FLOWRATE:SP:RBV", expected_rbv_value, timeout=5)
+
+        # 3. set time setpoint and start pump
+        self.ca.set_pv_value("TIME:RUN:SP", pump_for_time)
+        self.ca.set_pv_value("START:SP", "Start")
+
+        # 4. check calculated volume is based on flowrate setpoint readback (:SP:RBV rather than :SP)
+        self.ca.assert_that_pv_is("TIME:RUN:CALCVOL", expected_volume_value)
+
+    # TODO:
     # @skip_if_recsim("LeWIS backdoor not supported in RECSIM")
     # def test_GIVEN_an_ioc_WHEN_set_flowrate_and_pump_to_start_THEN_ioc_uses_rbv_for_calculation_of_remaining_time(self):
     # 1. set flowrate
