1bb385820cb4dd03e9e88a9a54be490c10abaf4e
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-01-12 21:31:16 +0000

    Add tests for ner genesys functionality.

--- tests/tdk_lambda_genesys.py
+++ tests/tdk_lambda_genesys.py
@@ -1,5 +1,6 @@
 from __future__ import division
 
+import contextlib
 import os
 import unittest
 
@@ -11,6 +12,12 @@ from utils.ioc_launcher import EPICS_TOP
 
 AMPS_TO_GAUSS = 10
 
+DEFAULT_READ_OFFSET = 0
+DEFAULT_WRITE_OFFSET = 0
+
+MAX_VOLTAGE = 200
+MAX_CURRENT = 250
+
 
 IOCS = [
     {
@@ -20,8 +27,14 @@ IOCS = [
             "ADDR1": "1",
             "PORT1": "1",
             "AMPSTOGAUSS1": "{}".format(AMPS_TO_GAUSS),
+            "TOLERANCE": 0.1,
+            "READ_OFFSET1": DEFAULT_READ_OFFSET,
+            "WRITE_OFFSET1": DEFAULT_WRITE_OFFSET,
+            "MAX_VOLTAGE1": MAX_VOLTAGE,
+            "MAX_CURRENT1": MAX_CURRENT,
         },
         "emulator": "tdk_lambda_genesys",
+        "pv_for_existence": "1:CURR"
     },
 ]
 
@@ -36,6 +49,18 @@ class OutputMode(object):
 
 class TdkLambdaGenesysTests(unittest.TestCase):
 
+    @contextlib.contextmanager
+    def _temporarily_change_offsets(self, read_offset, write_offset):
+        self.ca.set_pv_value("1:CURR:_CALC.B", read_offset)
+        self.ca.set_pv_value("1:CURR:SP:RBV:_CALC.B", read_offset)
+        self.ca.set_pv_value("1:CURR:SP:_CALC.B", write_offset)
+        try:
+            yield
+        finally:
+            self.ca.set_pv_value("1:CURR:_CALC.B", DEFAULT_READ_OFFSET)
+            self.ca.set_pv_value("1:CURR:SP:RBV:_CALC.B", DEFAULT_READ_OFFSET)
+            self.ca.set_pv_value("1:CURR:SP:_CALC.B", DEFAULT_WRITE_OFFSET)
+
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("tdk_lambda_genesys", "GENESYS_01")
         self.ca = ChannelAccess(default_timeout=10, device_prefix="GENESYS_01")
@@ -43,11 +68,11 @@ class TdkLambdaGenesysTests(unittest.TestCase):
 
     def _write_voltage(self, expected_voltage):
         self._lewis.backdoor_set_on_device("voltage", expected_voltage)
-        self.ca.set_pv_value("1:VOLT", expected_voltage)
+        self.ca.set_pv_value("1:SIM:VOLT", expected_voltage)
 
     def _write_current(self, expected_current):
         self._lewis.backdoor_set_on_device("current", expected_current)
-        self.ca.set_pv_value("1:CURR", expected_current)
+        self.ca.set_pv_value("1:SIM:CURR", expected_current)
 
     def _set_power_state(self, expected_state):
         self._lewis.backdoor_set_on_device("powerstate", expected_state)
@@ -122,4 +147,33 @@ class TdkLambdaGenesysTests(unittest.TestCase):
         self._lewis.backdoor_set_on_device("setpoint_current", test_value)
         self.ca.assert_that_pv_is_number("1:CURR:SP:RBV", test_value)
         self.ca.assert_that_pv_is_number("1:FIELD:SP:RBV",  test_value * AMPS_TO_GAUSS)
+
+    @skip_if_recsim("Uses lewis backdoor")
+    def test_GIVEN_non_zero_offsets_WHEN_setpoint_sent_to_psu_THEN_adjusted_by_offset(self):
+        test_value = 25
+        write_offset = 10
+        with self._temporarily_change_offsets(read_offset=0, write_offset=write_offset):
+            self.ca.set_pv_value("1:CURR:SP", test_value)
+            self._lewis.assert_that_emulator_value_is("setpoint_current", test_value + write_offset, cast=float)
+
+    @skip_if_recsim("Uses lewis backdoor")
+    def test_GIVEN_non_zero_offsets_WHEN_value_read_back_from_psu_THEN_adjusted_by_offset(self):
+        test_value = 43
+        read_offset = 10
+        with self._temporarily_change_offsets(read_offset=read_offset, write_offset=0):
+            self._lewis.backdoor_set_on_device("current", test_value)
+            self._lewis.backdoor_set_on_device("setpoint_current", test_value)
+
+            self.ca.assert_that_pv_is_number("1:CURR", test_value + read_offset)
+            self.ca.assert_that_pv_is_number("1:CURR:SP:RBV", test_value + read_offset)
+
+    def test_GIVEN_voltage_setpoint_higher_than_max_THEN_capped_to_maximum_and_readback_alarm(self):
+        self.ca.set_pv_value("1:VOLT:SP", MAX_VOLTAGE + 1)
+        self.ca.assert_that_pv_is_number("1:VOLT:SP", MAX_VOLTAGE)
+        self.ca.assert_that_pv_alarm_is("1:VOLT:SP:RBV", self.ca.Alarms.MINOR)
+
+    def test_GIVEN_current_setpoint_higher_than_max_THEN_capped_to_maximum_and_readback_alarm(self):
+        self.ca.set_pv_value("1:CURR:SP", MAX_CURRENT + 1)
+        self.ca.assert_that_pv_is_number("1:CURR:SP", MAX_CURRENT)
+        self.ca.assert_that_pv_alarm_is("1:CURR:SP:RBV", self.ca.Alarms.MINOR)
 
