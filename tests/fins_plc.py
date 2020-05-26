b1ebbda25b7acf0b09b25d2a9e6c4da6b6615581
Alexandru Dascalu <965337@swansea.ac.uk>
2020-05-26 19:24:57 +0100

    made ioc test framework load correct db

--- tests/fins_plc.py
+++ tests/fins_plc.py
@@ -1,18 +1,26 @@
+import os
 import unittest
 
 from utils.channel_access import ChannelAccess
-from utils.ioc_launcher import get_default_ioc_dir, IOCRegister
+from utils.ioc_launcher import get_default_ioc_dir, IOCRegister, EPICS_TOP
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
 
 # Device prefix
 DEVICE_PREFIX = "FINS_01"
 
+IOC_NAME = "FINS"
+TEST_PATH = os.path.join(EPICS_TOP, "ioc", "master", IOC_NAME, "exampleSettings", "HELIUM_RECOVERY")
+
+IOC_PREFIX = "{}:HE-RCVRY".format(DEVICE_PREFIX)
+
 IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("FINS"),
+        "custom_prefix": IOC_PREFIX,
         "macros": {
+            "FINSCONFIGDIR": TEST_PATH.replace("\\", "/"),
             "PLCIP": "127.0.0.1",
             "PLCNODE": 58,
         },
@@ -30,7 +38,7 @@ class FinsPLCTests(unittest.TestCase):
 
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc(IOCS[0]["emulator"], DEVICE_PREFIX)
-        self.ca = ChannelAccess(default_timeout=20, device_prefix="HE-RCVRY")
+        self.ca = ChannelAccess(default_timeout=20, device_prefix=IOC_PREFIX)
 
         if not IOCRegister.uses_rec_sim:
             self._lewis.backdoor_run_function_on_device("reset")
