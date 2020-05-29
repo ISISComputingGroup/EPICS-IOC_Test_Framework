a2d2e1854120ca2b27b91ed28b39e76dca733178
Alexandru Dascalu <965337@swansea.ac.uk>
2020-05-29 18:23:33 +0100

    added he purity test

--- tests/fins_plc.py
+++ tests/fins_plc.py
@@ -47,3 +47,7 @@ class FinsPLCTests(unittest.TestCase):
     def test_WHEN_heartbeat_set_backdoor_THEN_ioc_read_correctly(self):
         self._lewis.backdoor_set_on_device("heartbeat", 1)
         self.ca.assert_that_pv_is("HEARTBEAT", 1)
+
+    def test_WHEN_helium_purity_set_backdoor_THEN_ioc_read_correctly(self):
+        self._lewis.backdoor_set_on_device("helium_purity", 2)
+        self.ca.assert_that_pv_is("HE_PURITY", 2)
