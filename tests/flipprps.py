de730abb84527a1066f89445be764f7e02bd6430
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-04 17:08:51 +0100

    Minor tidy-ups

--- tests/flipprps.py
+++ tests/flipprps.py
@@ -29,26 +29,25 @@ class FlipprpsTests(unittest.TestCase):
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("flipprps", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+        self._lewis.backdoor_set_on_device("connected", True)
 
     @skip_if_recsim("Lewis backdoor commands not available in RecSim")
     def test_SET_polarity(self):
-        self._lewis.backdoor_set_on_device("connected", True)
         self.ca.set_pv_value("POLARITY", "Down")
         self._lewis.assert_that_emulator_value_is("polarity", "0")
         self.ca.set_pv_value("POLARITY", "Up")
         self._lewis.assert_that_emulator_value_is("polarity", "1")
 
     def test_GET_id(self):
-        self._lewis.backdoor_set_on_device("connected", True)
         self.ca.assert_that_pv_is("ID", "Flipper")
 
     @skip_if_recsim("Lewis backdoor commands not available in RecSim")
     def test_GIVEN_device_not_connected_THEN_id_is_in_alarm(self):
         self._lewis.backdoor_set_on_device("connected", False)
-        self.ca.assert_that_pv_alarm_is("ID", "INVALID", 20)
+        self.ca.assert_that_pv_alarm_is("ID", self.ca.Alarms.INVALID, 20)
 
     @skip_if_recsim("Lewis backdoor commands not available in RecSim")
     def test_GIVEN_device_not_connected_THEN_polarity_raises_timeout_alarm_after_set(self):
         self._lewis.backdoor_set_on_device("connected", False)
         self.ca.set_pv_value("POLARITY", "Up")
-        self.ca.assert_that_pv_alarm_is("POLARITY", "INVALID")
+        self.ca.assert_that_pv_alarm_is("POLARITY", self.ca.Alarms.INVALID)
