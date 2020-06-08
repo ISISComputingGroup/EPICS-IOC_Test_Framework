64329ea66b31e9175281d6298423ce278e4c20f3
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-08 16:16:20 +0100

    Fix get_instrument

--- tests/danfysik_local_calib.py
+++ tests/danfysik_local_calib.py
@@ -30,7 +30,7 @@ class DanfysikLocalCalibTests(DanfysikBase, unittest.TestCase):
     """
     def test_GIVEN_local_calib_macro_set_to_no_THEN_calib_base_dir_is_common_dir(self):
         g.set_instrument(None)
-        inst = os.environ.get("INSTRUMENT", g.get_instrument())
+        inst = os.environ.get("INSTRUMENT", g.adv.get_instrument())
         for pv in ["FIELD:CALIB", "FIELD:SP:CALIB"]:
             self.ca.assert_that_pv_is("{}.TDIR".format(pv), r"calib/magnets")
             self.ca.assert_that_pv_is("{}.BDIR".format(pv), r"C:/Instrument/Settings/config/{}".format(inst))
