5c5e1ee7a4edc378ecca0ddb4b22e8fe4b673af4
Alexandru Dascalu <965337@swansea.ac.uk>
2020-06-17 11:08:16 +0100

    added documentation for method

--- tests/helium_recovery_plc.py
+++ tests/helium_recovery_plc.py
@@ -85,6 +85,12 @@ class HeliumRecoveryPLCTests(unittest.TestCase):
 
     @staticmethod
     def _get_index_value(list_of_test_values, test_value):
+        """
+        Returns a 1-based index of the given test value in the given list.
+        :param list_of_test_values: the list of test values within which we search for the given test value.
+        :param test_value: the test value we want the index for.
+        :return: a 1-based index of the test value.
+        """
         return list_of_test_values.index(test_value) + 1
 
     @parameterized.expand(parameterized_list(zip(PV_NAMES, TEST_VALUES)))
