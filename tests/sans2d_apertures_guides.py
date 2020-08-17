6ebbea9513316f838428adc07a68041c508242f4
JamesKingWork <james.c.king@stfc.ac.uk>
2020-08-17 16:13:16 +0100

    Prevent instability of tests

--- tests/sans2d_apertures_guides.py
+++ tests/sans2d_apertures_guides.py
@@ -5,33 +5,33 @@ from parameterized import parameterized
 from utils.ioc_launcher import get_default_ioc_dir, EPICS_TOP
 from utils.test_modes import TestModes
 from utils.channel_access import ChannelAccess
-from utils.axis import set_axis_moving, assert_axis_not_moving, assert_axis_moving
-import time
+from utils.axis import set_axis_moving, assert_axis_not_moving, assert_axis_moving, stop_axis_moving
 
 galil_settings_path = os.path.realpath(
-    os.path.join(os.getenv("EPICS_KIT_ROOT"), "support", "motorExtensions", "master", "settings", "sans2d")
+    os.path.join(
+        os.getenv("EPICS_KIT_ROOT"), "support", "motorExtensions",
+        "master", "settings", "sans2d_apertures_guides"
+    )
 )
 
-GALIL_ADDR = "128.0.0.0"
+GALIL_ADDR = "127.0.0.1"
 
 ioc_name = "FINS"
 fins_settings_path = os.path.join(EPICS_TOP, "ioc", "master", ioc_name, "exampleSettings", "SANS2D_vacuum")
 ioc_prefix = "FINS_VAC"
 
-# Create GALIL_01 and GALIL_02
 IOCS = [
     {
-        "name": "GALIL_0{}".format(i),
-        "directory": get_default_ioc_dir("GALIL", i),
+        "name": "GALILMUL_01",
+        "directory": get_default_ioc_dir("GALILMUL"),
         "custom_prefix": "MOT",
-        "pv_for_existence": "MTR0{}01".format(i),
+        "pv_for_existence": "MTR0101",
         "macros": {
-            "GALILADDR": GALIL_ADDR,
-            "MTRCTRL": "0{}".format(i),
+            "MTRCTRL1": "01",
+            "GALILADDR1": GALIL_ADDR,
             "GALILCONFIGDIR": galil_settings_path.replace("\\", "/"),
         }
-    } for i in [1, 2]
-] + [
+    },
     {
         "name": "FINS_01",
         "directory": get_default_ioc_dir(ioc_name),
@@ -62,6 +62,10 @@ class Sans2dAperturesGuidesTests(unittest.TestCase):
         # Set interlock to enabled
         self.ca.set_pv_value("FINS_VAC:SIM:ADDR:1001", 64)
         self.ca.assert_that_pv_is("FINS_VAC:GALIL_INTERLOCK", "CAN MOVE")
+        # Stop axis to prevent test instability (other tests may have left it moving)
+        stop_axis_moving(axis)
+        assert_axis_not_moving(axis)
+        # Execute test
         for _ in range(3):
             set_axis_moving(axis)
             assert_axis_moving(axis)
@@ -69,10 +73,11 @@ class Sans2dAperturesGuidesTests(unittest.TestCase):
             assert_axis_not_moving(axis)
 
     @parameterized.expand(AXES_TO_STOP)
-    def test_GIVEN_move_disabled_axis_moving_WHEN_stop_all_THEN_axis_stopped(self, axis):
+    def test_GIVEN_move_disabled_axis_moving_WHEN_stop_all_THEN_axis_not_stopped(self, axis):
         # Set interlock to disabled
         self.ca.set_pv_value("FINS_VAC:SIM:ADDR:1001", 0)
         self.ca.assert_that_pv_is("FINS_VAC:GALIL_INTERLOCK", "CANNOT MOVE")
+        # Execute test
         for _ in range(3):
             set_axis_moving(axis)
             assert_axis_moving(axis)
