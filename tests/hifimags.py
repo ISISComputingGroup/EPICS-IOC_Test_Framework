0e99bcc9ef141d80add5f3774332a9a380b359f3
Kathryn Baker <kathryn.baker@stfc.ac.uk>
2021-01-06 18:45:28 +0000

    Adding tests for all main PSU backwards compatibility

--- tests/hifimags.py
+++ tests/hifimags.py
@@ -50,6 +50,9 @@ WRITE_PVS = [
 ]
 MAIN_PVS = [
     {"PV": "STAT", "EXTRA_READ_PV":"MAIN:STAT:RBV", "values":STATUSES, "init_value": ""},
+    {"PV": "READY", "EXTRA_READ_PV": "MAIN:READY:RBV", "values": READYS, "init_value": ""},
+    {"PV": "OUTPUT:FIELD:GAUSS", "EXTRA_READ_PV": "MAIN:OUT:RBV", "values":GAUSS, "init_value":-4},
+    {"PV": "TARGET:TIME", "EXTRA_READ_PV": "MAIN:MINTO:RBV", "values":TIMES, "init_value":""},
 ]
 
 class HifimagsTests(unittest.TestCase):
@@ -61,7 +64,6 @@ class HifimagsTests(unittest.TestCase):
 
     # RECSIM Tests
 
-    @skip_if_recsim
     def test_GIVEN_sim_status_WHEN_sim_status_is_updated_THEN_all_statuses_match(self):
         sim_status = "Unit Test"
         for PSU in PSUS:
@@ -74,7 +76,6 @@ class HifimagsTests(unittest.TestCase):
             self.ca.assert_that_pv_is(PSU + ":STAT:RBV", sim_status)
             self.ca.assert_that_pv_is(PSU + ":STAT", sim_status)
 
-    @skip_if_recsim
     def test_GIVEN_readback_values_WHEN_sim_values_set_THEN_all_values_update(self):
         for PV in READ_PVS:
             if not PV["init_value"] == "":
@@ -86,7 +87,6 @@ class HifimagsTests(unittest.TestCase):
                 if not PV["EXTRA_READ_PV"] == "":
                     self.ca.assert_that_pv_is("X:" + PV["EXTRA_READ_PV"], sim_value)
 
-    @skip_if_recsim
     def test_GIVEN_settable_values_WHEN_sim_values_set_THEN_all_values_update(self):
         for PV in WRITE_PVS:
             if not PV["init_value"] == "":
@@ -98,16 +98,17 @@ class HifimagsTests(unittest.TestCase):
                 if not PV["EXTRA_READ_PV"] == "":
                     self.ca.assert_that_pv_is(PV["EXTRA_READ_PV"], sim_value)
 
-    @skip_if_recsim
     def test_GIVEN_error_active_WHEN_using_backwards_compatibility_THEN_the_correct_status_is_reported(self):
+        # Errors for Main PSU different, so use M rather than X
         sim_value = "There is a simulated error"
-        self.ca.set_pv_value("SIM:X:STAT", sim_value)
-        self.ca.set_pv_value("SIM:X:ERROR", "Error")
-        self.ca.assert_that_pv_is("X:ERRORS:RBV", sim_value)
-        self.ca.set_pv_value("SIM:X:ERROR", "No Error")
-        self.ca.assert_that_pv_is("X:ERRORS:RBV", "")
+        self.ca.set_pv_value("SIM:M:STAT", sim_value)
+        self.ca.set_pv_value("SIM:M:ERROR", "Error")
+        self.ca.assert_that_pv_is("M:ERRORS:RBV", sim_value)
+        self.ca.assert_that_pv_is("MAIN:ERRORS:RBV", sim_value)
+        self.ca.set_pv_value("SIM:M:ERROR", "No Error")
+        self.ca.assert_that_pv_is("M:ERRORS:RBV", "")
+        self.ca.assert_that_pv_is("MAIN:ERRORS:RBV", "")
 
-    @skip_if_recsim
     def test_GIVEN_abort_requested_THEN_abort_is_propagated(self):
         sim_value = "Aborting X"
         self.ca.set_pv_value("X:ABORT:SP", 1)
