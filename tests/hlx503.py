473e8c703945a6b1b49e7500d4d30eb8b92c05ab
JamesKingWork <james.c.king@stfc.ac.uk>
2021-02-16 16:56:31 +0000

    HLX503: Add tests for itc number and channels

--- tests/hlx503.py
+++ tests/hlx503.py
@@ -1,9 +1,10 @@
 import unittest
+from parameterized import parameterized
 
 from utils.test_modes import TestModes
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
-from utils.testing import get_running_lewis_and_ioc
+from utils.testing import get_running_lewis_and_ioc, parameterized_list
 
 # Device prefix
 DEVICE_PREFIX = "HLX503_01"
@@ -11,11 +12,26 @@ DEVICE_PREFIX = "HLX503_01"
 # Emulator name
 emulator_name = "hlx503"
 
+# ITC503 numbers and channels
+itc_numbers = {
+    "SORB_ITC": 1,
+    "1KPOT_ITC": 2,
+    "HE3POT_LOWT_ITC": 3,
+    "HE3POT_HIGHT_ITC": 4
+}
+channel_numbers = {
+    "SORB_CH": 1,
+    "1KPOT_CH": 2,
+    "HE3POT_LOWT_CH": 3,
+    "HE3POT_HIGHT_CH": 4
+}
+
 IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("HLX503"),
         "emulator": emulator_name,
+        "macros": {**itc_numbers, **channel_numbers}
     },
 ]
 
@@ -37,3 +53,12 @@ class HLX503Tests(unittest.TestCase):
 
     def test_WHEN_ioc_started_THEN_ioc_connected(self):
         self.ca.get_pv_value("DISABLE")
+
+    @parameterized.expand(parameterized_list(itc_numbers.items()))
+    def test_WHEN_ioc_set_up_with_itc_numbers_THEN_itc_numbers_are_correct(self, _, macro_pv_name, itc_address):
+        self.ca.assert_that_pv_is(macro_pv_name, itc_address)
+
+    @parameterized.expand(parameterized_list(channel_numbers.items()))
+    def test_WHEN_ioc_set_up_with_channels_THEN_channels_are_correct(self, _, macro_pv_name, itc_address):
+        self.ca.assert_that_pv_is(macro_pv_name, itc_address)
+
