01f0b95d55c2476db7f7fa585da7ec59259cb957
JamesKingWork <james.c.king@stfc.ac.uk>
2021-02-16 17:12:07 +0000

    HLX503: ISOBUS address tests

--- tests/hlx503.py
+++ tests/hlx503.py
@@ -12,18 +12,12 @@ DEVICE_PREFIX = "HLX503_01"
 # Emulator name
 emulator_name = "hlx503"
 
-# ITC503 numbers and channels
-itc_numbers = {
-    "SORB_ITC": 1,
-    "1KPOT_ITC": 2,
-    "HE3POT_LOWT_ITC": 3,
-    "HE3POT_HIGHT_ITC": 4
-}
-channel_numbers = {
-    "SORB_CH": 1,
-    "1KPOT_CH": 2,
-    "HE3POT_LOWT_CH": 3,
-    "HE3POT_HIGHT_CH": 4
+# ITC503 ISOBUS addresses
+isobus_addresses = {
+    "SORB_ISOBUS": 1,
+    "1KPOT_ISOBUS": 2,
+    "HE3POT_LOWT_ISOBUS": 3,
+    "HE3POT_HIGHT_ISOBUS": 4
 }
 
 IOCS = [
@@ -31,7 +25,7 @@ IOCS = [
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("HLX503"),
         "emulator": emulator_name,
-        "macros": {**itc_numbers, **channel_numbers}
+        "macros": isobus_addresses
     },
 ]
 
@@ -41,7 +35,7 @@ TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 
 class HLX503Tests(unittest.TestCase):
     """
-    Tests for the ITC503/Heliox 3He Refrigerator.
+    Tests for the ISOBUS503/Heliox 3He Refrigerator.
     """
 
     def setUp(self):
@@ -54,11 +48,6 @@ class HLX503Tests(unittest.TestCase):
     def test_WHEN_ioc_started_THEN_ioc_connected(self):
         self.ca.get_pv_value("DISABLE")
 
-    @parameterized.expand(parameterized_list(itc_numbers.items()))
-    def test_WHEN_ioc_set_up_with_itc_numbers_THEN_itc_numbers_are_correct(self, _, macro_pv_name, itc_address):
-        self.ca.assert_that_pv_is(macro_pv_name, itc_address)
-
-    @parameterized.expand(parameterized_list(channel_numbers.items()))
-    def test_WHEN_ioc_set_up_with_channels_THEN_channels_are_correct(self, _, macro_pv_name, itc_address):
-        self.ca.assert_that_pv_is(macro_pv_name, itc_address)
-
+    @parameterized.expand(parameterized_list(isobus_addresses.items()))
+    def test_WHEN_ioc_set_up_with_ISOBUS_numbers_THEN_ISOBUS_numbers_are_correct(self, _, macro_pv_name, isobus_addresses):
+        self.ca.assert_that_pv_is(macro_pv_name, isobus_addresses)
