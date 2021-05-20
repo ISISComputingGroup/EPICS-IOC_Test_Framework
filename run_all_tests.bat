de97af5ac2ed8f5673d8d4bfeb06859c3cf407f7
Jack Harper <jack.harper@stfc.ac.uk>
2021-05-20 15:00:18 +0100

    remove lewis re-installation - this will be done in genie python itself

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -9,7 +9,5 @@ set "PYTHONUNBUFFERED=1"
 
 REM Command line arguments always passed to the test script
 SET ARGS=-rc
-call %PYTHON3% -m pip uninstall --yes lewis
-call %PYTHON3% -m pip install git+git://github.com/ess-dmsc/lewis@0cefafc1ccbc3867ed6f13d51190fcc290b7c8c6
 call %PYTHON3% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" %ARGS% %* 
 IF %ERRORLEVEL% NEQ 0 EXIT /b %errorlevel%
