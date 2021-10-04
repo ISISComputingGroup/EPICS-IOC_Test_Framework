232d76e1e320c4b47b10b33c64cddd760ed9ee35
Jack Harper <jack.harper@stfc.ac.uk>
2021-10-04 18:11:33 +0100

    remove crap tests

--- common_tests/oscillating_collimators.py
+++ common_tests/oscillating_collimators.py
@@ -25,18 +25,6 @@ class OscillatingCollimatorBase(object):
         self.ca.set_pv_value(ANGLE, 5.0)
         self.ca.assert_that_pv_is_number(ANGLE, 2.0)
 
-    def test_WHEN_frequency_set_negative_THEN_angle_is_zero(self):
-        self.ca.set_pv_value(FREQUENCY, -1.0)
-        self.ca.assert_that_pv_is_number(FREQUENCY, 0.0)
-
-    def test_WHEN_angle_set_greater_than_half_THEN_angle_is_half(self):
-        self.ca.set_pv_value(FREQUENCY, 1.0)
-        self.ca.assert_that_pv_is_number(FREQUENCY, 0.5)
-
-    def test_WHEN_frq_set_greater_than_two_THEN_angle_is_two(self):
-        self.ca.set_pv_value(ANGLE, 5.0)
-        self.ca.assert_that_pv_is_number(ANGLE, 2.0)
-
     def test_WHEN_input_values_cause_discriminant_to_be_positive_THEN_discriminant_pv_is_zero(self):
 
         # Act
