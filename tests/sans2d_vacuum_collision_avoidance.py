bd244e47cd24a4b2a3b79606c85a00b4fdf5cf3a
bishal <bishal.rai@stfc.ac.uk>
2021-10-20 19:11:57 +0100

    added new test to test axes prohibition during movement
    added new test to test axes are set to Pause once moved to its destination

--- tests/sans2d_vacuum_collision_avoidance.py
+++ tests/sans2d_vacuum_collision_avoidance.py
@@ -101,6 +101,8 @@ TEST_SPEED = 200
 # acceleration is number of seconds until motor goes from 0 to full speed
 TEST_ACCELERATION = 0
 
+FRONTDET_INITIAL_POS = 100
+
 
 class Sans2dVacCollisionAvoidanceTests(unittest.TestCase):
     """
@@ -109,6 +111,7 @@ class Sans2dVacCollisionAvoidanceTests(unittest.TestCase):
 
     def setUp(self):
         self.ca = ChannelAccess(device_prefix="MOT", default_timeout=30)
+        self.ca.set_pv_value("SANS2DVAC:STOP_MOTORS:ALL.PROC", 1)
         with ManagerMode(ChannelAccess()):
             for axis in BAFFLES_AND_DETECTORS_Z_AXES:
                 self.ca.set_pv_value("{}:MTR.VMAX".format(axis), TEST_SPEED, sleep_after_set=0)
@@ -152,7 +155,7 @@ class Sans2dVacCollisionAvoidanceTests(unittest.TestCase):
             end_values[axis_pair.rear_axis] = rear_axis_pos
             self.ca.set_pv_value(axis_pair.rear_axis_sp, rear_axis_pos, sleep_after_set=0)
 
-        self.ca.set_pv_value("SANS2DVAC:CALC_MOVE_ALL.PROC", 1, sleep_after_set=10)
+        self.ca.set_pv_value("SANS2DVAC:MOVE_ALL.PROC", 1, sleep_after_set=10)
         for key in end_values.keys():
             self.assertEquals(end_values[key], self.ca.get_pv_value(key))
 
@@ -162,7 +165,7 @@ class Sans2dVacCollisionAvoidanceTests(unittest.TestCase):
             self.ca.set_pv_value(axis_pair.rear_axis_sp, rear_axis_pos, sleep_after_set=0)
 
         with self.assertRaises(WriteAccessException, msg="DISP should be set on inhibited axis"):
-            self.ca.set_pv_value("SANS2DVAC:CALC_MOVE_ALL.PROC", 1, sleep_after_set=10)
+            self.ca.set_pv_value("SANS2DVAC:MOVE_ALL.PROC", 1, sleep_after_set=10)
 
     def test_GIVEN_some_positions_invalid_WHEN_move_all_THEN_axes_movement_is_inhibited(self):
         # invalid, invalid, valid, valid positions
@@ -171,4 +174,48 @@ class Sans2dVacCollisionAvoidanceTests(unittest.TestCase):
             self.ca.set_pv_value(axis_pair.rear_axis_sp, positions[axis_pair.rear_axis])
 
         with self.assertRaises(WriteAccessException, msg="DISP should be set on inhibited axis"):
-            self.ca.set_pv_value("SANS2DVAC:CALC_MOVE_ALL.PROC", 1, sleep_after_set=10)
+            self.ca.set_pv_value("SANS2DVAC:MOVE_ALL.PROC", 1, sleep_after_set=10)
+
+    def test_GIVEN_some_axis_are_moving_THEN_not_possible_to_change_SP(self):
+        positions = {"FRONTDETZ": 5000, "FRONTBAFFLEZ": 6100, "REARBAFFLEZ": 7500, "REARDETZ": 8000}
+        for axis_position in positions:
+            self.ca.set_pv_value(axis_position + ":SP", positions[axis_position])
+
+        self.ca.set_pv_value("SANS2DVAC:MOVE_ALL.PROC", 1)
+
+        for axis_position in positions:
+            with self.assertRaises(WriteAccessException, msg="DISP should be set on inhibited axis"):
+                self.ca.set_pv_value(axis_position + ":SP", positions[axis_position])
+
+            with self.assertRaises(WriteAccessException, msg="DISP should be set on inhibited axis"):
+                self.ca.set_pv_value(axis_position + ":MTR", positions[axis_position])
+
+    @parameterized.expand(parameterized_list(BAFFLES_AND_DETECTORS_Z_AXES))
+    def test_GIVEN_some_axes_have_stopped_moving_THEN_stopped_axes_are_set_to_PAUSE(self, _, axis):
+
+        self.ca.set_pv_value("{}:MTR.VMAX".format(axis), 1, sleep_after_set=0)
+        self.ca.set_pv_value("{}:MTR.VELO".format(axis), 1, sleep_after_set=0)
+        self.ca.set_pv_value("{}:MTR.ACCL".format(axis), 1, sleep_after_set=0)
+
+        global FRONTDET_INITIAL_POS
+        FRONTDET_INITIAL_POS = FRONTDET_INITIAL_POS + 100
+        self.ca.set_pv_value("FRONTDETZ:SP", FRONTDET_INITIAL_POS)
+
+        for axis_pair in AXIS_PAIRS:
+            self.ca.set_pv_value(axis_pair.rear_axis_sp, self.ca.get_pv_value(axis_pair.front_axis_sp)
+                                 + axis_pair.minimum_interval + 500)
+
+        self.ca.set_pv_value("SANS2DVAC:MOVE_ALL.PROC", 1, sleep_after_set=5)
+
+        for tank_axis in BAFFLES_AND_DETECTORS_Z_AXES:
+            if tank_axis in axis:
+                self.ca.assert_that_pv_is("{}:MTR.SPMG".format(tank_axis), "Move")
+            else:
+                self.ca.assert_that_pv_is("{}:MTR.SPMG".format(tank_axis), "Pause")
+
+        self.ca.set_pv_value("SANS2DVAC:STOP_MOTORS:ALL.PROC", 1)
+
+        self.ca.set_pv_value("{}:MTR.VMAX".format(axis), TEST_SPEED, sleep_after_set=0)
+        self.ca.set_pv_value("{}:MTR.VELO".format(axis), TEST_SPEED, sleep_after_set=0)
+        self.ca.set_pv_value("{}:MTR.ACCL".format(axis), TEST_ACCELERATION, sleep_after_set=0)
+
