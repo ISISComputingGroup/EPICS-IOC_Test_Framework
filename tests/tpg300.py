17137a1c2afd3c7c9bf6f34aa9bdc726ccfe9c79
Adam-Szw <adamszwandziak2@gmail.com>
2022-06-29 15:24:50 +0100

    rework

--- tests/tpg300.py
+++ tests/tpg300.py
@@ -90,40 +90,34 @@ class Tpg300Tests(unittest.TestCase):
     def set_switching_function(self, function):
         self.ca.set_pv_value("FUNCTION", function)
 
-    def set_switching_function_thresholds(self, threshold_low, exponent_low, threshold_high, exponent_high,
-                                          circuit_assignment):
+    def set_switching_function_thresholds(self, threshold_low, threshold_high, circuit_assignment):
         self.ca.set_pv_value("FUNCTION:LOW:SP", threshold_low)
-        self.ca.set_pv_value("FUNCTION:LOW:E:SP", exponent_low)
         self.ca.set_pv_value("FUNCTION:HIGH:SP", threshold_high)
-        self.ca.set_pv_value("FUNCTION:HIGH:E:SP", exponent_high)
         self.ca.set_pv_value("FUNCTION:ASSIGN:SP", circuit_assignment)
         self.ca.process_pv("FUNCTION:ASSIGN:SP:OUT")
 
     def check_switching_function_thresholds(self, function, thresholds, check_pv_is=True):
         assignments = ("No assignment", "A1", "A2", "B1", "B1", "A1 self-monitor", "A2 self-monitor", "B1 self-monitor", "B1 self-monitor")
         if check_pv_is:
-            low_value = thresholds[0] * 10 ** thresholds[1]
-            high_value = thresholds[2] * 10 ** thresholds[3]
-            assignment = assignments[thresholds[4]]
-            self.ca.assert_that_pv_is_number("FUNCTION:" + function + ":LOW:SP:RBV", low_value, 0.001)
-            self.ca.assert_that_pv_is_number("FUNCTION:" + function + ":HIGH:SP:RBV", high_value, 0.001)
+            assignment = assignments[thresholds[2]]
+            self.ca.assert_that_pv_is_number("FUNCTION:" + function + ":LOW:SP:RBV", thresholds[0], 0.001)
+            self.ca.assert_that_pv_is_number("FUNCTION:" + function + ":HIGH:SP:RBV", thresholds[1], 0.001)
             self.ca.assert_that_pv_is("FUNCTION:" + function + ":ASSIGN:SP:RBV", assignment)
 
     @parameterized.expand([
-        (('2', 0.5, 2, 1.7, -5, 2), (0.5, 2, 1.7, -5, 2)),
-        (('A', 0.5534, 215, 125, -5, 6), (0.5, 99, 9.9, -5, 6)),  # The values are truncated not rounded
-        (('B', 12, -215, -12, 3, 3), (9.9, -99, 0.0, 3, 3))
+        (('2', 0.5E2, 1.7E-5, 2), (0.5E2, 1.7E-5, 2)),
+        (('A', 0.5534E55, 1.25E-5, 6), (5.5E54, 1.25E-5, 6)),
+        (('B', 12E-215, -12E3, 3), (9.9E-99, 0.0E3, 3))
     ])
     @skip_if_recsim("Requires emulator")
     def test_GIVEN_function_thresholds_set_THEN_thresholds_readback_correct(self, function_set, function_read):
         self.set_switching_function("1")
-        self.set_switching_function_thresholds(0.0, 0, 0.0, 0, 1)
+        self.set_switching_function_thresholds(0.0, 0.0, 1)
         self.set_switching_function(function_set[0])
-        self.set_switching_function_thresholds(function_set[1], function_set[2], function_set[3], function_set[4],
-                                               function_set[5])
+        self.set_switching_function_thresholds(function_set[1], function_set[2], function_set[3])
         self.check_switching_function_thresholds(str(function_set[0]), function_read)
         self.set_switching_function("1")
-        self.check_switching_function_thresholds("1", (0.0, 0, 0.0, 0, 1))
+        self.check_switching_function_thresholds("1", (0.0, 0.0, 1))
 
     def check_switching_function_statuses(self, expected_statuses):
         self.ca.assert_that_pv_is("FUNCTION:STATUS:1:RB", str(expected_statuses[0]))
@@ -141,7 +135,7 @@ class Tpg300Tests(unittest.TestCase):
     def test_GIVEN_thresholds_settings_and_pressure_above_THEN_check_if_violation_detected(self):
         self._set_pressure(0, "A2")
         self.set_switching_function("3")
-        self.set_switching_function_thresholds(5, 2, 7.5, 4, 2)
+        self.set_switching_function_thresholds(5E2, 7.5E4, 2)
         self.ca.assert_that_pv_is("FUNCTION:3:THRESHOLD:BELOW", 1)
         self._set_pressure(6.43, "A2")
         self.ca.assert_that_pv_is("FUNCTION:3:THRESHOLD:BELOW", 1)
