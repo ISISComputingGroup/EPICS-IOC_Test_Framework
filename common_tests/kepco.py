8907cc7b82198e90ae3e358b09ac59d11bd100df
HannahCawley <HannahCawley@NDW1798.isis.cclrc.ac.uk>
2022-08-11 11:42:55 +0100

    Refactor common_tests/kepco test with new util fn

--- common_tests/kepco.py
+++ common_tests/kepco.py
@@ -133,11 +133,11 @@ class KepcoTests(object):
 
     @skip_if_recsim("In rec sim you can not diconnect the device")
     def test_GIVEN_diconnected_WHEN_read_THEN_alarms_on_readbacks(self):
-        self._lewis.backdoor_set_on_device("connected", False)
-
-        self.ca.assert_that_pv_alarm_is("OUTPUTMODE", self.ca.Alarms.INVALID)
-        self.ca.assert_that_pv_alarm_is("CURRENT", self.ca.Alarms.INVALID)
-        self.ca.assert_that_pv_alarm_is("VOLTAGE", self.ca.Alarms.INVALID)
+        print("boop")
+        with self._lewis.backdoor_simulate_disconnected_device():
+            self.ca.assert_that_pv_alarm_is("OUTPUTMODE", self.ca.Alarms.INVALID)
+            self.ca.assert_that_pv_alarm_is("CURRENT", self.ca.Alarms.INVALID)
+            self.ca.assert_that_pv_alarm_is("VOLTAGE", self.ca.Alarms.INVALID)
 
     def _test_ramp_to_target(self, start_current, target_current, ramp_rate, step_number, wait_between_changes):
         self._write_current(start_current)
