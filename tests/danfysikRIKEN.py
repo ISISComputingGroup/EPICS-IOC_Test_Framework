30673fdf1da6f22b131000f7c215b96f0e3be788
David Keymer <david.keymer@stfc.ac.uk>
2023-01-10 14:55:55 +0000

    remove duplicated tests by deleting class

--- tests/danfysikRIKEN.py
+++ tests/danfysikRIKEN.py
@@ -1,11 +1,6 @@
 from utils.test_modes import TestModes
 from utils.ioc_launcher import get_default_ioc_dir, ProcServLauncher
-from utils.testing import skip_if_recsim, parameterized_list
-from parameterized import parameterized
-import itertools
-from time import sleep
-
-from tests.danfysik8500 import Danfysik8500Tests, DEVICE_PREFIX, EMULATOR_NAME, POWER_STATES
+from tests.danfysik8500 import Danfysik8500Tests, DEVICE_PREFIX, EMULATOR_NAME
 
 MAX_RAW_SETPOINT = 1000000
 MIN_RAW_SETPOINT = MAX_RAW_SETPOINT * (-1)
@@ -18,7 +13,7 @@ IOCS = [
         "directory": get_default_ioc_dir("DFKPS"),
         "macros": {
             "DEV_TYPE": "8500",
-            "PROTO_OVERRIDE": "RIKEN",
+            "PROTO_OVERRIDE": "RIKEN",       # use modified protocol file for RB2 PSU
             "CALIBRATED": "0",
             "FACTOR_READ_I": "1",
             "FACTOR_READ_V": "1",
@@ -29,7 +24,7 @@ IOCS = [
             "POLARITY": "BIPOLAR",
         },
         "emulator": EMULATOR_NAME,
-        "lewis_protocol": "RIKEN",
+        "lewis_protocol": "RIKEN",       # use modified protocol file for RB2 PSU
         "ioc_launcher_class": ProcServLauncher,
     },
 ]
@@ -40,8 +35,13 @@ TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
 USE_SLEW_MACRO = "USE_SLEW"
 SLEW_PVs = ["SLEW1", "SLEW2", "SLEWABS"]
 
+
 class DanfysikRIKENTests(Danfysik8500Tests):
     """
     Tests for danfysik-like PSU on RIKEN (RB2). Tests inherited from Danfysik8500.
     """
     pass
+
+
+# delete Danfysik class so that unitest does not instantiate and rerun tests
+del Danfysik8500Tests
