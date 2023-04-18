7e9754319d377b235f7eaa0462f9f9dd31438b4b
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-04-18 01:39:58 +0100

    Update mclennan tests

--- tests/mclennan.py
+++ tests/mclennan.py
@@ -9,18 +9,21 @@ DEVICE_PREFIX = "MCLEN_01"
 EMULATOR_NAME = "mclennan"
 
 MTR1 = "MTR0101"
-MTR_DESC = f"{MTR1}.DESC"
-MTR_JOG = f"{MTR1}.JOGF"
-MTR_HLM = f"{MTR1}.HLM"
-MTR_STOP = f"{MTR1}.STOP"
-MTR_MOVN = f"{MTR1}.MOVN"
-MTR_RBV = f"{MTR1}.RBV"
-MTR_MRES = f"{MTR1}.MRES"
+MTR1_NAME = "Test"
+MTR1_DESC = f"{MTR1}.DESC"
+MTR1_JOG = f"{MTR1}.JOGF"
+MTR1_HLM = f"{MTR1}.HLM"
+MTR1_STOP = f"{MTR1}.STOP"
+MTR1_MOVN = f"{MTR1}.MOVN"
+MTR1_RBV = f"{MTR1}.RBV"
+MTR1_MRES = f"{MTR1}.MRES"
+MTR1_HVEL = f"{MTR1}.HVEL"
+MTR1_HOMR = f"{MTR1}.HOMR"
 MTR2 = "MTR0102"
 MTR2_MRES = f"{MTR2}.MRES"
 MTR2_HVEL = f"{MTR2}.HVEL"
 MTR2_HOMR = f"{MTR2}.HOMR"
-MTR_NAME = "Test"
+MTR2_STOP = f"{MTR2}.STOP"
 
 POLL_RATE = 1
 
@@ -35,9 +38,10 @@ IOCS = [
             "MTRCTRL": "01",
             "AXIS1": "yes",
             "AXIS2": "yes",
-            "NAME1": MTR_NAME,
+            "NAME1": MTR1_NAME,
             "POLL_RATE": POLL_RATE,
-            "HOME2": 2
+            "HOME1": 2, # SNL home
+            "HOME2": 0  # builtin controller home to datum
         },
     },
 ]
@@ -56,41 +60,54 @@ class MclennanTests(unittest.TestCase):
         self._lewis, self._ioc = get_running_lewis_and_ioc(EMULATOR_NAME, DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_timeout=10)
         self.ca_motor = ChannelAccess(default_timeout=30, device_prefix="MOT")
-        self.ca_motor.set_pv_value(MTR_STOP, 1, sleep_after_set=0)
+        self.ca_motor.set_pv_value(MTR1_STOP, 1, sleep_after_set=1)
+        self.ca_motor.set_pv_value(MTR2_STOP, 1, sleep_after_set=1)
 
     def test_WHEN_ioc_running_THEN_motor_has_correct_description(self):
-        self.ca_motor.assert_that_pv_is(MTR_DESC, MTR_NAME + " (MCLEN)")
+        self.ca_motor.assert_that_pv_is(MTR1_DESC, MTR1_NAME + " (MCLEN)")
 
     def test_WHEN_motor_jogged_THEN_motor_moves_and_does_not_stop_after_polled(self):
-        self.ca_motor.assert_setting_setpoint_sets_readback(10000, MTR_HLM, set_point_pv=MTR_HLM)
+        self.ca_motor.assert_setting_setpoint_sets_readback(10000, MTR1_HLM, set_point_pv=MTR1_HLM)
 
-        self.ca_motor.set_pv_value(MTR_JOG, 1, sleep_after_set=0)
+        self.ca_motor.set_pv_value(MTR1_JOG, 1, sleep_after_set=0)
 
-        self.ca_motor.assert_that_pv_value_is_increasing(MTR_RBV, 1)
-        self.ca_motor.assert_that_pv_is(MTR_MOVN, 1)
-        self.ca_motor.assert_that_pv_value_is_unchanged(MTR_MOVN, POLL_RATE * 1.5)
+        self.ca_motor.assert_that_pv_value_is_increasing(MTR1_RBV, 1)
+        self.ca_motor.assert_that_pv_is(MTR1_MOVN, 1)
+        self.ca_motor.assert_that_pv_value_is_unchanged(MTR1_MOVN, POLL_RATE * 1.5)
 
     def test_WHEN_motor_position_changes_outside_IBEX_THEN_motor_position_updated(self):
         test_position = -10
-        self.ca_motor.assert_that_pv_is_not_number(MTR_RBV, test_position)
-        mres = self.ca_motor.get_pv_value(MTR_MRES)
+        self.ca_motor.assert_that_pv_is_not_number(MTR1_RBV, test_position)
+        mres = self.ca_motor.get_pv_value(MTR1_MRES)
         self._lewis.backdoor_set_on_device("position", test_position/mres)
-        self.ca_motor.assert_that_pv_is_number(MTR_RBV, test_position)
+        self.ca_motor.assert_that_pv_is_number(MTR1_RBV, test_position)
 
-    def test_WHEN_velocity_changes_WHEN_sending_home_THEN_velocity_is_changed_then_set_back_after_home(self):
+    def test_WHEN_velocity_changes_WHEN_sending_builtin_home_THEN_creep_speed_is_changed_then_set_back_after_home(self):
+        self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
         vel = 0.2
         self.ca_motor.set_pv_value(MTR2_HVEL, vel)
         self.ca_motor.set_pv_value(MTR2_HOMR, 1)
         mres = self.ca_motor.get_pv_value(MTR2_MRES)
-        self._lewis.assert_that_emulator_value_is("creep_speed", str(int(vel/mres)))
+        self._lewis.assert_that_emulator_value_is("creep_speed2", str(int(vel/mres)))
         self.ca_motor.set_pv_value(MTR2, 1)
-        self._lewis.assert_that_emulator_value_is("creep_speed", str(700))
+        self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
+
+    def test_WHEN_velocity_changes_WHEN_sending_SNL_home_THEN_creep_speed_is_not_changed(self):
+        self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
+        vel = 0.2
+        self.ca_motor.set_pv_value(MTR1_HVEL, vel)
+        self.ca_motor.set_pv_value(MTR1_HOMR, 1)
+        mres = self.ca_motor.get_pv_value(MTR1_MRES)
+        self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
+        self.ca_motor.set_pv_value(MTR1, 1)
+        self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
 
     def test_WHEN_sending_home_with_high_velocity_THEN_creep_speed_is_capped(self):
+        self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
         vel = 100
         self.ca_motor.set_pv_value(MTR2_HVEL, vel)
         self.ca_motor.set_pv_value(MTR2_HOMR, 1)
         # Should be capped at the max creep speed (800)
-        self._lewis.assert_that_emulator_value_is("creep_speed", str(800))
+        self._lewis.assert_that_emulator_value_is("creep_speed2", str(800))
         self.ca_motor.set_pv_value(MTR2, 1)
-        self._lewis.assert_that_emulator_value_is("creep_speed", str(700))
+        self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
