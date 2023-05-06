9602619659b12439166793956aacf834cfa9320a
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-05-06 18:12:43 +0100

    Update run_all_tests_split.bat

--- run_all_tests_split.bat
+++ run_all_tests_split.bat
@@ -2,15 +2,16 @@
 setlocal EnableDelayedExpansion
 
 REM glob is case insensitive on windows but on linux would need [a-hA-H]* etc
+REM have had issues with instrona and ngpsu, so split here
 
 call %~dp0run_all_tests.bat -tf "[a-h]*"
 set ERRCODE=!ERRORLEVEL!
 if !ERRCODE! EQU 0 (
-    call %~dp0run_all_tests.bat -tf "[i-p]*"
+    call %~dp0run_all_tests.bat -tf "[i-m]*"
     set ERRCODE=!ERRORLEVEL!
 )
 if !ERRCODE! EQU 0 (
-    call %~dp0run_all_tests.bat -tf "[q-z]*"
+    call %~dp0run_all_tests.bat -tf "[n-z]*"
     set ERRCODE=!ERRORLEVEL!
 )
 exit /b !ERRCODE!
