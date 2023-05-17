b040eb9db09f9047f7d8c15f4c835db7e8bdd4bc
HannahCawley <HannahCawley@NDW1798.isis.cclrc.ac.uk>
2023-05-17 11:22:24 +0100

    Add test for pressure status & tidy some tests

--- common_tests/tpgx00.py
+++ common_tests/tpgx00.py
@@ -1,12 +1,31 @@
 import unittest
 import contextlib
 import abc
+import itertools
+from enum import Enum, unique
 
 from parameterized import parameterized
 
 from utils.channel_access import ChannelAccess
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, parameterized_list
 from itertools import product
+
+
+@unique
+class GuageStatus(Enum):
+    DATA_OK     = (0, "Measured data okay", "NO_ALARM")
+    UNDERRANGE  = (1, "Underrange", "MINOR")
+    OVERRANGE   = (2, "Overrange", "MINOR")
+    POINT_ERR   = (3, "Point error", "MAJOR")
+    POINT_OFF   = (4, "Point switched off", "MAJOR")
+    NO_HARDWARE = (5, "No hardware", "INVALID")
+
+    def __new__(cls, value, desc, sevr):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.desc = desc
+        obj.sevr = sevr
+        return obj
 
 CHANNELS = "A1", "A2", "B1", "B2"
 TEST_PRESSURES = 1.23, -10.23, 8, 1e-6, 1e+6
@@ -27,6 +46,10 @@ class Tpgx00Base():
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("tpgx00", self.get_prefix())
         self.ca = ChannelAccess(20, device_prefix=self.get_prefix(), default_wait_time=0.0)
+        
+        # Reset pressure status for each channel to okay
+        for channel in CHANNELS:
+            self._lewis.backdoor_run_function_on_device("backdoor_set_pressure_status", [channel, GAUGE_STATUS["Measured data okay"]])
 
     def tearDown(self):
         self._connect_emulator()
@@ -60,11 +83,11 @@ class Tpgx00Base():
             self.ca.assert_that_pv_is("UNITS:SP", expected_unit)
             self.ca.assert_that_pv_is("UNITS", expected_unit)
 
-    def test_that_GIVEN_a_connected_emulator_and_pressure_value_WHEN_set_pressure_is_set_THEN_the_ioc_is_updated(self):
-        for expected_pressure, channel in product(TEST_PRESSURES, CHANNELS):
-            pv = "PRESSURE_{}".format(channel)
-            self._set_pressure(expected_pressure, channel)
-            self.ca.assert_that_pv_is(pv, expected_pressure)
+    @parameterized.expand(parameterized_list(itertools.product(TEST_PRESSURES, CHANNELS)))
+    def test_that_GIVEN_a_connected_emulator_and_pressure_value_WHEN_set_pressure_is_set_THEN_the_ioc_is_updated(self, _, expected_pressure, channel):
+        pv = "PRESSURE_{}".format(channel)
+        self._set_pressure(expected_pressure, channel)
+        self.ca.assert_that_pv_is(pv, expected_pressure)
 
     @parameterized.expand(parameterized_list(CHANNELS))
     @skip_if_recsim("Recsim is unable to simulate a disconnected device")
@@ -76,16 +99,26 @@ class Tpgx00Base():
 
         self.ca.assert_that_pv_alarm_is(pv, self.ca.Alarms.NONE)
 
-    def set_switching_function(self, function):
+    @parameterized.expand(parameterized_list(CHANNELS))
+    @skip_if_recsim("Requires emulator")
+    def test_GIVEN_pressure_status_changed_THEN_pressure_status_and_pressure_severity_updated(self, _, channel):
+        for status in GuageStatus:
+            self._lewis.backdoor_run_function_on_device("backdoor_set_pressure_status", [channel, status.value])
+            self._set_pressure(TEST_PRESSURES[0], channel)
+            self.ca.assert_that_pv_is(f"PRESSURE_{channel}_STAT", status.desc, timeout=15)
+            self.ca.assert_that_pv_is(f"PRESSURE_{channel}_STAT.SEVR", status.sevr, timeout=15)
+            self.ca.assert_that_pv_is(f"PRESSURE_{channel}.SEVR", status.sevr, timeout=15)
+
+    def _set_switching_function(self, function):
         self.ca.set_pv_value("FUNCTION", function)
 
-    def set_switching_function_thresholds(self, threshold_low, threshold_high, circuit_assignment):
+    def _set_switching_function_thresholds(self, threshold_low, threshold_high, circuit_assignment):
         self.ca.set_pv_value("FUNCTION:LOW:SP", threshold_low)
         self.ca.set_pv_value("FUNCTION:HIGH:SP", threshold_high)
         self.ca.set_pv_value("FUNCTION:ASSIGN:SP", circuit_assignment)
         self.ca.process_pv("FUNCTION:ASSIGN:SP:OUT")
 
-    def check_switching_function_thresholds(self, function, thresholds, check_pv_is=True):
+    def _check_switching_function_thresholds(self, function, thresholds, check_pv_is=True):
         assignments = ("No assignment", "A1", "A2", "B1", "B1", "A1 self-monitor", "A2 self-monitor", "B1 self-monitor", "B1 self-monitor")
         if check_pv_is:
             assignment = assignments[thresholds[2]]
@@ -100,15 +133,15 @@ class Tpgx00Base():
     ])
     @skip_if_recsim("Requires emulator")
     def test_GIVEN_function_thresholds_set_THEN_thresholds_readback_correct(self, function_set, function_read):
-        self.set_switching_function("1")
-        self.set_switching_function_thresholds(0.0, 0.0, 1)
-        self.set_switching_function(function_set[0])
-        self.set_switching_function_thresholds(function_set[1], function_set[2], function_set[3])
-        self.check_switching_function_thresholds(str(function_set[0]), function_read)
-        self.set_switching_function("1")
-        self.check_switching_function_thresholds("1", (0.0, 0.0, 1))
+        self._set_switching_function("1")
+        self._set_switching_function_thresholds(0.0, 0.0, 1)
+        self._set_switching_function(function_set[0])
+        self._set_switching_function_thresholds(function_set[1], function_set[2], function_set[3])
+        self._check_switching_function_thresholds(str(function_set[0]), function_read)
+        self._set_switching_function("1")
+        self._check_switching_function_thresholds("1", (0.0, 0.0, 1))
 
-    def check_switching_function_statuses(self, expected_statuses):
+    def _check_switching_function_statuses(self, expected_statuses):
         self.ca.assert_that_pv_is("FUNCTION:STATUS:1:RB", str(expected_statuses[0]))
         self.ca.assert_that_pv_is("FUNCTION:STATUS:2:RB", str(expected_statuses[1]))
         self.ca.assert_that_pv_is("FUNCTION:STATUS:3:RB", str(expected_statuses[2]))
@@ -118,27 +151,27 @@ class Tpgx00Base():
     def test_GIVEN_function_status_set_THEN_readback_correct(self):
         function_statuses = [0, 0, 1, 1, 0, 1]
         self._lewis.backdoor_run_function_on_device("backdoor_set_switching_function_status", [function_statuses])
-        self.check_switching_function_statuses(function_statuses)
+        self._check_switching_function_statuses(function_statuses)
 
     @skip_if_recsim("Requires emulator")
     def test_GIVEN_thresholds_settings_and_pressure_above_THEN_check_if_violation_detected(self):
         self._set_pressure(0, "A2")
-        self.set_switching_function("3")
-        self.set_switching_function_thresholds(5E2, 7.5E4, 2)
+        self._set_switching_function("3")
+        self._set_switching_function_thresholds(5E2, 7.5E4, 2)
         self.ca.assert_that_pv_is("FUNCTION:3:THRESHOLD:BELOW", 1)
         self._set_pressure(6.43, "A2")
         self.ca.assert_that_pv_is("FUNCTION:3:THRESHOLD:BELOW", 1)
         self._set_pressure(501.0, "A2")
         self.ca.assert_that_pv_is("FUNCTION:3:THRESHOLD:BELOW", 0)
 
-    def check_alarm_status_rbvs(self, alarm):
+    def _check_alarm_status_rbvs(self, alarm):
         for channel in ("SEL", "1", "2", "3", "4", "A", "B"):
             self.ca.assert_that_pv_alarm_is("FUNCTION:" + channel + ":RB", alarm)
             self.ca.assert_that_pv_alarm_is("FUNCTION:" + channel + ":LOW:SP:RBV", alarm)
             self.ca.assert_that_pv_alarm_is("FUNCTION:" + channel + ":HIGH:SP:RBV", alarm)
             self.ca.assert_that_pv_alarm_is("FUNCTION:" + channel + ":ASSIGN:SP:RBV", alarm)
 
-    def check_alarm_status_function_statuses(self, alarm):
+    def _check_alarm_status_function_statuses(self, alarm):
         self.ca.assert_that_pv_alarm_is("FUNCTION:STATUS:RB", alarm)
         for channel in ("1", "2", "3", "4"):
             self.ca.assert_that_pv_alarm_is("FUNCTION:STATUS:" + channel + ":RB", alarm)
@@ -146,16 +179,17 @@ class Tpgx00Base():
 
     @skip_if_recsim("Requires emulator")
     def test_WHEN_device_disconnected_THEN_rbv_values_go_into_alarm(self):
-        self.check_alarm_status_rbvs(self.ca.Alarms.NONE)
+        self._check_alarm_status_rbvs(self.ca.Alarms.NONE)
         with self._disconnect_device():
-            self.check_alarm_status_rbvs(self.ca.Alarms.INVALID)
+            self._check_alarm_status_rbvs(self.ca.Alarms.INVALID)
 
-        self.check_alarm_status_rbvs(self.ca.Alarms.NONE)
+        self._check_alarm_status_rbvs(self.ca.Alarms.NONE)
 
     @skip_if_recsim("Requires emulator")
     def test_WHEN_device_disconnected_THEN_function_statuses_go_into_alarm(self):
-        self.check_alarm_status_function_statuses(self.ca.Alarms.NONE)
+        self._check_alarm_status_function_statuses(self.ca.Alarms.NONE)
         with self._disconnect_device():
-            self.check_alarm_status_function_statuses(self.ca.Alarms.INVALID)
+            self._check_alarm_status_function_statuses(self.ca.Alarms.INVALID)
 
-        self.check_alarm_status_function_statuses(self.ca.Alarms.NONE)
+        self._check_alarm_status_function_statuses(self.ca.Alarms.NONE)
+
