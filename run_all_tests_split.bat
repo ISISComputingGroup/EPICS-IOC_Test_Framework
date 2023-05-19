313b32b3014cd4597f78f2d56babff78498fe8ed
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-05-19 19:10:27 +0100

    Correct error variable name

--- run_all_tests_split.bat
+++ run_all_tests_split.bat
@@ -6,9 +6,9 @@ REM have had issues with instrona and ngpsu, so split here
 
 for %%i in ( "a-h" "i" "j-m" "n" "o-z" ) do (
     call %~dp0run_all_tests.bat -tf "[%%~i]*"
-    if !ERRCODE! NEQ 0 (
-        @echo ERROR code !ERRCODE! returned from run_all_tests.bat
-        exit /b !ERRCODE!
+    if !errorlevel! NEQ 0 (
+        @echo ERROR code !errorlevel! returned from run_all_tests.bat
+        exit /b !errorlevel!
     )
 )
 exit /b 0
