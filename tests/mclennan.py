dd8c81728f10c20db88b66d1f3d5cb1b92c342d3
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-05-23 11:46:31 +0100

    Update tests for security group

--- tests/mclennan.py
+++ tests/mclennan.py
@@ -28,14 +28,7 @@ MTR2_STOP = f"{MTR2}.STOP"
 
 POLL_RATE = 1
 
-IOCS = [
-    {
-        "name": DEVICE_PREFIX,
-        "directory": get_default_ioc_dir("MCLEN"),
-        "emulator": EMULATOR_NAME,
-        "ioc_launcher_class": ProcServLauncher,
-        "pv_for_existence": "AXIS1",
-        "macros": {
+IOC_MACROS = {
             "MTRCTRL": "01",
             "AXIS1": "yes",
             "AXIS2": "yes",
@@ -46,7 +39,18 @@ IOCS = [
             "CMOD2": "CLOSED",            
             "HOME1": 3, # SNL home
             "HOME2": 2  # builtin controller home to datum
-        },
+}
+
+PV_TO_WAIT_FOR = "AXIS1"
+
+IOCS = [
+    {
+        "name": DEVICE_PREFIX,
+        "directory": get_default_ioc_dir("MCLEN"),
+        "emulator": EMULATOR_NAME,
+        "ioc_launcher_class": ProcServLauncher,
+        "pv_for_existence": PV_TO_WAIT_FOR,
+        "macros": IOC_MACROS,
     },
 ]
 
@@ -70,13 +74,17 @@ class MclennanTests(unittest.TestCase):
         self.ca_motor.assert_that_pv_is(MTR1_DESC, MTR1_NAME + " (MCLEN)")
 
     def test_WHEN_motor_jogged_THEN_motor_moves_and_does_not_stop_after_polled(self):
-        self.ca_motor.assert_setting_setpoint_sets_readback(10000, MTR1_HLM, set_point_pv=MTR1_HLM)
+        macros = IOC_MACROS
+        upper_limit = 10000
+        macros['DHLM1'] = upper_limit
+        with self._ioc.start_with_macros(macros, pv_to_wait_for=PV_TO_WAIT_FOR):
+            self.ca_motor.assert_that_pv_is_number(MTR1_HLM, upper_limit)
 
-        self.ca_motor.set_pv_value(MTR1_JOG, 1, sleep_after_set=0)
+            self.ca_motor.set_pv_value(MTR1_JOG, 1, sleep_after_set=0)
 
-        self.ca_motor.assert_that_pv_value_is_increasing(MTR1_RBV, 1)
-        self.ca_motor.assert_that_pv_is(MTR1_MOVN, 1)
-        self.ca_motor.assert_that_pv_value_is_unchanged(MTR1_MOVN, POLL_RATE * 1.5)
+            self.ca_motor.assert_that_pv_value_is_increasing(MTR1_RBV, 1)
+            self.ca_motor.assert_that_pv_is(MTR1_MOVN, 1)
+            self.ca_motor.assert_that_pv_value_is_unchanged(MTR1_MOVN, POLL_RATE * 1.5)
 
     def test_WHEN_motor_position_changes_outside_IBEX_THEN_motor_position_updated(self):
         test_position = -10
@@ -88,29 +96,38 @@ class MclennanTests(unittest.TestCase):
     def test_WHEN_velocity_changes_WHEN_sending_builtin_home_THEN_creep_speed_is_changed_then_set_back_after_home(self):
         self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
         vel = 0.2
-        self.ca_motor.set_pv_value(MTR2_HVEL, vel)
-        self.ca_motor.set_pv_value(MTR2_HOMR, 1)
-        mres = self.ca_motor.get_pv_value(MTR2_MRES)
-        self._lewis.assert_that_emulator_value_is("creep_speed2", str(int(vel/mres)))
-        self.ca_motor.set_pv_value(MTR2, 1)
-        self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
+        macros = IOC_MACROS
+        macros['HVEL2'] = vel
+        with self._ioc.start_with_macros(macros, pv_to_wait_for=PV_TO_WAIT_FOR):
+            self.ca_motor.assert_that_pv_is_number(MTR2_HVEL, vel, timeout=30)
+            self.ca_motor.set_pv_value(MTR2_HOMR, 1)
+            mres = self.ca_motor.get_pv_value(MTR2_MRES)
+            self._lewis.assert_that_emulator_value_is("creep_speed2", str(int(vel/mres)))
+            self.ca_motor.set_pv_value(MTR2, 1)
+            self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
 
     def test_WHEN_velocity_changes_WHEN_sending_SNL_home_THEN_creep_speed_is_not_changed(self):
         self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
         vel = 0.2
-        self.ca_motor.set_pv_value(MTR1_HVEL, vel)
-        self.ca_motor.set_pv_value(MTR1_HOMR, 1)
-        mres = self.ca_motor.get_pv_value(MTR1_MRES)
-        self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
-        self.ca_motor.set_pv_value(MTR1, 1)
-        self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
+        macros = IOC_MACROS
+        macros['HVEL1'] = vel
+        with self._ioc.start_with_macros(macros, pv_to_wait_for=PV_TO_WAIT_FOR):
+            self.ca_motor.assert_that_pv_is_number(MTR1_HVEL, vel, timeout=30)
+            self.ca_motor.set_pv_value(MTR1_HOMR, 1)
+            mres = self.ca_motor.get_pv_value(MTR1_MRES)
+            self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
+            self.ca_motor.set_pv_value(MTR1, 1)
+            self._lewis.assert_that_emulator_value_is("creep_speed1", str(700))
 
     def test_WHEN_sending_home_with_high_velocity_THEN_creep_speed_is_capped(self):
         self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
         vel = 100
-        self.ca_motor.set_pv_value(MTR2_HVEL, vel)
-        self.ca_motor.set_pv_value(MTR2_HOMR, 1)
-        # Should be capped at the max creep speed (800)
-        self._lewis.assert_that_emulator_value_is("creep_speed2", str(800))
-        self.ca_motor.set_pv_value(MTR2, 1)
-        self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
+        macros = IOC_MACROS
+        macros['HVEL2'] = vel
+        with self._ioc.start_with_macros(macros, pv_to_wait_for=PV_TO_WAIT_FOR):
+            self.ca_motor.assert_that_pv_is_number(MTR2_HVEL, vel, timeout=30)
+            self.ca_motor.set_pv_value(MTR2_HOMR, 1)
+            # Should be capped at the max creep speed (800)
+            self._lewis.assert_that_emulator_value_is("creep_speed2", str(800))
+            self.ca_motor.set_pv_value(MTR2, 1)
+            self._lewis.assert_that_emulator_value_is("creep_speed2", str(700))
