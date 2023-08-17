2b7658d5f5bffa777ab53761889b3b377095debb
esmithExperimentControls <evan.smith@stfc.ac.uk>
2023-08-17 10:49:01 +0100

    Added scaling of 1.0, as otherwise modbus tests break these tests.

--- tests/eurotherm_eibisynch.py
+++ tests/eurotherm_eibisynch.py
@@ -41,6 +41,10 @@ TEST_MODES = [TestModes.DEVSIM]
 
 
 class EurothermTests(EurothermBaseTests, unittest.TestCase):
+    def setUp(self):
+        super(EurothermTests, self).setUp()
+        self._lewis.backdoor_set_on_device("scaling", 1.0 / float(SCALING))
+    
     def get_device(self):
         return DEVICE
 
@@ -54,74 +58,74 @@ class EurothermTests(EurothermBaseTests, unittest.TestCase):
         return contextlib.nullcontext()
 
 
-    #@parameterized.expand([
-    #    ("over_range_calc_pv_is_over_range", NONE_TXT_CALIBRATION_MAX_TEMPERATURE + 5.0, 1.0),
-    #    ("over_range_calc_pv_is_within_range", NONE_TXT_CALIBRATION_MAX_TEMPERATURE - 200, 0.0),
-    #    ("over_range_calc_pv_is_within_range", NONE_TXT_CALIBRATION_MAX_TEMPERATURE, 0.0)
-    #])
-    #def test_GIVEN_None_txt_calibration_file_WHEN_temperature_is_set_THEN(
-    #        self, _, temperature, expected_value_of_over_range_calc_pv):
-    #    """
-    #    Note: this test can only run on BISYNCH eurotherms, modbus max temperature is 6553.5 but ramp file goes up
-    #    to 10,000 and this test attempts to check this behaviour
-    #    """
-    #    # Arrange
-#
-    #    self._assert_using_mock_table_location()
-    #    with use_calibration_file(self.ca, "None.txt", prefix="A01:"):
-    #        self.ca.assert_that_pv_exists("A01:CAL:RANGE")
-    #        self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.B", NONE_TXT_CALIBRATION_MAX_TEMPERATURE)
-#
-    #        # Act:
-    #        self._set_setpoint_and_current_temperature(temperature)
-#
-    #        # Assert
-    #        self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.A", temperature)
-    #        self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER", expected_value_of_over_range_calc_pv)
-#
-    #def test_GIVEN_None_txt_calibration_file_WHEN_changed_to_C006_txt_calibration_file_THEN_the_calibration_limits_change(
-    #        self):
-    #    """
-    #    Note: this test can only run on BISYNCH eurotherms, modbus max temperature is 6553.5 but ramp file goes up
-    #    to 10,000 and this test attempts to check this behaviour
-    #    """
-    #    C006_CALIBRATION_FILE_MAX = 330.26135292267900000000
-    #    C006_CALIBRATION_FILE_MIN = 1.20927230303971000000
-#
-    #    # Arrange
-    #    self._assert_using_mock_table_location()
-    #    with use_calibration_file(self.ca, "None.txt", prefix="A01:"):
-    #        self.ca.assert_that_pv_exists("A01:CAL:RANGE")
-    #        self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.B", NONE_TXT_CALIBRATION_MAX_TEMPERATURE)
-    #        self.ca.assert_that_pv_is("A01:TEMP:RANGE:UNDER.B", NONE_TXT_CALIBRATION_MIN_TEMPERATURE)
-#
-    #    # Act:
-    #    with use_calibration_file(self.ca, "C006.txt", prefix="A01:"):
-#
-    #        # Assert
-    #        self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.B", C006_CALIBRATION_FILE_MAX)
-    #        self.ca.assert_that_pv_is("A01:TEMP:RANGE:UNDER.B", C006_CALIBRATION_FILE_MIN)
-#
-    #def test_GIVEN_simulated_delay_WHEN_temperature_read_from_multiple_sensors_THEN_all_reads_correct(self):
-    #    self._lewis.backdoor_set_on_device("delay_time", 300 / 1000)
-#
-    #    for temp in range(1, 10):
-    #        self._lewis.backdoor_set_on_device("current_temperature", float(temp))
-    #        for id in range(1,7):
-    #            sensor = f"A{id:02}"
-    #            self.ca.assert_that_pv_is(f"{sensor}:RBV", float(temp))
-#
-    #    self._lewis.backdoor_set_on_device("delay_time", None)
-#
-    #def test_GIVEN_simulated_delay_WHEN_temperature_set_on_multiple_sensors_THEN_all_reads_correct(self):
-    #    self._lewis.backdoor_set_on_device("delay_time", 300 / 1000)
-    #    for id in range(1,7):
-    #        sensor = f"A{id:02}"
-    #        self._reset_device_state(sensor=sensor)
-    #    for id in range(1,7):
-    #        sensor = f"A{id:02}"
-    #        for temp in range(1, 5):
-    #            self._set_setpoint_and_current_temperature(float(temp), sensor=sensor)
-    #            self.ca.assert_that_pv_is(f"{sensor}:RBV", float(temp), timeout=30.0)
-#
-    #    self._lewis.backdoor_set_on_device("delay_time", None)
+    @parameterized.expand([
+        ("over_range_calc_pv_is_over_range", NONE_TXT_CALIBRATION_MAX_TEMPERATURE + 5.0, 1.0),
+        ("over_range_calc_pv_is_within_range", NONE_TXT_CALIBRATION_MAX_TEMPERATURE - 200, 0.0),
+        ("over_range_calc_pv_is_within_range", NONE_TXT_CALIBRATION_MAX_TEMPERATURE, 0.0)
+    ])
+    def test_GIVEN_None_txt_calibration_file_WHEN_temperature_is_set_THEN(
+            self, _, temperature, expected_value_of_over_range_calc_pv):
+        """
+        Note: this test can only run on BISYNCH eurotherms, modbus max temperature is 6553.5 but ramp file goes up
+        to 10,000 and this test attempts to check this behaviour
+        """
+        # Arrange
+
+        self._assert_using_mock_table_location()
+        with use_calibration_file(self.ca, "None.txt", prefix="A01:"):
+            self.ca.assert_that_pv_exists("A01:CAL:RANGE")
+            self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.B", NONE_TXT_CALIBRATION_MAX_TEMPERATURE)
+
+            # Act:
+            self._set_setpoint_and_current_temperature(temperature)
+
+            # Assert
+            self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.A", temperature)
+            self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER", expected_value_of_over_range_calc_pv)
+
+    def test_GIVEN_None_txt_calibration_file_WHEN_changed_to_C006_txt_calibration_file_THEN_the_calibration_limits_change(
+            self):
+        """
+        Note: this test can only run on BISYNCH eurotherms, modbus max temperature is 6553.5 but ramp file goes up
+        to 10,000 and this test attempts to check this behaviour
+        """
+        C006_CALIBRATION_FILE_MAX = 330.26135292267900000000
+        C006_CALIBRATION_FILE_MIN = 1.20927230303971000000
+
+        # Arrange
+        self._assert_using_mock_table_location()
+        with use_calibration_file(self.ca, "None.txt", prefix="A01:"):
+            self.ca.assert_that_pv_exists("A01:CAL:RANGE")
+            self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.B", NONE_TXT_CALIBRATION_MAX_TEMPERATURE)
+            self.ca.assert_that_pv_is("A01:TEMP:RANGE:UNDER.B", NONE_TXT_CALIBRATION_MIN_TEMPERATURE)
+
+        # Act:
+        with use_calibration_file(self.ca, "C006.txt", prefix="A01:"):
+
+            # Assert
+            self.ca.assert_that_pv_is("A01:TEMP:RANGE:OVER.B", C006_CALIBRATION_FILE_MAX)
+            self.ca.assert_that_pv_is("A01:TEMP:RANGE:UNDER.B", C006_CALIBRATION_FILE_MIN)
+
+    def test_GIVEN_simulated_delay_WHEN_temperature_read_from_multiple_sensors_THEN_all_reads_correct(self):
+        self._lewis.backdoor_set_on_device("delay_time", 300 / 1000)
+
+        for temp in range(1, 10):
+            self._lewis.backdoor_set_on_device("current_temperature", float(temp))
+            for id in range(1,7):
+                sensor = f"A{id:02}"
+                self.ca.assert_that_pv_is(f"{sensor}:RBV", float(temp))
+
+        self._lewis.backdoor_set_on_device("delay_time", None)
+
+    def test_GIVEN_simulated_delay_WHEN_temperature_set_on_multiple_sensors_THEN_all_reads_correct(self):
+        self._lewis.backdoor_set_on_device("delay_time", 300 / 1000)
+        for id in range(1,7):
+            sensor = f"A{id:02}"
+            self._reset_device_state(sensor=sensor)
+        for id in range(1,7):
+            sensor = f"A{id:02}"
+            for temp in range(1, 5):
+                self._set_setpoint_and_current_temperature(float(temp), sensor=sensor)
+                self.ca.assert_that_pv_is(f"{sensor}:RBV", float(temp), timeout=30.0)
+
+        self._lewis.backdoor_set_on_device("delay_time", None)
