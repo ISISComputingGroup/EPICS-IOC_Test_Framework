a46ca759524b6e328969390f38ba36ba7cff507d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-09-23 11:34:16 +0100

    Fix tests

--- tests/CAENv895.py
+++ tests/CAENv895.py
@@ -24,9 +24,8 @@ def pre_ioc_launch_hook():
     defaults_contents_with_pv_prefix = defaults_contents.replace("{pv_prefix}", g.my_pv_prefix)
     write_defaults_to_autosave_configuration(defaults_contents_with_pv_prefix)
 
-
-NUM_CARDS_CRATE0 = 6
-NUM_CARDS_CRATE1 = 10
+MAX_CARD_NUM_CRATE0 = 6
+MAX_CARD_NUM_CRATE1 = 10
 
 IOCS = [
     {
@@ -35,8 +34,8 @@ IOCS = [
         "pv_for_existence": "CR0:BOARD_ID",
         "macros": {
             "DEFAULTCFG": "ioctestdefaults",
-            "CARDS0": NUM_CARDS_CRATE0,
-            "CARDS1": NUM_CARDS_CRATE1,
+            "CARDS0": MAX_CARD_NUM_CRATE0,
+            "CARDS1": MAX_CARD_NUM_CRATE1,
         },
         "environment_vars": {
             "DEFAULTCFG": "ioctestdefaults",
@@ -107,13 +106,13 @@ class CAENv895Tests(unittest.TestCase):
 
     def test_GIVEN_card_macro_set_WHEN_ioc_started_THEN_correct_number_of_card_pvs_loaded(self):
         # Crate 0 macro requests 6 cards
-        self.ca.assert_that_pv_is("CR0:CARDS", NUM_CARDS_CRATE0)
+        self.ca.assert_that_pv_is("CR0:CARDS", MAX_CARD_NUM_CRATE0+1)
         # The 6th card should exist, but the 7th shouldn't
         self.ca.assert_that_pv_exists("CR0:C6:ENABLE")
         self.ca.assert_that_pv_does_not_exist("CR0:C7:ENABLE")
 
         # Crate 1 macro requests 10 cards
-        self.ca.assert_that_pv_is("CR1:CARDS", NUM_CARDS_CRATE1)
+        self.ca.assert_that_pv_is("CR1:CARDS", MAX_CARD_NUM_CRATE1+1)
         # The 6th card should exist, but the 7th shouldn't
         self.ca.assert_that_pv_exists("CR1:C10:ENABLE")
         self.ca.assert_that_pv_does_not_exist("CR1:C11:ENABLE")
