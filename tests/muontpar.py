e1a09b9aea9033f49029c247f100249e555477ab
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2026-08-20 23:27:38 +0100

    Fix tests

--- tests/muontpar.py
+++ tests/muontpar.py
@@ -54,17 +54,18 @@ class MuonTPARTests(unittest.TestCase):
     def test_tpar_file_contents_match_disk_contents_on_read(self):
         self.ca.assert_that_pv_is("INPUT_FILE", DEFAULT_INPUT_FILE)
         self.ca.assert_that_pv_is("OUTPUT_FILE", DEFAULT_OUTPUT_FILE)
-        with open(test_config_path / DEFAULT_OUTPUT_FILE, "r") as tpar_file:
-            self.ca.assert_that_pv_is("LINES_ARRAY:SP", tpar_file.read())
+        with open(test_config_path / DEFAULT_OUTPUT_FILE, "rb") as tpar_file:
+            self.ca.assert_that_pv_is("LINES_ARRAY:SP", tpar_file.read().decode('utf-8'))
 
     def test_tpar_editor_writes_tpar_content(self):
         self.ca.assert_that_pv_is("UNSAVED_CHANGES", "No")
         self.ca.set_pv_value("LINES_ARRAY:SP", TEST_TPAR)
+        self.ca.assert_that_pv_is("LINES_ARRAY:SP", TEST_TPAR)
         self.ca.assert_that_pv_is("UNSAVED_CHANGES", "Yes")
         self.ca.set_pv_value("SAVE_FILE", 1, wait=True)
         self.ca.assert_that_pv_is("UNSAVED_CHANGES", "No")
-        with open(test_config_path / DEFAULT_OUTPUT_FILE, "r") as tpar_file:
-            self.assertEqual(TEST_TPAR, tpar_file.read())
+        with open(test_config_path / DEFAULT_OUTPUT_FILE, "rb") as tpar_file:
+            self.assertEqual(TEST_TPAR, tpar_file.read().decode('utf-8'))
 
     def test_tpar_editor_reset(self):
         self.ca.assert_that_pv_is("UNSAVED_CHANGES", "No")
@@ -72,5 +73,5 @@ class MuonTPARTests(unittest.TestCase):
         self.ca.assert_that_pv_is("UNSAVED_CHANGES", "Yes")
         self.ca.set_pv_value("RESET", 1)
         self.ca.assert_that_pv_is("UNSAVED_CHANGES", "No")
-        with open(test_config_path / DEFAULT_OUTPUT_FILE, "r") as tpar_file:
-            self.ca.assert_that_pv_is("LINES_ARRAY:SP", tpar_file.read())
+        with open(test_config_path / DEFAULT_OUTPUT_FILE, "rb") as tpar_file:
+            self.ca.assert_that_pv_is("LINES_ARRAY:SP", tpar_file.read().decode('utf-8'))
